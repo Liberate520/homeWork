e3588769fe81ba6963b4d60f83cb7bfe97a3bec0
Mezer_Ilya <mr.mezer@myttk.ru>
2022-10-06 22:00:33 +0700

    Долбавил логирование, но при очерезной записи лога, создается новый файл, а не дозапись исходного.

--- src/HW2/HW2_Task1.java
+++ src/HW2/HW2_Task1.java
@@ -1,20 +1,59 @@
 package HW2;
 
+import com.sun.tools.javac.Main;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class HW2_Task1 {
 
-    private static String[] dirScanner(String dirPath){
-        String [] subsArray;
-        var Dir = new File(dirPath);
-        return subsArray = Dir.list();
+    private static String[] dirScanner(String dirPath) {
+
+        Logger logger = Logger.getLogger(Main.class.getName());
+        FileHandler handler = null;
+        try {
+            handler = new FileHandler("log.txt",true);
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+        var formatter = new SimpleFormatter();
+        handler.setLevel(Level.INFO);
+        handler.setFormatter(formatter);
+        logger.addHandler(handler);
+
+        String[] subsArray = new String[0];
+        try {
+            var Dir = new File(dirPath);
+            subsArray = Dir.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.warning(e.getMessage());
+        }
+
+        return subsArray;
     }
 
     private static void stringArrayToFile(String[] array){
+        Logger logger = Logger.getLogger(Main.class.getName());
+        FileHandler handler = null;
         try {
-            String path = "C:\\Users\\carna\\IdeaProjects\\GB_Java_homeWork\\src\\HW2\\test.txt";
+            handler = new FileHandler("log.txt", true);
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+        var formatter = new SimpleFormatter();
+        handler.setLevel(Level.INFO);
+        handler.setFormatter(formatter);
+        logger.addHandler(handler);
+
+        try {
+            String path = "test.txt";
 
             BufferedWriter outputWriter = new BufferedWriter(new FileWriter(path));
             for (String str : array) {
@@ -24,7 +63,8 @@ public class HW2_Task1 {
             outputWriter.flush();
             outputWriter.close();
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            logger.warning(e.getMessage());
+            e.printStackTrace();
         }
 
     }
