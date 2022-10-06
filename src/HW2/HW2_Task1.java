150b7367a35b2b799972c5dc33ec51ac6b52e112
Mezer_Ilya <mr.mezer@myttk.ru>
2022-10-06 20:46:28 +0700

    Методы работают

--- src/HW2/HW2_Task1.java
+++ src/HW2/HW2_Task1.java
@@ -1,14 +1,36 @@
 package HW2;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 
 public class HW2_Task1 {
+
     private static String[] dirScanner(String dirPath){
         String [] subsArray;
         var Dir = new File(dirPath);
         return subsArray = Dir.list();
     }
+
+    private static void stringArrayToFile(String[] array){
+        try {
+            String path = "C:\\Users\\carna\\IdeaProjects\\GB_Java_homeWork\\src\\HW2\\test.txt";
+
+            BufferedWriter outputWriter = new BufferedWriter(new FileWriter(path));
+            for (String str : array) {
+                outputWriter.write(str);
+                outputWriter.newLine();
+            }
+            outputWriter.flush();
+            outputWriter.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
     public static void main(String[] args) {
     String[] list = dirScanner("E:\\переводы");
+    stringArrayToFile(list);
     }
 }
