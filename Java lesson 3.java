4a8c45c2dabc56cd8b532dc5f3348946b6c6e749
Elvinge <97745478+Elvinge@users.noreply.github.com>
2022-11-11 11:43:54 +0400

    Update Java lesson 3.java

--- Java lesson 3.java
+++ Java lesson 3.java
@@ -2,10 +2,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-// Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее арифметическое из этого списка. 
-// (Collections.max())
-// *Реализовать алгоритм сортировки слиянием
-
 public class task_2 {
     public static void main(String[] args) {
         List<Integer> inputList = new ArrayList<Integer>();
