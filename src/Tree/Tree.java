ba59ccaeb7df2554c2beda41b18246bdd2203c44
GrafanyS <admin@valet.of.by>
2022-11-21 16:24:41 +0300

    Класс Дерево

--- src/Tree/Tree.java
+++ src/Tree/Tree.java
@@ -1,31 +1,27 @@
-package org.lesson01;
+package Tree;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Tree {
-    private List<Link> links=new ArrayList<>();
+    private final List<Communications> links=new ArrayList<>();
 
-    public List<Link> getLinks() {
+    public List<Communications> getLinks() {
         return links;
     }
 
-    public void setLinks(List<Link> links) {
-        this.links.addAll(links);
-    }
-
     public void addLink(Person p1, Person p2, Relations r1, Relations r2) {
-        this.links.add(new Link(p1,p2,r1));
-        this.links.add(new Link(p2,p1,r2));
+        this.links.add(new Communications(p1,p2,r1));
+        this.links.add(new Communications(p2,p1,r2));
     }
 
     @Override
     public String toString() {
-        String result=new String();
-        for (Link el :
+        StringBuilder result= new StringBuilder();
+        for (Communications el :
                 links) {
-            result+=el+"\n";
+            result.append(el).append("\n");
         }
-        return result;
+        return result.toString();
     }
 }
