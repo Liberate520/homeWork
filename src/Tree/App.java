bd9c415c70b994d94153f5fb4fa0db0bbf85c5ee
GrafanyS <admin@valet.of.by>
2022-11-21 19:52:41 +0300

    Тело программы. В нем созданы несколько личностей, назначены им родственные связи и приведен вызов вункций исследования

--- src/Tree/App.java
+++ src/Tree/App.java
@@ -3,16 +3,15 @@ package Tree;
 public class App {
     public static void main(String[] args) {
         Tree tree = new Tree();
-        Person pa = new Person("Vasiliy","Valetov", "Innokentievich","11/07/1940",
+        Person pa = new Person("Valetov", "Vasiliy","Innokentievich","11/07/1940",
                 Gender.Man);
-        Person ma = new Person("Elena","Valetova", "Petrovna","24/03/1943",
+        Person ma = new Person("Valetova","Elena", "Petrovna","24/03/1943",
                 Gender.Female);
-        Person so = new Person("Andrey","Valetov", "Vasilievich","30/10/1972",
+        Person so = new Person("Valetov", "Andrey","Vasilievich","30/10/1972",
                 Gender.Man);
-        Person dot = new Person("Tamara","Voynash", "Vasilevna","20/11/1966",
+        Person dot = new Person("Voynash","Tamara", "Vasilevna","20/11/1966",
                 Gender.Man);
-        Person vot = new Person("Tamara","Voynash", "Vasilevna","20/11/1966",
-                Gender.Man);
+
 
 
         tree.addLink(pa,ma,Relations.Husband,Relations.Wife);
@@ -21,7 +20,7 @@ public class App {
         tree.addLink(ma,so,Relations.Mother,Relations.Son);
         tree.addLink(ma,dot,Relations.Mother,Relations.Daughter);
         tree.addLink(so,dot,Relations.Brother,Relations.Sister);
-        tree.addLink(vot,dot,Relations.Brother,Relations.Sister);
+
 
         System.out.println("-------------------\n");
         System.out.println(tree);
@@ -34,7 +33,7 @@ public class App {
         System.out.println("-------------------\n");
         Research.getRelation(Relations.Sister, tree);
         System.out.println("-------------------\n");
-        Research.getRelation(vot, Relations.Father, tree);
+
 
     }
 }
