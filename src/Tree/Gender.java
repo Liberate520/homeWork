7b9af60bbf05fe4d2662fec46c46758bb89a6e59
GrafanyS <admin@valet.of.by>
2022-11-25 16:08:16 +0300

    Gender класс ENUM, для описания пола. Так как пол - понятие определенное,      * то формируем справочник, исключая возможность ошибки ввода

--- src/Tree/Gender.java
+++ src/Tree/Gender.java
@@ -3,6 +3,8 @@ package Tree;
 public enum Gender {
     /**
      * The default gender of the children
+     * Gender класс ENUM, для описания пола. Так как пол - понятие определенное,
+     * то формируем справочник, исключая возможность ошибки ввода
      */
     Man,
     Female
