48f05ed8a2a7e95f318e1162c1f61b186e52ef14
GrafanyS <admin@valet.of.by>
2022-11-25 16:11:06 +0300

    Research Класс определяющий методы исследования. Имеет три перегрузки, для получения списка зависимостей

--- src/Tree/Research.java
+++ src/Tree/Research.java
@@ -2,11 +2,12 @@ package Tree;
 
 public class Research {
     /**
-     * @param person //
-     * @param relation//
-     * @param tree//
+     * Research Класс определяющий методы исследования. Имеет три перегрузки, для получения списка зависимостей:
+     * По Человеку и типу родственной связи (допустим ищет для Человека всех Сыновей),
+     * По типу родственной связи (Допустим ищет всех людей являющихся кому-либо Мужем),
+     * По Человеку (ищет все записи родственных связей для конкретного человека).
      */
-    public static void getRelation(Person person, Relations relation, Tree tree) {
+    public static void getResearch( Relations relation, Person person, Tree tree) {
         for (Communications el : tree.getLinks()) {
             if (el.getPeople().getSurName().equals(person.getSurName())  &&
                     el.getPeople().getFirstName().equals(person.getFirstName())
@@ -16,11 +17,8 @@ public class Research {
         }
     }
 
-    /**
-     * @param relation //
-     * @param tree//
-     */
-    public static void getRelation(Relations relation, Tree tree) {
+
+    public static void getResearch(Relations relation, Tree tree) {
         for (Communications el :
                 tree.getLinks()) {
             if (el.getRelationType() == relation) {
@@ -29,7 +27,7 @@ public class Research {
         }
     }
 
-    public static void getRelation(Person person, Tree tree) {
+    public static void getResearch(Person person, Tree tree) {
         for (Communications el :
                 tree.getLinks()) {
             if (el.getPeople().getSurName().equals(person.getSurName())   &&
@@ -38,4 +36,5 @@ public class Research {
             }
         }
     }
+
 }
