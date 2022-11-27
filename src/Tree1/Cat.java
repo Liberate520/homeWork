d4bb2ae103d114a91c39298a7370205790f673ad
GrafanyS <admin@valet.of.by>
2022-11-27 14:17:10 +0300

    Класс для создания экземпляров кошек. Имеет переопределенный метод описывающий реакцию отзыва в соответствии с типом.

--- src/Tree1/Cat.java
+++ src/Tree1/Cat.java
@@ -1,6 +1,10 @@
-package Tree2;
+package Tree1;
 
 public class Cat extends Pet{
+    /**
+     * Класс для создания экземпляров кошек. Имеет переопределенный метод описывающий реакцию отзыва в
+     * соответствии с типом.
+     */
     public Cat(String nickName, int age) {
         super(nickName, age);
     }
