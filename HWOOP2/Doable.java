6293acf38baac474a7e6b91743ea43c2a91eb3e4
Leonid <Leosb@mail.ru>
2022-11-28 14:53:15 +0300

    Result HWOOP2

---
+++ HWOOP2/Doable.java
@@ -0,0 +1,6 @@
+public interface Doable {
+    public void addRelation(Human human1, Human human2, Relations relation1, Relations relation2);
+    public void addPerson(Human human1);
+    public void search(String name);
+    public String print();
+}
