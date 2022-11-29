a314bf2266b509ca290cdc3de1a8481155409f22
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-11-29 13:35:34 +0300

    создал интерфейс для дерева

---
+++ src/IThree.java
@@ -0,0 +1,8 @@
+import human.Human;
+
+public interface IThree {
+    void addTreeFamily(Human human);
+    void searchHuman(String name);
+    void addChildren(Human baby, Human father, Human mother);
+
+}
