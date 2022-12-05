ac06382f859a836636fb935ea96a620e6ec58a39
Artur <artur.gikramin@yandex.ru>
2022-12-05 21:48:53 +0500

    PetTree and Person Tree типизированны

--- src/HW2/PersonTree.java
+++ src/HW2/PersonTree.java
@@ -3,24 +3,24 @@ package HW2;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PersonTree {
+public class PersonTree<T> {
     /**
      * Класс хранящий список людей. Имеет сеттер и геттер.
      */
-    private final List<Person> tree = new ArrayList<>();
+    private final List<T> tree = new ArrayList<>();
 
-    public List<Person> getTree() {
+    public List<T> getTree() {
         return tree;
     }
 
-    public void add(Person person){
+    public void add(T person){
         tree.add(person);
     }
 
     @Override
     public String toString() {
         StringBuilder str = new StringBuilder();
-        for (Person el:tree){
+        for (T el:tree){
             str.append(el.toString()).append("\n");
         }
         return str.toString();
