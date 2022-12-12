a6d13ac86a27dabdbba44b453531aa20f029d161
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-12-12 23:05:23 +0300

    add

---
+++ src/Human.java
@@ -0,0 +1,16 @@
+public class Human {
+    private String name;
+    private String surname;
+    private String birth;
+    private String death;
+
+    public Human(String name, String surname, String birth, String death){
+        this.name = name;
+        this.surname = surname;
+        this.birth = birth;
+        this.death = death;
+    }
+
+    
+    
+}
---
+++ src/Trees.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Trees {
+    private List<Human> humanlist;
+
+    public Trees(List<Human> humanlist){
+        this.humanlist = humanlist;
+    }
+    public Trees(){
+        this(new ArrayList<>());
+    }
+    
+
+}
