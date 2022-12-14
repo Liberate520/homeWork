7bee88a6ff79a5683936bc797a5a26f7f6bbf6ae
Nikita <nikvs@mail.ru>
2022-12-14 21:26:43 +0700

    First test commit

---
+++ src/Human.java
@@ -0,0 +1,4 @@
+package homeWork.src;
+
+public class Human {
+}
---
+++ src/main.java
@@ -0,0 +1,7 @@
+package homeWork.src;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("qwerty");
+    }
+}
