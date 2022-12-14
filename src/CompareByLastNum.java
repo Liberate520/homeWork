21b716b4bc4275e6860e788da7e80386935f5b95
rybalchenko-eugene <reebak@rambler.ru>
2022-12-15 01:08:29 +0300

    V4

--- src/CompareByLastNum.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class CompareByLastNum implements  Comparator<Integer>{
-
-    @Override
-    public int compare(Integer o1, Integer o2) {
-        return Integer.compare(o1 % 10, o2 % 10);
-    }
-}
