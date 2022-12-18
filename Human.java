891127f4673e5b751a7817554453d410a4b7116a
Karina <k5524392@gmail.com>
2022-12-18 23:37:01 +0300

    HW

---
+++ Human.java
@@ -0,0 +1,116 @@
+package Семинар1.homeWork;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+// Сделать наследованием от первого класса
+public class Human  {
+    private String firstname;
+    private String lastname;
+    private String birth_date;
+    private String deth_date;
+    private String gender;
+    private String person_id;
+    private String father_id;
+    private String mother_id;
+    List<Human> family;
+
+    
+    
+    public Human(String inp_firstname, 
+                       String inp_lastname, 
+                       String inp_birth_date, 
+                       String inp_deth_date, 
+                       String inp_gender, 
+                       String inp_person_id, 
+                       String inp_father_id, 
+                       String inp_mother_id){
+
+        this.firstname = inp_firstname;
+        this.lastname = inp_lastname;
+        this.birth_date = inp_birth_date;
+        this.deth_date = inp_deth_date;
+        this.gender = inp_gender;
+        this.person_id = inp_person_id;
+        this.father_id = inp_father_id;
+        this.mother_id = inp_mother_id;
+
+    }
+    public Human(String inp_firstname, String inp_lastname, String inp_birth_date, String inp_gender, String inp_person_id){
+        this(inp_firstname, inp_lastname, inp_birth_date, null, inp_gender, inp_person_id, null, null);
+
+    }
+    // Нужно прописать способ, чтобы null проставлялся как  - и изучить формат даты, в том числе, как их в f-строку записывать
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public String getPerson_id() {
+        return person_id;
+    }
+    public void setPerson_id(String person_id) {
+        this.person_id = person_id;
+    }
+    public String getgender() {
+        return gender;
+    }
+    public void setgender(String gender) {
+        this.gender = gender;
+    }
+    public String getBirthDate() {
+        return birth_date;
+    }
+    public void setBirthDate(String birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public String getDethDate() {
+        return deth_date;
+    }
+    public void setDethDate(String deth_date) {
+        this.deth_date = deth_date;
+    }
+
+    public String getFirstName() {
+        return firstname;
+    }
+    public void setFirstName(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastname;
+    }
+    public void setLastName(String lastname) {
+        this.lastname = lastname;
+    }
+    public String getFather_id() {
+        return father_id;
+    }
+    public void setFather_id(String father_id) {
+        this.father_id = father_id;
+    }
+
+    public String getMother_id() {
+        return mother_id;
+    }
+    public void setMother_id(String mother_id) {
+        this.mother_id = mother_id;
+    }
+    
+    
+    
+
+
+
+     
+}
+    
---
+++ Main.java
@@ -0,0 +1,35 @@
+package Семинар1.homeWork;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+public class Main {
+    
+    public static void main(String[] args) throws IOException{
+
+        Human person_1 = new Human("Pavel", "Petrov", "10.01.1980", null, "m", "1PP_80", null, null);
+        Human person_2 = new Human("Maria", "Petrova", "15.09.1980", null, "w", "1MP_80", null, null);
+        Human person_3 = new Human("Petr", "Petrov", "12.11.1961", null, "m", "1PP_61", null, null);
+        Human person_4 = new Human("Marina", "Petrova", "22.10.1965", null, "w", "1MP_65", null, null);
+        List<Human> family = new ArrayList<>();
+        person_3.setMother_id(person_2.getPerson_id());
+        person_3.setFather_id(person_1.getPerson_id());
+        person_4.setMother_id(person_2.getPerson_id());
+        person_4.setFather_id(person_1.getPerson_id());
+       
+        family.add(person_1);
+        family.add(person_2);
+        family.add(person_3);
+        family.add(person_4);
+    
+        
+        Tree.print_sister_and_brother(family, person_3);
+    
+        ((Семинар1.homeWork.Tree) family).print_info(family);
+        ((Семинар1.homeWork.Tree) family).read_info(family, "input_family.txt");
+        System.out.println(family);    
+        ((Семинар1.homeWork.Tree) family).age(person_4);
+    
+        }
+    }
+    
