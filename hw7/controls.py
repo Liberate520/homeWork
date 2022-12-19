0bcfc8842ff2ce4e1d927ca79c6042a2304439f2
VladimirS0l <vbnm1993@yandex.ru>
2022-12-19 23:03:07 +0300

    HW7 game starwars python

---
+++ hw7/controls.py
@@ -0,0 +1,109 @@
+import pygame, sys
+from bullet import Bullet
+from ino import Ino
+import time
+
+
+def event(screen, gun, bullets):
+    """обработка событий"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            #кнопка вправо
+            if event.key == pygame.K_RIGHT:
+                gun.mright = True
+            #кнопка влево
+            elif event.key == pygame.K_LEFT:
+                gun.mleft = True
+            elif event.key == pygame.K_SPACE:
+                new_bullet = Bullet(screen, gun)
+                bullets.add(new_bullet)
+
+        elif event.type == pygame.KEYUP:
+            # кнопка вправо
+            if event.key == pygame.K_RIGHT:
+                gun.mright = False
+            # кнопка влево
+            elif event.key == pygame.K_LEFT:
+                gun.mleft = False
+
+def update(bg_color, screen, stats, sc, gun, inos, bullets):
+    """обновление экрана"""
+    screen.fill(bg_color)
+    sc.show_score()
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    gun.output()
+    inos.draw(screen)
+    pygame.display.flip()
+
+def update_bullets(screen, stats, sc, inos, bullets):
+    """обновляет позиции пуль"""
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+    collisions = pygame.sprite.groupcollide(bullets, inos, True, True)
+    if collisions:
+        for inos in collisions.values():
+            stats.score += 10 * len(inos)
+        sc.image_score()
+        chech_high_score(stats, sc)
+        sc.image_guns()
+    if len(inos) == 0:
+        bullets.empty()
+        create_army(screen, inos)
+
+def gun_kill(stats, screen, sc, gun, inos, bullets):
+    """столкновение пушки и армии"""
+    if stats.guns_left > 0:
+        stats.guns_left -= 1
+        sc.image_guns()
+        inos.empty()
+        bullets.empty()
+        create_army(screen, inos)
+        gun.create_gun()
+        time.sleep(1)
+    else:
+        stats.guns_left = False
+        sys.exit()
+
+def update_inos(stats, screen, sc, gun, inos, bullets):
+    """обновляет позицию враж кораблей"""
+    inos.update()
+    if pygame.sprite.spritecollideany(gun, inos):
+        gun_kill(stats, screen, sc, gun, inos, bullets)
+    inos_check(stats, screen, sc, gun, inos, bullets)
+
+def inos_check(stats, screen, sc, gun, inos, bullets):
+    """добралась ли армия до края экрана"""
+    screen_rect = screen.get_rect()
+    for ino in inos.sprites():
+        if ino.rect.bottom >= screen_rect.bottom:
+            gun_kill(stats, screen, sc, gun, inos, bullets)
+            break
+
+def create_army(screen, inos):
+    """создание армии вражеских кораблей"""
+    ino = Ino(screen)
+    ino_width = ino.rect.width
+    number_ino_x = int((700 - 2 * ino_width) / ino_width)
+    ino_height = ino.rect.height
+    number_ino_y = int((800 - 100 - 2 * ino_height) / ino_height)
+
+    for row_number in range(number_ino_y - 2):
+        for ino_number in range(number_ino_x):
+            ino = Ino(screen)
+            ino.x = ino_width + ino_width * ino_number
+            ino.y = ino_height + ino_height * row_number
+            ino.rect.x = ino.x
+            ino.rect.y = ino.rect.height + ino.rect.height * row_number
+            inos.add(ino)
+
+def chech_high_score(stats, sc):
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sc.image_high_score()
+        with open("highscore.txt", 'w') as f:
+            f.write(str(stats.high_score))
