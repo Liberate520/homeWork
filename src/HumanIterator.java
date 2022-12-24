9937b98feab4369c986e89946087dfe10c1601d2
vit21513 <vit21513@yandex.ru>
2022-12-24 11:59:13 +0300

    try2

--- src/HumanIterator.java
+++ src/HumanIterator.java
@@ -3,10 +3,7 @@ import java.util.List;
 
 public class HumanIterator implements Iterator<Human> {
 
-
-
-    FamilyTree tree = new FamilyTree();
-
+  
     private  int index;
     private List<Human> famTree;
 
