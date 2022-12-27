26c97c02c4dd6e30cf66426dd7f6f34dc802add2
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-12-27 22:18:48 +0300

    Указал яввно тип

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.Serializable;
 
 public class Main implements Serializable {
     public static void main(String[] args) throws IOException {
-        Trees<Human> familyTrees = new Trees();
+        Trees<Human> familyTrees = new Trees<Human>();
         Human human1 = new Human("Igor", "Petrov", 1, 60, "male");
         Human human2 = new Human("Olga", "Petrova", 2, 59, "female");
         Human human3 = new Human("Geo", "Petrov", 20, 90, "male");
