69201f2f7c433a1aad483acdce661efc1ced84ed
NazarovIlya <eliakohai@gmail.com>
2022-12-28 21:58:46 +0300

    TComparatorByDate<T extends LiveBeing>

---
+++ src/gb/task_4/TComparatorByDate.java
@@ -0,0 +1,11 @@
+package gb.task_4;
+
+import java.util.Comparator;
+
+public class TComparatorByDate<T extends LiveBeing> implements Comparator<T> {
+    @Override
+    public int compare(T o1, T o2) {
+        return Integer.compare(o1.getDate(), o2.getDate());
+    }
+
+}
