8512327e7153a9699fe2fa716775e64672fdb447
Joeblack1989 <calwe89@gmail.com>
2022-12-28 20:52:36 +1000

    seminar_2

--- src/Tree.java
+++ src/Tree.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Tree implements GenealogicalTree {
     private List<TreeElement> treeElementList;
-
+    
     Tree(List<TreeElement> treeElementList) {
         this.treeElementList = treeElementList;
     }
@@ -11,7 +13,7 @@ public class Tree implements GenealogicalTree {
     Tree() {
         this(new ArrayList<>());
     }
-
+    
     public List<TreeElement> getPersonList() {
         return treeElementList;
     }
@@ -19,13 +21,28 @@ public class Tree implements GenealogicalTree {
     public void setPersonList(List<TreeElement> treeElementList) {
         this.treeElementList = treeElementList;
     }
-
+    
     public void addElement(TreeElement treeElement) {
         treeElementList.add(treeElement);
     }
 
     @Override
+    public TreeElement findElement(String name) {
+        for (TreeElement treeElement : treeElementList) {
+            if (treeElement.getName().equals(name)) {
+                return treeElement;
+            }
+        }
+        return null;
+    }
+
+    @Override
     public String toString() {
         return "personList = " + treeElementList;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
     }
 }
