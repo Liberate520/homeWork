518e13fcba6374fb683d28a27bf14384cab07c9b
Elena <9513495670@mail.ru>
2022-12-30 13:08:50 +0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
+import java.io.*;
 import java.util.List;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main implements Serializable{
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Family family = new Family();
 
-        Human granA = new Human("Ekaterina", "female", 80, null, null);
-        Human dadA = new Human("Ivan", "male", 53, null,granA);
-        Human granB = new Human("Mikhail", "male", 61, null,null);
-        Human granC = new Human("Anastasia", "female", 49, null,null);
-        Human momA = new Human("Julia", "female", 29, granB,granC);
-        Human momB = new Human("Vika", "female", 35, granB,null);
-        Human momD = new Human("Maria", "female", 56, null,null);
-        Human sonA = new Human("Petr", "male", 30, dadA,momD);
-        Human granson1 = new Human("Daria", "female", 2, sonA,momA);
-        Human granson2 = new Human("Vasia", "male", 5, sonA,momA);
-        Human granson3 = new Human("Jurii", "male", 8, sonA,momA);
+        Human granA = new Human("Ekaterina", Gender.WOMAN, 1942, null, null);
+        Human dadA = new Human("Ivan", Gender.MAN, 1969, null,granA);
+        Human granB = new Human("Mikhail", Gender.MAN, 1961, null,null);
+        Human granC = new Human("Anastasia", Gender.WOMAN, 1973, null,null);
+        Human momA = new Human("Julia", Gender.WOMAN, 1993, granB,granC);
+        Human momB = new Human("Vika", Gender.WOMAN, 1987, granB,null);
+        Human momD = new Human("Maria", Gender.WOMAN, 1966, null,null);
+        Human sonA = new Human("Petr", Gender.MAN, 1992, dadA,momD);
+        Human granson1 = new Human("Daria", Gender.WOMAN, 2020, sonA,momA);
+        Human granson2 = new Human("Vasia", Gender.MAN, 2017, sonA,momA);
+        Human granson3 = new Human("Jurii", Gender.MAN, 2014, sonA,momA);
         family.add(granA);
         family.add(dadA);
         family.add(granB);
@@ -27,16 +28,23 @@ public class Main {
         family.add(granson3);
         family.add(granson1);
 
-        System.out.println(family);
+        FileHandler fileHandler = new FileHandler();
+        family.setFileHandler(fileHandler);
+        family.save();
+        Family read = family.read();
+        System.out.println(read);
 
-        System.out.println(family.search("Ivan"));
-
+        Human found = family.search("Ivan");
         List<String> broAndSis = family.getBroAndSis("Vasia");
-        System.out.println(broAndSis);
         List<String> broAndSis2 = family.getBroAndSis("Julia");
-        System.out.println(broAndSis2);
         List<String> broAndSis3 = family.getBroAndSis("Ivan");
-        System.out.println(broAndSis3);
+
+
+
+
+
+
+
 
 
     }
