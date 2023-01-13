56c5c04f33c04abf3d634dc76665f72bbd305564
Elena <9513495670@mail.ru>
2023-01-13 18:28:14 +0300

    методы засписи и чтения перенесены в service из filehandler

--- src/Service.java
+++ src/Service.java
@@ -1,6 +1,8 @@
+import java.io.*;
 import java.util.Collections;
+import java.util.Map;
 
-public class Service {
+public class Service implements Writable, Serializable {
     private Family family;
 
     public Service(Family family) {
@@ -13,6 +15,27 @@ public class Service {
 
     public void sortByYearBirth() {
         Collections.sort(family.getMembers(), new HumanComparatorYearBirth());
+    }
+
+
+    @Override
+    public void save(Serializable serializable) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("family.dat"))) {
+            oos.writeObject(serializable);
+             System.out.println("Saved");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public Family read() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("family.dat"))) {
+            return (Family) ois.readObject();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
     }
 
 }
