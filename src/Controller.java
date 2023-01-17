94978c5b166607591e7534f257d7dd3071484372
NazarovIlya <eliakohai@gmail.com>
2023-01-17 23:30:42 +0300

    controller added

---
+++ src/Controller.java
@@ -0,0 +1,40 @@
+public class Controller<T extends LiveBeing<T>> {
+    FamilyTree<T> familyTree;
+    FamilyTreeView<T> familyTreeView;
+
+    public Controller(FamilyTree<T> familyTree) {
+        this.familyTree = familyTree;
+        this.familyTreeView = new FamilyTreeView<>(familyTree);
+    }
+
+    void start() {
+        familyTreeView.menu();
+        int value = familyTreeView.getValue();
+        switch (value) {
+            case 1:
+                familyTree.showAllInConsole();
+                break;
+            case 2:
+                familyTree.sortByName();
+                familyTree.showAllInConsole();
+                break;
+            case 3:
+                familyTree.sortByDate();
+                familyTree.showAllInConsole();
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 0:
+                System.exit(0);
+                break;
+            default:
+
+                break;
+        }
+
+    }
+}
