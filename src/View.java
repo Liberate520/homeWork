9f6ad033a1e704dd237e3a26043455c14b76d221
DmitrySuro <dmitrysurodeev@yandex.ru>
2023-01-18 13:30:25 +0300

    add comandlist

--- src/View.java
+++ src/View.java
@@ -1,21 +1,16 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class View {
     List<Option> comandlist = new ArrayList<>();
 
-    public List<Option> getComandlist() {
-        return comandlist;
-    }
-
-    public void setComandlist(List<Option> comandlist) {
-        this.comandlist = comandlist;
-    }
-
     public View(List<Option> comandlist) {
-        this.comandlist = comandlist;
+        comandlist = this.comandlist;
+    }
+    public void addcomand(List<Option> comandList){
+        comandlist.add(new ComandSave());
     }
     
-    comandlist.add(new ComandSave());
     
 }
