3b369fb31bd3e2b1a475e914f9e751864dca33c3
Elvira <elviraw@mail.ru>
2023-01-19 14:41:01 +0300

    deleted

--- Array/src/Sort.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class Sort implements Comparator<ArrayNumbers> {
-    @Override
-    public int compare(ArrayNumbers o1, ArrayNumbers o2) {
-        return Integer.compare(o1.getNumber(),o2.getNumber());
-    }
-}
