804cfa08733cee78cef9f576e9cc9dbc61694d61
NazarovIlya <eliakohai@gmail.com>
2023-01-21 01:31:45 +0300

    show sort by name m ethod added

---
+++ src/ShowAllSortByName.java
@@ -0,0 +1,14 @@
+public class ShowAllSortByName<T extends LiveBeing<T>> implements ICommand {
+    private FamilyTreeView<T> familyTreeView;
+
+    @Override
+    public void execute() {
+        familyTreeView.showAllByName();
+    }
+
+    @Override
+    public String discription() {
+        return "Сортировать по имени и показать";
+    }
+
+}
