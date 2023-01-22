9145811f53183db1cdbad635745d36870137e82a
ZhenyaGro <bon4ios@gmail.com>
2023-01-22 17:23:17 +0300

    Renamed

--- src/Main/Program.java
+++ src/Main/Program.java
@@ -2,12 +2,12 @@ package src.Main;
 
 import src.Entities.FamilyTree;
 import src.Entities.Human;
-import src.Service.Tree.FTFirstFill;
+import src.Service.Tree.TreeFillData;
 
 public class Program {
   public static void main(String[] args) {
     FamilyTree<Human> familyTree = new FamilyTree<Human>();
-    new FTFirstFill(familyTree);
+    new TreeFillData(familyTree);
     Controller<Human> control = new Controller<>(familyTree);
 
     while (control.getControllerStatus()) {
