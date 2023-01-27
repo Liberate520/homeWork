eb9cf5dcfa41e70bf7f4d6ee6ba87e372154270d
Froge <maksimdis31@gmail.com>
2023-01-27 19:43:36 +0300

    intermediate saving#22

--- src/VIVI/ViewNew.java
+++ src/VIVI/ViewNew.java
@@ -1,4 +1,5 @@
 package VIVI;
+
 import static javax.swing.JOptionPane.showInputDialog;
 import static javax.swing.JOptionPane.showMessageDialog;
 
@@ -8,9 +9,9 @@ import javax.swing.*;
 import java.util.Objects;
 
 public class ViewNew {
-    public void ViewvNEW(){
+    public void ViewvNEW() {
         //Новый метод вызова
-        Developer developer =new Developer(
+        Developer developer = new Developer(
                 new AddHumanCommand(),
                 new AddCildrenCommand(),
                 new addParentsCommand(),
@@ -24,25 +25,26 @@ public class ViewNew {
                               Выберете действие
                                 
                                 1.Добавить пользователя
-                                2.Добавить пользователя с Id
-                                3.Добавить детей
-                                4.Добавить родителей
-                                5.Показать все древо
-                                6.Поиск по имени
-                                7.Сортировка
+                                2.Добавить детей
+                                3.Добавить родителей
+                                4.Показать все древо
+                                5.Поиск по имени
+                                6.Сортировка
                 """);
         String txt = showInputDialog("Напишите действие");
-        if (Objects.equals(txt, "Добавить пользователя")){
+        if (Objects.equals(txt, "Добавить пользователя")) {
             developer.AddHumanCommandRecord();
-        } else if (Objects.equals(txt, "Добавить пользователя с Id"))
-
-
-        developer.AddHumanCommandRecord();
-        developer.AddСhildrenCommandRecord();
-        developer.AddParentsCommandRecord();
-        developer.SearchCommandRecord();
-        developer.ShowTreeAllCommandRecord();
-        developer.SortingCommand();
+        } else if (Objects.equals(txt, "Добавить детей")) {
+            developer.AddСhildrenCommandRecord();
+        } else if (Objects.equals(txt, "Добавить родителей")) {
+            developer.AddParentsCommandRecord();
+        }else if (Objects.equals(txt, "Показать все древо")) {
+            developer.ShowTreeAllCommandRecord();
+        }else if (Objects.equals(txt, "Поиск по имени")) {
+            developer.SearchCommandRecord();
+        }else if (Objects.equals(txt, "Сортировка")) {
+            developer.SortingCommand();
+        }
     }
 
 
