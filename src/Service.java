5b8bb9a626f0607035cd11c37494881a486e85e0
Elena <9513495670@mail.ru>
2023-02-03 17:31:39 +0300

    добавлено создание человека

--- src/Service.java
+++ src/Service.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.Collections;
-import java.util.Map;
+import java.util.Scanner;
 
 public class Service implements Writable, Serializable {
     private Family family;
@@ -8,6 +8,42 @@ public class Service implements Writable, Serializable {
     public Service(Family family) {
         this.family = family;
     }
+
+    public void addPerson(Human human){
+        family.add(human);
+
+    }
+    public void searchMember(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String name = scan.nextLine();
+        Human serch = family.search(name);
+        System.out.println(serch);
+    }
+    public void createPerson(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Name: ");
+        String name = scan.nextLine();
+        System.out.println("Gender (w - woman/ m - man): ");
+        String genderStr = scan.nextLine();
+        Gender gender;
+        if(genderStr.contains("w")){
+            gender = Gender.WOMAN;
+        } else {
+            gender = Gender.MAN;
+        }
+        System.out.println("Year birth: ");
+        int yearBirth = Integer.parseInt(scan.nextLine());
+        System.out.println("Fathers name: ");
+        String fatherStr = scan.nextLine();
+        Human father = family.search(fatherStr);// если нет имени то вернуть пусто или создать Human father
+        System.out.println(father);
+        System.out.println("Mothers name: ");
+        String motherStr = scan.nextLine();
+        Human mother = family.search(motherStr);
+        addPerson(new Human(name, gender, yearBirth, father, mother));
+    }
+
 
     public void sortByName() {
         Collections.sort(family.getMembers());
@@ -27,6 +63,7 @@ public class Service implements Writable, Serializable {
             System.out.println(ex.getMessage());
         }
     }
+
 
     @Override
     public Family read() {
