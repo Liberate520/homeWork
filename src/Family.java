4fa17de27d805600e4228b0a1af326c91a567ffd
Elena <9513495670@mail.ru>
2023-02-10 19:21:29 +0300

    убрана часть методов

--- src/Family.java
+++ src/Family.java
@@ -28,61 +28,6 @@ public class Family <T extends Human> implements Serializable, Iterable<T>{
         }
     }
 
-    public T search(String sName) {
-        T find = null;
-        for (T human: members) {
-            if (human.getName().contains(sName)) {
-                find = human;
-            }
-        }
-        return find;
-    }
-
-    public List<T> getParents(T child) {
-        List<T> parents = new ArrayList<>();
-        if(child.getFather() != null) {
-            parents.add((T) child.getFather());
-        }
-        if(child.getMother() != null) {
-            parents.add((T) child.getMother());
-        }
-        return parents;
-    }
-
-    public List<T> getChildrens(List<T> parents) {
-        List<T> children = new ArrayList<>();
-        for (T parent: parents) {
-            if(parent.getChildren() != null){
-                children.addAll((Collection<? extends T>) parent.getChildren());
-            }
-        }
-        Set<T> uniq_children = new HashSet<>(children);
-        children = new ArrayList<>(uniq_children);
-        return children;
-    }
-
-    public List<T> check(String sName) {
-        T find = search(sName);
-        List<T> parents = getParents(find);
-        if(parents.isEmpty()) return parents;
-        else{
-            List<T> children = getChildrens(parents);
-            children.remove(find);
-            return children;
-        }
-    }
-
-    public StringBuilder getBroAndSis(String sName) {
-        List<T> res = check(sName);
-        StringBuilder result = new StringBuilder(sName  + ": ");
-        if(res.isEmpty()) result.append("no sister brother");
-        else {
-            for (T human: res) {
-                result.append(human.getName() + " ");
-            }
-        }
-        return result;
-    }
 
     @Override
     public String toString() {
