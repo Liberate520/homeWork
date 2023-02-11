0a988182d3d96f9bea3fcd72c087159505e7551b
Saglara <nohaevass@yandex.ru>
2023-02-11 20:43:18 +0300

    Update Human.java

--- src/Human.java
+++ src/Human.java
@@ -5,9 +5,17 @@ public class Human {
     private Gender gender;
     private Human father;
     private Human mother;
-    private List <Human> children;
+    private List<Human> children;
+
+    public void AddChild(Human human) {
+        children.add(0, human);
 
 
-    
-    
+    }
+
+    @Override
+    public String toString() {        
+        return "Имя: "+this.name+"; " +"Пол: "+this.gender+"; "+"Отец: "+this.father+"; " +"Мать: "+this.mother+"; " +"Дети: "+ this.children;
+    }
+
 }
