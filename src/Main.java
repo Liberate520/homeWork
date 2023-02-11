ad277db1571fcd05fa1c02cc3f2c60f6a14345b8
Saglara <nohaevass@yandex.ru>
2023-02-11 20:28:50 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    FamilyTree familyTree1=new FamilyTree();
+    Human human1 = new Human();
+    Human human2 = new Human();
+    Human human3 = new Human();
+    Human human4 = new Human();
+    Human human5 = new Human();
+
+
+    
+}
