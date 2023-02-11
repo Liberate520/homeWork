4c7e8c4ff3761e7ca2d54f9f70a8526e614a1b94
Saglara <nohaevass@yandex.ru>
2023-02-11 20:31:47 +0300

    Update Human.java

--- src/Human.java
+++ src/Human.java
@@ -1,6 +1,13 @@
+import java.util.List;
+
 public class Human {
     private String name;
     private Gender gender;
+    private Human father;
+    private Human mother;
+    private List <Human> children;
+
+
     
     
 }
