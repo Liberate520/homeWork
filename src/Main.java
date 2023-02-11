5c8e46490c2f3f5018ca8a3d57bab43639cf1b96
Saglara <nohaevass@yandex.ru>
2023-02-11 20:59:05 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     FamilyTree familyTree1=new FamilyTree();
-    Human human1 = new Human();
-    Human human2 = new Human();
-    Human human3 = new Human();
-    Human human4 = new Human();
-    Human human5 = new Human();
-
-
-    
+    Human human1 = new Human(null, null, human1, human1);
+    Human human2 = new Human(null, null, human2, human2);
+    Human human3 = new Human(null, null, human3, human3);
+    Human human4 = new Human(null, null, human4, human4);
+    Human human5 = new Human(null, null, human5, human5);       
 }
