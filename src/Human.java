9d09e2869f8ae59f9db037bdcbf1f489f7c39109
Saglara <nohaevass@yandex.ru>
2023-02-11 20:45:55 +0300

    Update Human.java

--- src/Human.java
+++ src/Human.java
@@ -9,13 +9,13 @@ public class Human {
 
     public void AddChild(Human human) {
         children.add(0, human);
-
-
     }
+    
 
-    @Override
-    public String toString() {        
-        return "Имя: "+this.name+"; " +"Пол: "+this.gender+"; "+"Отец: "+this.father+"; " +"Мать: "+this.mother+"; " +"Дети: "+ this.children;
+
+@Override
+    public String toString(Human human) {        
+        return "Имя: "+human.name+"; " +"Пол: "+human.gender+"; "+"Отец: "+this.father+"; " +"Мать: "+human.mother+"; " +"Дети: "+ human.children;
     }
 
 }
