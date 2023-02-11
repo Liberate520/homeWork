27ebd07c93910f5f6effa7c2512c7d09b05f3f56
Saglara <nohaevass@yandex.ru>
2023-02-11 20:28:41 +0300

    Create FamilyTree.java

---
+++ src/FamilyTree.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+    private List <Human> humans;
+
+    public FamilyTree(){
+        this(new ArrayList<>());
+    }
+
+    private FamilyTree(List <Human> humans) {
+        this.humans=humans;
+    }
+
+    
+
+    
+    
+}
