9f5a91242108ffc7c598344c6ddef0a6477aaa8f
Saglara <nohaevass@yandex.ru>
2023-02-11 20:59:02 +0300

    Update Human.java

--- src/Human.java
+++ src/Human.java
@@ -7,11 +7,19 @@ public class Human {
     private Human mother;
     private List<Human> children;
 
+
+    public Human(String name,Gender gender,Human father,Human mother){
+        this.name=name;
+        this.gender=gender;
+        this.father=father;
+        this.mother=mother;
+
+    }
+
     public void AddChild(Human human) {
         children.add(0, human);
     }
     
-
 
 public String toString(Human human2) {        
         return "Имя: "+name+"; " +"Пол: "+gender+"; "+"Отец: "+human2.father+"; " +"Мать: "+human2.mother+"; " +"Дети: "+ children;
