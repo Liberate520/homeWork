b907d0f7c0effa1171d8845cc6367a3442de8e69
Saglara <nohaevass@yandex.ru>
2023-02-12 22:14:15 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
-    FamilyTree familyTree1=new FamilyTree();
-    
-    Human human1 = new Human(null, null, human1, human1);
-    Human human2 = new Human(null, null, human2, human2);
-    Human human3 = new Human(null, null, human3, human3);
-    Human human4 = new Human(null, null, human4, human4);
-    Human human5 = new Human(null, null, human5, human5);       
+    public static void main(String[] args) {
+
+        FamilyTree familyTree1 = new FamilyTree();
+
+        Human human1 = new Human("Иван", Gender.male);
+        Human human2 = new Human("Мария", Gender.female);
+        Human human3 = new Human("Анна", Gender.female, human1, human2);
+        Human human4 = new Human("Пётр", Gender.male, human1, human2);
+        Human human5 = new Human("Владимир", Gender.male, human1, human2);
+        Human human6 = new Human("Александр", Gender.male);
+        Human human7 = new Human("Николай", Gender.male,human6,human3);
+
+        // familyTree1.add(human1);
+        // human3(AddChild("Иван"; Gender.male; human11; human12));
+    }
 }
