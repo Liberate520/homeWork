a23623c85c881e350c3f46af916116cfbaa71408
Saglara <nohaevass@yandex.ru>
2023-02-13 20:15:36 +0300

    Update FamilyTree.java

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -2,80 +2,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FamilyTree {
-    private List <Human> humans;
+    private List<Human> humans;
 
-    public FamilyTree(){
+    public FamilyTree() {
         this(new ArrayList<>());
     }
 
-    private FamilyTree(List <Human> humans) {
-        this.humans=humans;
+    private FamilyTree(List<Human> humans) {
+        this.humans = humans;
     }
-    // public getFamilyTree() {
-    //     return familyTree1;
-    // }
-    // public void setFamilyTree1(FamilyTree familyTree1) {
-    //     this.familyTree1 = familyTree1;
-    // }
-    // public Human getHuman11() {
-    //     return human11;
-    // }
-    // public void setHuman11(Human human11) {
-    //     this.human11 = human11;
-    // }
-    // public Human getHuman12() {
-    //     return human12;
-    // }
-    // public void setHuman12(Human human12) {
-    //     this.human12 = human12;
-    // }
-    // public Human getHuman21() {
-    //     return human21;
-    // }
-    // public void setHuman21(Human human21) {
-    //     this.human21 = human21;
-    // }
-    // public Human getHuman22() {
-    //     return human22;
-    // }
-    // public void setHuman22(Human human22) {
-    //     this.human22 = human22;
-    // }
-    // public Human getHuman1() {
-    //     return human1;
-    // }
-    // public void setHuman1(Human human1) {
-    //     this.human1 = human1;
-    // }
-    // public Human getHuman2() {
-    //     return human2;
-    // }
-    // public void setHuman2(Human human2) {
-    //     this.human2 = human2;
-    // }
-    // public Human getHuman3() {
-    //     return human3;
-    // }
-    // public void setHuman3(Human human3) {
-    //     this.human3 = human3;
-    // }
-    // public Human getHuman4() {
-    //     return human4;
-    // }
-    // public void setHuman4(Human human4) {
-    //     this.human4 = human4;
-    // }
-    // public Human getHuman5() {
-    //     return human5;
-    // }
-    // public void setHuman5(Human human5) {
-    //     this.human5 = human5;
-    // }  
-
-
-
     
+    public String getChildrenToString(Human human) {
+        String res = "Дети от родителя "+ human.getInfo()+": ";
+        List<Human> children = new ArrayList<>();
+        for (int i = 0; i < humans.size(); i++) {
+            if (humans.get(i).getFather() == human){
+                children.add(humans.get(i));
+            }
+        }
+        for (int i = 0; i < children.size(); i++) {
+            res = res +"\n"+(i+1) +"). " +children.get(i).getInfo();
+        }
+        return res;
+    }
 
-    
-    
+
+
+    public String getAllHumanToString() {        
+        String res = "Генеологическое дерево: ";
+        List<Human> list1 = new ArrayList<>();
+        for (int i = 0; i < humans.size(); i++) {
+            list1.add(humans.get(i));
+        }       
+        for (int i = 0; i < humans.size(); i++) {
+            res = res +"\n"+(i+1) +"). " +humans.get(i).getInfo();
+        }
+        return res;
+    }
+
+    public void setHumans(Human human) {
+        humans.add(human);
+    }
 }
