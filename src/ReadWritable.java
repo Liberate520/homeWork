0b7000af62d33128f64f5c19e7b1e289ef7203b1
karp-ushin <karp.ushin@yandex.ru>
2023-02-17 17:35:15 +0300

    Redandant public removed

--- src/ReadWritable.java
+++ src/ReadWritable.java
@@ -2,6 +2,6 @@ import java.io.IOException;
 import java.io.Serializable;
 
 public interface ReadWritable {
-    public void write(Serializable obj) throws IOException;
-    public Object read() throws IOException, ClassNotFoundException;
+    void write(Serializable obj) throws IOException;
+    Object read() throws IOException, ClassNotFoundException;
 }
