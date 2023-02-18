cdaf64f0d0b12814a41ad9c4181608b8426f1ba4
Saglara <nohaevass@yandex.ru>
2023-02-18 22:34:28 +0300

    Update FileHandler.java

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,7 +1,18 @@
 import java.io.Serializable;
+import java.nio.channels.WritableByteChannel;
 
-public class FileHandler implements Serializable{
-    public void write(String path){
+public class FileHandler implements Writable {
+
+    @Override
+    public void save() {
         
+        System.out.println("Сохранили");
     }
+
+    @Override
+    public Object read() {
+        return null;
+    }
+
+    public void test(){}
 }
