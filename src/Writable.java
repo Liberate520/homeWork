07553d64301c2e374db865b4742565abc2b4854b
Saglara <nohaevass@yandex.ru>
2023-02-19 01:15:58 +0300

    Update Writable.java

--- src/Writable.java
+++ src/Writable.java
@@ -1,6 +1,8 @@
+import java.io.FileNotFoundException;
 import java.io.Serializable;
 
 public interface Writable {
-    void save(Serializable serializable);
+    void save(Serializable serializable) throws FileNotFoundException;
     Object read();
+    
 }
