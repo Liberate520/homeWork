f1fecb92a4b77e31345ccf237f5cd64fdd473aec
AleksaPol <113082742+AleksaPol@users.noreply.github.com>
2023-02-18 18:31:17 +1000

    HW3
    add HW3

--- FamilyTree.java
+++ FamilyTree.java
@@ -1,24 +1,29 @@
+package homeWork;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
-public class FamilyTree implements Serializable{
+
+public class FamilyTree implements Serializable, Iterable<Human>{
+    public static final char[] humamList = null;
     private List<Human> humanList;
     public FamilyTree() {this(new ArrayList<>());}
 
     public FamilyTree(List<Human> humanList) {this.humanList = humanList;}
 
-    public boolean add(Human human){
-        if (human == null){
+    public boolean add(Human humanlist){
+        if (humanlist == null){
             return false;
         }
-        if(!humanList.contains(human)){
-            humanList.add(human);
-            if(human.getFather() != null){
-                human.getFather().addChild(human);
+        if(!humanList.contains(humanlist)){
+            humanList.add(humanlist);
+            if(humanlist.getFather() != null){
+                humanlist.getFather().addChild(humanlist);
             }
-            if(human.getMother() != null){
-                human.getMother().addChild(human);
+            if(humanlist.getMother() != null){
+                humanlist.getMother().addChild(humanlist);
             }
             return true;
         }
@@ -43,7 +48,28 @@ public class FamilyTree implements Serializable{
             }
     return sb.toString();
     }
+
+    // public void sortByName(){
+    //     Collection.sort(Human.getHumanList());
+    // }
+    public List<Human> getHumanList() {
+        return humanList;
     }
+
+    public void sortFamilyTree() {
+        Collections.sort(this.getHumanList());
+
+    }
+
+    @Override
+    public Iterator<Human> iterator() {
+        // return humanList.iterator();
+        return new FamilyTreeIterator(humanList);
+    }
+}
+    
+
+    
 
    
    
