b824e00ad2e0eeaaaefd79ab74c3c649ce9a9df4
Saglara <nohaevass@yandex.ru>
2023-02-21 21:07:15 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ public class Main {
 
         FamilyTree familyTree1 = new FamilyTree();
         
-        Human human1 = new Human("Иван", Gender.male);
-        Human human2 = new Human("Мария", Gender.female);
-        Human human3 = new Human("Анна", Gender.female, human1, human2);
-        Human human4 = new Human("Пётр", Gender.male, human1, human2);
-        Human human5 = new Human("Владимир", Gender.male, human1, human2);
-        Human human6 = new Human("Александр", Gender.male);
-        Human human7 = new Human("Николай", Gender.male,human6,human3);
-        Human human8 = new Human("Софья", Gender.female);
+        Human human1 = new Human("Иван", Gender.male, 1950);
+        Human human2 = new Human("Мария", Gender.female, 1949);
+        Human human3 = new Human("Анна", Gender.female, 1971, human1, human2);
+        Human human4 = new Human("Пётр", Gender.male,1969, human1, human2);
+        Human human5 = new Human("Владимир", Gender.male,1976, human1, human2 );
+        Human human6 = new Human("Александр", Gender.male, 1970);
+        Human human7 = new Human("Николай", Gender.male,1999, human6,human3);
+        Human human8 = new Human("Софья", Gender.female, 2005);
 
         familyTree1.addHuman(human1);
         familyTree1.addHuman(human2);
@@ -24,14 +24,31 @@ public class Main {
         familyTree1.addHuman(human6);
         familyTree1.addHuman(human7);
         familyTree1.addHuman(human8);
-        System.out.println(familyTree1.getAllHumanToString());
-        System.out.println(familyTree1.getChildrenToString(human1));
-        System.out.println(familyTree1.getByName("Пётр").getInfo());
-
-       
+        // System.out.println(familyTree1.getAllHumanToString());
+        // System.out.println(familyTree1.getChildrenToString(human1));
+        // System.out.println(familyTree1.getByName("Пётр").getInfo());
         familyTree1.setFileHandler(new FileHandler());
-        familyTree1.read();
-        familyTree1.save();
-        familyTree1.read();
+        // familyTree1.read();
+        // familyTree1.save();
+        // familyTree1.read();
+
+        for (Human human: familyTree1){
+            System.out.println(human.getInfo());
+        }
+
+        System.out.println("Сортировка по имени: ");
+
+        familyTree1.sortByName();
+        for (Human human: familyTree1){
+            System.out.println(human.getInfo());
+        }
+
+
+        System.out.println("Сортировка году рождения: ");
+
+        familyTree1.sortByYearBirth();
+        for (Human human: familyTree1){
+            System.out.println(human.getInfo());
+        }
     }
 }
