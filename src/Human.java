9a7cd6a7ced335f5ad7d3008daeaa636ec3ff267
Saglara <nohaevass@yandex.ru>
2023-02-21 21:07:08 +0300

    Update Human.java

--- src/Human.java
+++ src/Human.java
@@ -2,72 +2,77 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Human implements Serializable{
+public class Human implements Serializable, Comparable<Human> {
     private String name;
     private Gender gender;
+    private Integer yearofbirth;
     private Human father;
     private Human mother;
     private List<Human> children;
 
-    public Human(String name, Gender gender, Human father, Human mother) {
+    public Human(String name, Gender gender, Integer yearofbirth, Human father, Human mother) {
         this.name = name;
         this.gender = gender;
+        this.yearofbirth = yearofbirth;
         this.father = father;
-        this.mother = mother;
-        children=new ArrayList<>();
+        this.mother = mother;        
+        children = new ArrayList<>();
     }
 
-    public Human(String name, Gender gender) {
-        this(name, gender, null,null);
+    public Human(String name, Gender gender, Integer yearofbirth) {
+        this(name, gender,yearofbirth, null,null);
     }
 
     public void addChild(Human human) {
         children.add(human);
     }
 
-    public String getName() {        
+    public String getName() {
         return name;
     }
 
-    public Gender getGender() {        
-        return gender;
-    }
-   
-    public Human getFather() {        
-        return father;
-    }
-    public Human getMother() {        
-        return mother;
-    }
-    
-    public String getInfo() {
-        return String.format("Имя: %s; Пол: %s; Отец: %s; Мать: %s.", name, gender, getFatherInfo(), getMotherInfo());
+    public Integer getYearofbirth() {
+        return yearofbirth;
     }
 
-       public String getMotherInfo() {
-        String rez="";
-        if (mother!=null){
-            rez+=mother.name;
-        }
-        else{
-            rez="неизвестна";
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public String getInfo() {
+        return String.format("Имя: %s; Пол: %s; Год рождения: %d; Отец: %s; Мать: %s.", name, gender, yearofbirth, getFatherInfo(), getMotherInfo());
+    }
+
+    public String getMotherInfo() {
+        String rez = "";
+        if (mother != null) {
+            rez += mother.name;
+        } else {
+            rez = "неизвестна";
         }
         return rez;
     }
 
     public String getFatherInfo() {
-        String rez="";
-        if (father!=null){
-            rez+=father.name;
-        }
-        else{
-            rez="неизвестен";
+        String rez = "";
+        if (father != null) {
+            rez += father.name;
+        } else {
+            rez = "неизвестен";
         }
         return rez;
     }
-
-
-
-
+    @Override
+    public int compareTo(Human o) {
+        return name.compareTo(o.name);
+    }
 
 }
