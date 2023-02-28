54d75eae14cc5b251d12c3df4693c3b9856efecb
Saglara <nohaevass@yandex.ru>
2023-02-28 20:59:38 +0300

    Create Presenter.java

---
+++ src/Presenter.java
@@ -0,0 +1,23 @@
+// public class Presenter {
+
+//     private View view;
+//     private FamilyTree familyTree;
+
+//     public Presenter(View view, FamilyTree<T> familyTree) {
+//         this.view = view;
+//         this.familyTree = familyTree;
+//         view.setPresenter(this);
+//     }
+
+//     public void onClick(String command) {
+//         if (command == "1") {
+//             String answer = familyTreeService.getAllHumanToString(command);
+//         }
+
+//         view.print(answer);
+//     }
+
+// }
+
+    
+
