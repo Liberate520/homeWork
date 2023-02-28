40b983996f4f12f896269ff1f816fd84047c6c39
Saglara <nohaevass@yandex.ru>
2023-02-28 20:59:30 +0300

    Create FamilyTreeService.java

---
+++ src/FamilyTreeService.java
@@ -0,0 +1,32 @@
+// import java.util.ArrayList;
+// import java.util.List;
+
+// public class FamilyTreeService {
+//     private FamilyTree <Human> familyTree;
+
+//     public FamilyTreeService(FamilyTree familyTree){
+//         this.familyTree= familyTree;
+//     }
+    
+//     // public void addHuman(String name, String gender, Integer yearofbirth, String father, String mother) {
+//     //     familyTree.addHuman(new Human(name, gender, yearofbirth, father, mother));        
+//     // }
+
+//     // public void addHuman(Human human) {
+//     // }
+    
+//     public String getAllHumanToString() {
+//         String res = "Генеологическое дерево: ";
+//         List<T> list1 = new ArrayList<>();
+//         for (int i = 0; i < humans.size(); i++) {
+//             list1.add(humans.get(i));
+//         }
+//         for (int i = 0; i < humans.size(); i++) {
+//             res = res + "\n" + (i + 1) + "). " + humans.get(i).getInfo();
+//         }
+//         return res;
+//     }
+
+
+    
+// }
