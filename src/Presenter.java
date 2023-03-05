f8e4685fb892f9842b066df372caff15d01a218e
Saglara <nohaevass@yandex.ru>
2023-03-05 20:18:55 +0300

    Update Presenter.java

--- src/Presenter.java
+++ src/Presenter.java
@@ -2,6 +2,7 @@ public class Presenter {
 
     private View view;
     private Human human;
+    FamilyTree familyTree1;
 
     public Presenter(View view) {
         this.view = view;
@@ -9,32 +10,30 @@ public class Presenter {
     }
 
     public void addHuman(Human human) {
+        this.human=human;
+        this.familyTree1 = familyTree1;
+        boolean addHuman = familyTree1.addHuman(human);
         System.out.println("презентер работает");
     }
 }
 
-
-
 // public class Presenter {
 
-//     private View view;
-//     private FamilyTree familyTree;
+// private View view;
+// private FamilyTree familyTree;
 
-//     public Presenter(View view, FamilyTree<T> familyTree) {
-//         this.view = view;
-//         this.familyTree = familyTree;
-//         view.setPresenter(this);
-//     }
-
-//     public void onClick(String command) {
-//         if (command == "1") {
-//             String answer = familyTreeService.getAllHumanToString(command);
-//         }
-
-//         view.print(answer);
-//     }
-
+// public Presenter(View view, FamilyTree<T> familyTree) {
+// this.view = view;
+// this.familyTree = familyTree;
+// view.setPresenter(this);
 // }
 
-    
+// public void onClick(String command) {
+// if (command == "1") {
+// String answer = familyTreeService.getAllHumanToString(command);
+// }
 
+// view.print(answer);
+// }
+
+// }
