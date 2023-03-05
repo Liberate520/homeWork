950b38c987def7e5f5df69b40b4bf4607a295316
Saglara <nohaevass@yandex.ru>
2023-03-05 18:49:52 +0300

    Update Console.java

--- src/Console.java
+++ src/Console.java
@@ -3,28 +3,113 @@ import java.util.Scanner;
 
 public class Console implements View {
     private Scanner scanner;
-    private FamilyTree familyTree1;    
+    private Presenter presenter;
+    private FamilyTree familyTree1;
+    private Human human;
+    private Menu menu;
+    private boolean work;   
 
-    public Console(FamilyTree familyTree1) {
+    
+    // public Console(FamilyTree familyTree1)
+    public Console() 
+    {
         scanner = new Scanner(System.in);
         this.familyTree1=familyTree1;
     }   
 
-    public void commands() {
 
-        System.out.println("Введите команду: '1' - распечатать дерево, '2' - сортировать дерево");
-        String comm = scanner.nextLine();
-        if (comm.equals("1")) {            
-            System.out.println(familyTree1.getAllHumanToString());
-                      
-        }
-        if (comm.equals("2")) {
-            familyTree1.sortByName();
-            System.out.println("Сортировка");
-            System.out.println(familyTree1.getAllHumanToString());
+
+
+    @Override
+    public void setPresenter(Presenter presenter) {
+        this.presenter = presenter;
+        
     }
-}
-  
+
+    @Override
+    public void start() {
+        scanner = new Scanner(System.in);
+        menu = new Menu(this);
+        work = true;
+        while (work){
+            hello();
+            String command = scanner.nextLine();
+            if (checkInput(command)){
+                menu.execute(Integer.parseInt(command));
+            } else {
+                System.out.println("что-то пошло не так");
+            }
+        }
+    }
+
+    
+    private boolean checkInput(String text){
+        return text.matches("[0-9]+");
+    }
+
+    private void hello() {
+        System.out.println(menu.printMenu());
+    }
+
+    @Override
+    public void print(String text){
+        System.out.println(text);
+    }
+
+    public void finish(){
+        work = false;
+    }
+
+    public void addHuman(){
+        System.out.println("Введите Имя");
+        String name2 = scanner.nextLine();
+        System.out.println("Введите пол (male или female): ");
+        String gender2 = scanner.nextLine();
+        System.out.println("Введите год рождения");
+        Integer yearbirth2 = scanner.nextInt();
+        System.out.println("Введите Имя отца");
+        String father2 = scanner.nextLine();
+        System.out.println("Введите Имя матери");
+        String mother2 = scanner.nextLine();
+        human = new Human(name2, gender2, yearbirth2, father2, mother2);
+        presenter.addHuman(human);
+    }
+
+    // public void addHuman(){
+    //     System.out.println("Введите Имя");
+    //     String name2 = scanner.nextLine();
+    //     System.out.println("Введите пол (male или female): ");
+    //     String gender2 = scanner.nextLine();
+    //     System.out.println("Введите год рождения");
+    //     Integer yearbirth2 = scanner.nextInt();
+    //     human = new Human(name2, gender2, yearbirth2);
+    //     presenter.addHuman(human);
+
+
+
+
+
+
+
+
+
+    
+//     public void commands() {
+
+//         System.out.println("Введите команду: '1' - распечатать дерево, '2' - сортировать дерево");
+//         String comm = scanner.nextLine();
+//         if (comm.equals("1")) {            
+//             System.out.println(familyTree1.getAllHumanToString());
+                      
+//         }
+//         if (comm.equals("2")) {
+//             familyTree1.sortByName();
+//             System.out.println("Сортировка");
+//             System.out.println(familyTree1.getAllHumanToString());
+//     }
+// }
+
+
     // не получается записать строку human и в gender???
     // @Override
     // public void commands() {
