1a964b696625309b703b1f415ebd8327abb472ea
Saglara <nohaevass@yandex.ru>
2023-03-10 20:30:21 +0300

    Update Option.java

--- src/Option.java
+++ src/Option.java
@@ -1,4 +1,7 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public interface Option {
     String description();
-    void execute();
+    void execute() throws FileNotFoundException, IOException;
 }
