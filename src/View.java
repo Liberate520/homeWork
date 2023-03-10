660faff844b634e61f42bcd1f3a3c18667f37998
Saglara <nohaevass@yandex.ru>
2023-03-10 20:30:38 +0300

    Update View.java

--- src/View.java
+++ src/View.java
@@ -1,10 +1,8 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public interface View {
     void setPresenter(Presenter presenter);
-    void start();
+    void start() throws NumberFormatException, FileNotFoundException, IOException;
     void print(String text);
 }
-
-
-// public interface View {    
-//     void commands();      
-// }
