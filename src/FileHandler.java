e58621998ee155de8a229dc560839670ac6d446a
Saglara <nohaevass@yandex.ru>
2023-03-10 20:30:02 +0300

    Update FileHandler.java

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,63 +1,35 @@
 import java.io.*;
 
-public class FileHandler implements Writable {
-    
-    @Override
-    public void save(Serializable serializable) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(("FamTree.data")))) {                      
-            oos.writeObject(serializable);
-            System.out.printf("Сохранено\n", ("FamTree.data")); 
-        } catch (Exception ex) {
-            System.out.println("Ошибка");           
-            System.out.println(ex.getMessage());
+public class FileHandler implements Writable{
+    private final String path;
+
+    public FileHandler(String path) {
+        this.path = path;
+    }
+
+    public void write(Serializable obj) {
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+                    new FileOutputStream(path));
+            objectOutputStream.writeObject(obj);
+            objectOutputStream.close();
+        } catch (Exception e) {
+            System.out.println("Could not save to file.");
         }
     }
-    
-    
-    
-    @Override
-    public FamilyTree read() {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(("FamTree.data")))) {
-            return (FamilyTree) ois.readObject();
-        } catch (Exception ex) {
-            System.out.println("Ошибка");
-            System.out.println(ex.getMessage());
-        }
-        return null;
-    }
-    
-    // @Override
-    // public void save(Serializable serializable) throws FileNotFoundException {
-    //     try (FileOutputStream outputStream = new FileOutputStream("FamTree.data")) {
-    //         ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-    //         objectOutputStream.writeObject(serializable);
 
-    //         try {
-    //             objectOutputStream.writeObject(serializable);
-    //         } catch (IOException e) {
-    //             e.printStackTrace();
-    //         }
-    //         objectOutputStream.close();            
-    //     } catch (IOException e) {            
-    //         e.printStackTrace();
-    //     }
-    //     System.out.println("Сохранили");
-    // }
+    public Object read() {
+        do {
+            try {
+                ObjectInputStream objectInputStream = new ObjectInputStream(
+                        new FileInputStream(path));
+                Object obj = objectInputStream.readObject();
+                objectInputStream.close();
+                return obj;
+            } catch (Exception e) {
+                System.out.println("Could not read from file");
+            }
+        } while (true);
 
-    // @Override
-    // public Object read() throws FileNotFoundException, IOException {
-    //     ObjectInputStream objectInputStream = new ObjectInputStream(
-    //             new FileInputStream("FamTree.data"));
-    //             try {
-    //                 Writable renatRestored = (Writable) objectInputStream.readObject();
-    //             } catch (ClassNotFoundException e) {
-    //                 // TODO Auto-generated catch block
-    //                 e.printStackTrace();
-    //             }
-    //     objectInputStream.close();
-    //     return null;
-    // }
-
-        public void test() {
     }
 }
