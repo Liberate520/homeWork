4710e884908cac8e23cfcd3862035df7e1492fd9
Saglara <nohaevass@yandex.ru>
2023-03-10 20:30:04 +0300

    Update Human.java

--- src/Human.java
+++ src/Human.java
@@ -1,52 +1,58 @@
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 public class Human extends Individual implements Serializable, Comparable<Human> {
     private String name;
     private Gender gender;
-    private Integer yearofbirth;
+    private Date birthDate;
     private Human father;
     private Human mother;
     private List<Human> children;
     private Presenter presenter;
     private Console console;
 
-    public Human(String name, Gender gender, Integer yearofbirth, Human father, Human mother) {
-        this.name = name;
-        this.gender = gender;
-        this.yearofbirth = yearofbirth;
-        this.father = father;
-        this.mother = mother;
-        children = new ArrayList<>();
+    public Human(String name, Gender gender, Date birthDate) {
+        this(name, gender, birthDate, null, null);
     }
 
-    public Human(String name, Gender gender, Integer yearofbirth) {
-        this(name, gender, yearofbirth, null, null);
+    public Human(String name, Gender gender, Date birthDate, Human father, Human mother) {
+        this.name = name;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.father = father;
+        this.mother = mother;
+        this.children = new ArrayList<>();
     }
+
+
+    public List<Human> getChildren() {
+        return children;
+    }
+
 
     public Human(String name2, String gender2, Integer yearofbirth2, String father2, String mother2) {
     }
 
-    public boolean addChild(Human human) {
-        if (!children.contains(human)) {
-            children.add(human);
+       public boolean addChild(Human child){
+        if (!children.contains(child)){
+            children.add(child);
             return true;
         }
         return false;
     }
+
     
-    public void setPresenter(Presenter presenter){
-        this.presenter = presenter;
-    }
+    // public void setPresenter(Presenter presenter){
+    //     this.presenter = presenter;
+    // }
 
     public String getName() {
         return name;
     }
 
-    public Integer getYearofbirth() {
-        return yearofbirth;
-    }
+    public Date getBirthDate() { return birthDate; }
 
     public Gender getGender() {
         return gender;
@@ -61,7 +67,7 @@ public class Human extends Individual implements Serializable, Comparable<Human>
     }
 
     public String getInfo() {
-        return String.format("Имя: %s; Пол: %s; Год рождения: %d; Отец: %s; Мать: %s.", name, gender, yearofbirth,
+        return String.format("Имя: %s; Пол: %s; Год рождения: %d; Отец: %s; Мать: %s.", name, gender, birthDate,
                 getFatherInfo(), getMotherInfo());
     }
 
@@ -83,6 +89,31 @@ public class Human extends Individual implements Serializable, Comparable<Human>
             rez = "неизвестен";
         }
         return rez;
+    }
+
+    
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj){
+            return true;
+        }
+        if (!(obj instanceof Human)){
+            return false;
+        }
+        Human human = (Human) obj;
+        return human.getName().equals(getName());
+    }
+
+    public  void removeChild(Human human) {
+        children.remove(human);
+    }
+    
+    public void removeMother() {
+        mother = null;
+    }
+
+    public void removeFather() {
+        father = null;
     }
 
     @Override
