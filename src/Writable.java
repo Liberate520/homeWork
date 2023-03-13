4eb47d0da058f0d6b0c79a22e1e8ef6c7aa36a68
Saglara <nohaevass@yandex.ru>
2023-03-13 17:26:42 +0300

    Delete Writable.java

--- src/Writable.java
+++
@@ -1,9 +0,0 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.Serializable;
-
-public interface Writable {
-    void write(Serializable serializable) throws FileNotFoundException;
-    Object read() throws FileNotFoundException, IOException;
-    
-}
