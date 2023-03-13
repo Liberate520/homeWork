7dfe957fbfda3af9fecc1b40d0b1adbe08aad38d
Saglara <nohaevass@yandex.ru>
2023-03-13 17:26:38 +0300

    Create Option.java

---
+++ src/ui/command/Option.java
@@ -0,0 +1,9 @@
+package ui.command;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public interface Option {
+    String description();
+    void execute() throws FileNotFoundException, IOException;
+}
