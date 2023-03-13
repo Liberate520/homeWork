2726763620009463d2c384c1905376497e338f38
Saglara <nohaevass@yandex.ru>
2023-03-13 17:25:51 +0300

    Create HumanComparatorByYearBirth.java

---
+++ src/model/HumanComparatorByYearBirth.java
@@ -0,0 +1,13 @@
+package model;
+import java.util.Comparator;
+
+public class HumanComparatorByYearBirth<T extends Individual> implements Comparator<T>{
+
+    @Override
+    public int compare (T o1, T o2) {       
+        return Integer.compare(o1.getYearofbirth(), o2.getYearofbirth());
+    }
+    
+}
+
+
