d594563da2eab5fd7405515c8b41086ea14dfeba
Saglara <nohaevass@yandex.ru>
2023-03-13 17:26:25 +0300

    Delete View.java

--- src/View.java
+++
@@ -1,8 +0,0 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-public interface View {
-    void setPresenter(Presenter presenter);
-    void start() throws NumberFormatException, FileNotFoundException, IOException;
-    void print(String text);
-}
