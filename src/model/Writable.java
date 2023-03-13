bd60177098a19e08988d18a70b38f9fa722b17a2
Saglara <nohaevass@yandex.ru>
2023-03-13 17:25:57 +0300

    Create Writable.java

---
+++ src/model/Writable.java
@@ -0,0 +1,10 @@
+package model;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Serializable;
+
+public interface Writable {
+    void write(Serializable serializable) throws FileNotFoundException;
+    Object read() throws FileNotFoundException, IOException;
+    
+}
