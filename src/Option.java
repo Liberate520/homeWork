0ef613a4159dc2f3518fe538e2a575b69148fe46
Saglara <nohaevass@yandex.ru>
2023-03-13 17:26:01 +0300

    Delete Option.java

--- src/Option.java
+++
@@ -1,7 +0,0 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-public interface Option {
-    String description();
-    void execute() throws FileNotFoundException, IOException;
-}
