64b33f690d2aaf4ff8580b8a9876866b81fd8d0c
Saglara <nohaevass@yandex.ru>
2023-03-13 17:25:19 +0300

    Delete HumanComparatorByName.java

--- src/HumanComparatorByName.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class HumanComparatorByName<T extends Individual> implements Comparator<T>{
-
-    @Override
-    public int compare (T o1, T o2) {       
-        return  o1.getName().compareTo(o2.getName());
-    }
-}
