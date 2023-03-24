eb86cd6215a195cbbec75ccd7f44cf4b1780f25c
alexey <asivlev@yandex.ru>
2023-03-25 02:56:22 +0300

    Цикл перенесен в пакет view.

--- src/presenter/Presenter.java
+++ src/presenter/Presenter.java
@@ -1,70 +1,41 @@
 package presenter;
 
-import model.Lesson;
-import model.StudyPlan;
-import ui.View;
-import java.util.ArrayList;
-
+import model.Model;
+import view.View;
 
 public class Presenter {
-    private final StudyPlan studyPlan;
+
+    // ПОЛЯ КЛАССА PRESENTER
     private final View view;
+    private final Model model;
 
-
-    public Presenter(StudyPlan studyPlan, View view) {
-        this.studyPlan = studyPlan;
+    // КОНСТРУКТОР КЛАССА PRESENTER
+    public Presenter(View view, Model model) {
         this.view = view;
+        this.model = model;
+        view.setPresenter(this);
     }
 
-    public void start() {
-        while (true) {
-            view.menu();
-            int userDecision = view.userDecision();
-            if (userDecision > 0 & userDecision < 6) {
-                switch (userDecision) {                                                   // ВЫБОР ВАРИАНТА ДЕЙСТВИЙ
-                    case 1 -> {                                                           // ПРОСМОТР ЗАНЯТИЙ
-                        if (!studyPlan.isEmpty()) view.printAll(studyPlan.toString());
-                        else view.emptyStudyPlan();
-                    }
-                    case 2 -> {                                                           // ДОБАВЛЕНИЕ НОВОГО ЗАНЯТИЯ
-                        ArrayList<String> userInput = view.addLesson();
-                        Lesson newLesson = new Lesson(userInput.get(0), userInput.get(1), userInput.get(2), userInput.get(3), userInput.get(4),
-                                userInput.get(5), userInput.get(6));
-                        studyPlan.add(newLesson);
-                        view.addedLesson();
-                    }
-                    case 3 -> {                                                           // УДАЛЕНИЕ ЗАНЯТИЯ
-                        if (studyPlan.isEmpty()) view.emptyStudyPlan();
-                        else {
-                            view.removeLesson();
-                            int numberLesson = view.userChoiceRemove();
-                            studyPlan.remove(numberLesson);
-                            view.removeConformation();
-                        }
-                    }
-                    case 4 -> {
-                        if (studyPlan.isEmpty()) view.emptyStudyPlan();
-                        else {
-                            view.printAll(studyPlan.toString());
-                            view.changeLesson();
-                            int numberLesson = view.userChange();
-                            studyPlan.remove(numberLesson);
-                            ArrayList<String> userInput = view.addLesson();
-                            Lesson newLesson = new Lesson(userInput.get(0), userInput.get(1), userInput.get(2), userInput.get(3), userInput.get(4),
-                                    userInput.get(5), userInput.get(6));
-                            studyPlan.add(newLesson);
-                            view.changeConformation();
-                        }
-                    }
-                    case 5 -> {
-                        return;
-                    }
-                }
-            }
-            else {  // ПРОВЕРКА ПРАВИЛЬНОСТИ ВВОДА ДАННЫХ ПОЛЬЗОВАТЕЛЕМ ПРИ РАБОТЕ С МЕНЮ
-                System.out.printf("\nВ списке нет числа %s\n", userDecision + ". Введите корректное значение.");
-                System.out.println();
-            }
-        }
+    // МЕТОДЫ КЛАССА PRESENTER
+
+    public void showAll() {
+
+        view.printText(this.model.toString());
+    }
+
+    public void addRec(String text, String deadline, String notes) {
+        this.model.addRec(text, deadline, notes);
+    }
+
+    public void delRec(Integer index) {
+        this.model.delRec(index);
+    }
+
+    public void load() {
+        this.model.load();
+    }
+
+    public void save() {
+        this.model.save();
     }
 }
