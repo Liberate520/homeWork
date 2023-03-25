dfe1cf305cb841d9c5f3788c6e2be85a89ff01b2
alexey <asivlev@yandex.ru>
2023-03-25 03:10:44 +0300

    Создан абстрактный класс План.

---
+++ src/model/Plan.java
@@ -0,0 +1,12 @@
+package model;
+
+public abstract class Plan {
+    // ПОЛЕ АБСТРАКТНОГО КЛАССА
+    protected String task;
+
+    // КОНСТРУКТОР КЛАССА PLAN
+    public Plan(String task) {
+        this.task = task;
+    }
+
+}
