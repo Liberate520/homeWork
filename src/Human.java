fc7c2fb56f80b6676747651afc615c62e2c9ae67
Eugene_Pro <proskuryakov.e@list.ru>
2023-03-27 12:39:50 +0300

    fields of the Human class have been created

--- src/Human.java
+++ src/Human.java
@@ -1,2 +1,17 @@
+import java.util.Date;
+import java.util.List;
+
 public class Human {
+
+    private String name;
+    private String surname;
+    private Date dateBirth;
+
+    Gender gender;
+    Human father;
+    Human mother;
+
+    List<Human> childList;
+
+
 }
