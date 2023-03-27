7e148b5539c3ea09c31a7a738e34a2772f4e130c
alexey <asivlev@yandex.ru>
2023-03-27 12:42:40 +0300

    PrintAll - without changes

--- src/view/operations/PrintAll.java
+++ src/view/operations/PrintAll.java
@@ -1,2 +1,19 @@
-package view.operations;public class PrintAll {
+package view.operations;
+
+import view.ConsoleUI;
+
+public class PrintAll extends ViewTasker {
+    public PrintAll(ConsoleUI consoleUI) {
+        super(consoleUI);
+    }
+
+    @Override
+    public String optionName() {
+        return "Показать план            ";
+    }
+
+    @Override
+    public void execute() {
+        getConsole().showAll();
+    }
 }
