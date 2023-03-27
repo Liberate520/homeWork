69390143303cce97518f9e5f59b5cb5df35e4c6a
alexey <asivlev@yandex.ru>
2023-03-27 12:41:03 +0300

    Exit - without changes

--- src/view/operations/Exit.java
+++ src/view/operations/Exit.java
@@ -1,2 +1,19 @@
-package view.operations;public class Exit {
+package view.operations;
+
+import view.ConsoleUI;
+
+public class Exit extends ViewTasker {
+    public Exit(ConsoleUI consoleUI) {
+        super(consoleUI);
+    }
+
+    @Override
+    public String optionName() {
+        return "Выйти из программы       ";
+    }
+    //         "Добавить новое занятие"
+    @Override
+    public void execute() {
+        getConsole().end();
+    }
 }
