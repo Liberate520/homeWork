c642e55a07a7162523e65370290aa74ab3c902fa
Eugene_Pro <proskuryakov.e@list.ru>
2023-03-27 12:03:23 +0300

    add class FamilyTree

---
+++ src/FamilyTree.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+    private List<Human> humanList;
+
+    public FamilyTree() {
+        this.humanList = new ArrayList<>();
+    }
+
+
+}
