5f9a37622f808e7d739c0904013759bc2ee236d6
Eugene_Pro <proskuryakov.e@list.ru>
2023-03-27 13:22:22 +0300

    added the getByName, get By Surname and addHuman methods to the FamilyTree class

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -8,5 +8,28 @@ public class FamilyTree {
         this.humanList = new ArrayList<>();
     }
 
+    public Human getByName(String name) {
+        for (Human human :
+                humanList) {
+            if (human.getName().equals(name))
+                return human;
+        }
+        return null;
+    }
+
+    public Human getBySurname(String surname) {
+        for (Human human :
+                humanList) {
+            if (human.getSurname().equals(surname))
+                return human;
+        }
+        return null;
+    }
+
+
+    public void addHuman(Human human) {
+        humanList.add(human);
+    }
+
 
 }
