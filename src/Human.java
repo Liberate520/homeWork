2b8917862a2449aecc49f0414d82d18ec5a821c7
Eugene_Pro <proskuryakov.e@list.ru>
2023-03-27 12:50:48 +0300

    added a full constructor and basic get methods to the Human class

--- src/Human.java
+++ src/Human.java
@@ -13,6 +13,35 @@ public class Human {
 
     List<Human> childList;
 
+    public Human(String name, String surname, Date dateBirth, Gender gender,
+                 Human father, Human mother, List<Human> childList) {
+        this.name = name;
+        this.surname = surname;
+        this.dateBirth = dateBirth;
+        this.gender = gender;
+        this.father = father;
+        this.mother = mother;
+        this.childList = childList;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public Date getDateBirth() {
+        return this.dateBirth;
+    }
+
+    public List<Human> getChildList() {
+        return childList;
+    }
+
+
+
 
 
 
