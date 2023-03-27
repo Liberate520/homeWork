897349cc898018af7e45383122927374f96acc55
alexey <asivlev@yandex.ru>
2023-03-27 12:38:38 +0300

    AddTask - without changes

--- src/view/operations/AddTask.java
+++ src/view/operations/AddTask.java
@@ -1,2 +1,19 @@
-package view.operations;public class AddTask {
+package view.operations;
+
+import view.ConsoleUI;
+
+public class AddTask extends ViewTasker {
+    public AddTask(ConsoleUI consoleUI) {
+        super(consoleUI);
+    }
+
+    @Override
+    public String optionName() {
+        return "Добавить новую задачу    ";
+    }
+
+    @Override
+    public void execute() {
+        getConsole().addRec();
+    }
 }
