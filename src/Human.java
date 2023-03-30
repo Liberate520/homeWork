68c6ca4444e0f8d99779b2291c12635240f7f79d
Lexuse <Lexuse2007@gmail.com>
2023-03-30 15:46:12 +0300

    finished

---
+++ src/Human.java
@@ -0,0 +1,49 @@
+
+
+public class Human {
+    private String name;
+    private String surname;
+    private String manOrWoman;
+    private String birthday;
+    private String dateOfDeath;
+
+    public Human(String name, String surname, String manOrWoman, String birthday){
+        this(name, surname, manOrWoman, birthday, "Жив");
+//        this.dateOfDeath = "Жив";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getManOrWoman() {
+        return manOrWoman;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public Human(String name, String surname, String manOrWoman, String birthday, String dateOfDeath) {
+        this.name = name;
+        this.surname = surname;
+        this.manOrWoman = manOrWoman;
+        this.birthday = birthday;
+        this.dateOfDeath = dateOfDeath;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return String.format("Имя: %s \nФамилия: %s \nПол: %s \nДата рождения: %s \nДата смерти: %s \n", name, surname, manOrWoman, birthday, dateOfDeath);
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,13 @@
+
+
+public class Program {
+
+
+    public static void main(String[] args) {
+        //System.out.println(human3.toString());
+        Tree.SearchChildren("Василий", "Иванов");
+    }
+
+
+}
+
