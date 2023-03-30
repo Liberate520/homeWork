5e12607acee990e9f9d5e002aa45ecbdf897b9cf
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2023-03-30 15:48:40 +0700

    Update Main.java
    correcting of cod

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,23 +10,14 @@ public class Main {
         Human h_5 = new Human("Грищенко Афонасий Ермолаевич", Gender.male, 1915);
         Human h_6 = new Human("Богданова Зоя Афонасьевна", Gender.female, h_5, h_3, 1937);
         Human h_7 = new Human("Грищенко Александр Афонасьевич", Gender.male, h_5, h_3, 1940);
-        ArrayList<Human> humans = new ArrayList<>(Arrays.asList(h_1, h_2, h_3, h_4, h_5, h_6, h_7));
+        ArrayList<Human> humans = new ArrayList<Human>(Arrays.asList(h_1, h_2, h_3, h_4, h_5, h_6, h_7));
 
-        FamilyTree familyTree = new FamilyTree(humans);
-        familyTree.checkAndAddChildren();
-        System.out.println(familyTree);
-
-        Iterator<Human> iterator = familyTree.iterator();
-        while (iterator.hasNext()) {
-            Human human = iterator.next();
-            if (human.getChildren().isEmpty()) {
-                System.out.printf("%s (%d) не имеет детей!\n ", human.getFullName(), human.getBirthYear());
-            } else {
-                System.out.printf("%s (%d) имеет детей:\n %s\n", human.getFullName(), human.getBirthYear(), human.getChildren());
-            }
+        FamilyTree familyTree = new FamilyTree();
+        for (Human human : humans) {
+            familyTree.addHuman(human);
         }
 
-        System.out.println(familyTree.getHumanByName("Кузнецов Иван Иванович"));
-
+        System.out.println(familyTree);
+        familyTree.printChildren();
     }
 }
