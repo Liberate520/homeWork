a542f2f5626654fea7fcd4074a6b7288680b758d
Pro100-Adm <kirow.alesha@yandex.ru>
2023-03-31 23:43:47 +0300

    Kirov Alexey Dmitrievitch homework 30.03.2023

--- src/Person.java
+++ src/Person.java
@@ -4,12 +4,17 @@ import java.util.*;
 public class Person implements Serializable {
     private String fullName;
     private Calendar dateOfBirth;
+    private Calendar dateOfDeath;
     private Gender gender;
 
-    public Person(String fullName, Calendar dateOfBirth, Gender gender) {
+    public Person(String fullName, Calendar dateOfBirth, Calendar dateOfDeath, Gender gender) {
         this.fullName = fullName;
         this.dateOfBirth = dateOfBirth;
         this.gender = gender;
+        this.dateOfDeath = dateOfDeath;
+    }
+    public Person(String fullName, Calendar dateOfBirth, Gender gender) {
+        this(fullName, dateOfBirth, null, gender);
     }
 
     public String getFullName() {
@@ -31,6 +36,18 @@ public class Person implements Serializable {
 
     public Gender getGender() {
         return gender;
+    }
+
+    public Calendar getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public void setDateOfDeath(Calendar dateOfDeath) {
+        this.dateOfDeath = dateOfDeath;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
     }
 
     @Override
