83f2cc6fe3f206bc2a3dc0043c7f06f3f170329e
Eugene_Pro <proskuryakov.e@list.ru>
2023-04-01 08:16:33 +0300

    created the save(), load() methods in the interface, override in FileHandler

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,2 +1,27 @@
-public class FileHandler {
+import javax.imageio.IIOException;
+import java.io.*;
+
+public class FileHandler implements Writable {
+    @Override
+    public void save(Writable serializable) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream("out.txt");
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(serializable);
+        }
+        catch (IIOException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    @Override
+    public Writable load() throws ClassNotFoundException, InvalidObjectException {
+        try (FileInputStream fis = new FileInputStream("out.txt");
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            Writable object = (Writable) ois.readObject();
+            return object;
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
+        throw new InvalidObjectException("Object fail");
+    }
 }
