c9894616238472d7bb37aa8672a7a1ac372a4c3f
Nikita Balenko <nikitabalenko2013@gmail.com>
2023-04-01 16:53:52 +0300

    Переделал класc FamilyTree

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -1,90 +1,45 @@
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 
 public class FamilyTree {
-    private HashMap<Parent, HashSet<Children>> tree;
+    private LinkedHashMap<HashSet<Parent>, HashSet<Children>> tree;
 
     FamilyTree() {
-        this.tree = new HashMap<>();
+        this.tree = new LinkedHashMap<HashSet<Parent>, HashSet<Children>>();
     }
 
-    public void put(Parent m, Parent f, Children child) {
-        put(f, child);
-        put(m, child);
+    public void put(Parent p1, Parent p2, Children child) {
+        p1.addChildrens(child);
+        p2.addChildrens(child);
+        child.addParent(p1, p2);
+
+        HashSet<Parent> parenst = new HashSet<Parent>();
+        parenst.add(p1);
+        parenst.add(p2);
+        HashSet<Children> c = new HashSet<Children>();
+        if (this.tree.containsKey(parenst) == false) this.tree.put(parenst, c);
+        this.tree.get(parenst).add(child);
     }
 
-    
-    public void put(Human m, Human f, Human child) {
-        Parent p1 = new Parent(m);
-        Parent p2 = new Parent(f);
-        put(p1, p2, new Children(child, p1, p2));
-    }
-    
-    public void put(Parent p, Children child) {
-        p.addChildrens(child);
-        child.addParent(p);
-        this.tree.put(p, p.getChildrens());
-    }
-
-    public String getParentChild(Human p) {
-        StringBuilder result = new StringBuilder();
-        result.append(p.toString());
-        HashSet<Children> child = new HashSet<>();
-
-
-        return result.toString();
-    }
-
-    public String getChildren(Parent p) {
-        ArrayList<Children> child = new ArrayList<>();
-        child.addAll(this.tree.get(p));
-        return child.toString(); 
+    public void put(Human p1, Human p2, Human child) {
+        Parent parent1 = new Parent(p1);
+        Parent parent2 = new Parent(p2);
+        Children c = new Children(child, parent1, parent2);
+        put(parent1, parent2, c);
     }
 
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
 
-        for (Parent key : this.tree.keySet()) {
-            result.append(key.toString() + " - ");
-            for (Children child : this.tree.get(key)) result.append(child.toString() + ", ");
-            result.deleteCharAt(result.length() - 2);
-            result.append("\n");
+        for (HashSet<Parent> parents : this.tree.keySet()) {
+            result.append(parents + "\n");
+            for (Children children : this.tree.get(parents)) {
+                result.append("\t" + children + "\n");
+            }
         }
-        result.deleteCharAt(result.length() - 1);
-
 
         return result.toString();
     }
-
-
-
-
-
-
-
-
-    
-    // public void addChildren(Human child, Parent mather, Parent father) {
-    //     Children children = new Children(child, mather, father);
-    //     this.childrens.add(children);
-    //     father.addChildrens(children);
-    //     mather.addChildrens(children);
-    // }
-
-    // public void addChildren(Human child, Human m, Human f) {
-    //     Parent mather = new Parent(m);
-    //     Parent father = new Parent(f); 
-    //     Children children = new Children(child, mather, father);
-    //     this.childrens.add(children);
-    //     father.addChildrens(children);
-    //     mather.addChildrens(children);
-    //     addParent(father);
-    //     addParent(mather);
-    // }
-
-    
-
 
 }
