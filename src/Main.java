9f51a98b105b81c5c34b26d6d32e1a1e59d03687
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-04-01 17:49:29 +0300

    Solutin3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
+import file.OutInput;
+import file.Slfamailytree;
+import human.Human;
+import tree.FamilyTree;
+import tree.Sorts;
+import tree.Tree;
+
 /**
  * Main
  */
@@ -9,12 +16,12 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
         Scanner scan = new Scanner(System.in);
-        Human human1 = new Human("Екатерина", "ж");
-        Human human2 = new Human("Галина", "ж");
-        Human human3 = new Human("Владимир", "м");
-        Human human4 = new Human("Иван", "м");
-        Human human5 = new Human("Евфалия", "ж");
-        Human human6 = new Human("Дарья", "ж");
+        Human human1 = new Human("Екатерина", "ж", 1974);
+        Human human2 = new Human("Галина", "ж", 1941);
+        Human human3 = new Human("Владимир", "м", 1935);
+        Human human4 = new Human("Иван", "м", 1905);
+        Human human5 = new Human("Евфалия", "ж", 1907);
+        Human human6 = new Human("Дарья", "ж", 1976);
 
         human1.setMother(human2);
         human1.setFather(human3);
@@ -23,7 +30,7 @@ public class Main {
         human2.setMother(human5);
         human3.setFather(human4);
 
-        FamilyTree famailytree = new FamilyTree();
+        Tree famailytree = new FamilyTree();
 
         famailytree.addpeople(human1, human2, human3);
         famailytree.addpeople(human2, human5, null);
@@ -33,9 +40,37 @@ public class Main {
         famailytree.addpeople(human5, null, null);
         System.out.println(famailytree);
 
+        for(Human human: famailytree){
+            System.out.println(human);
+        }
+
+        Sorts sorts = new Sorts(famailytree);
+        sorts.SortbyName();
+        System.out.println();
+        for(Human human: famailytree){
+            System.out.println(human);
+        }
+
+        sorts.SortbyId();
+        System.out.println();
+        for(Human human: famailytree){
+            System.out.println(human);
+        }
+
+        sorts.SortbyChild();
+        System.out.println();
+        for(Human human: famailytree){
+            System.out.println(human);
+        }
+
+        sorts.SortbyBirthyear();
+        System.out.println();
+        for(Human human: famailytree){
+            System.out.println(human);
+        }
         System.out.println("Введите имя человека, которого нужно найти: ");
         String name1 = scan.nextLine();
-        Human humanf = new Human("1", "ж");
+        Human humanf = new Human("1", "ж", 1900);
         humanf = famailytree.findHuman(name1, humanf);
 
         if (humanf.getName().equals("1")) {
@@ -47,7 +82,7 @@ public class Main {
 
         OutInput saveload = new Slfamailytree();
         saveload.save(famailytree);
-        famailytree = (FamilyTree) saveload.load();
-        System.out.println(famailytree);
+        famailytree = (Tree) saveload.load();
+        System.out.println(famailytree);  
     }
 }
--- src/OutInput.java
+++
@@ -1,8 +0,0 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-public interface OutInput {
-    void save(FamilyTree ft) throws FileNotFoundException, IOException;
-    Object load() throws ClassNotFoundException, IOException;
-}
-
