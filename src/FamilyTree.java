efce395a66eaeeececdc677619c237d72dc52531
theRiven26 <demize26rus@gmail.com>
2023-04-02 23:11:09 +0300

    Update FamilyTree.java

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -1,9 +1,8 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-
-public class FamilyTree {
-    List<Human> familyTree;
-
+public class FamilyTree implements FileProcessing, Serializable{
+   private List<Human> familyTree;
     public FamilyTree(List<Human> familyTree){
         this.familyTree = familyTree;
     }
@@ -12,8 +11,29 @@ public class FamilyTree {
         this(new ArrayList<>());
     }
 
-    public void AddFamilyTree( Human human){
-        this.familyTree.add(human);
+    public void addFamilyTree( Human human) {
+        if (!familyTree.contains(human)) {
+            if (human.getMother() != null) {
+                human.getMother().addChild(human);
+            }
+            if (human.getFather() != null) {
+                human.getFather().addChild(human);
+            }
+            this.familyTree.add(human);
+        }
     }
 
+    @Override
+    public void read(String fileAddress) throws IOException, ClassNotFoundException {
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileAddress));
+        FamilyTree tree =  (FamilyTree)in.readObject();
+        familyTree = tree.familyTree;
+    }
+
+    @Override
+    public void save(String fileAddress) throws IOException  {
+        ObjectOutputStream out  = new ObjectOutputStream( new FileOutputStream(fileAddress));
+        out.writeObject(this);
+        out.close();
+    }
 }
