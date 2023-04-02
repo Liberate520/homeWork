f805000389d8adbec00a07571292466f0133d01d
udav516 <eldar.udav516@yandex.ru>
2023-04-02 17:47:19 +0300

    Hw1

---
+++ src/tree/FamilyTree.java
@@ -0,0 +1,31 @@
+package tree;
+
+import java.util.ArrayList;
+
+public class FamilyTree {
+    private final ArrayList<Human> famTree = new ArrayList<>();
+
+    public void addHuman(Human human) {
+        famTree.add(human);
+        if (human.getMother() != null)
+            human.getMother().addChild(human);
+
+        if (human.getFather() != null)
+            human.getFather().addChild(human);
+    }
+
+    public String findHuman(String name) {
+        for (Human human : famTree)
+            if (human.getName().contains(name)) {
+                return human.toString();
+            }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return famTree.toString();
+    }
+
+
+}
---
+++ src/tree/Human.java
@@ -0,0 +1,105 @@
+package tree;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Human {
+    private String name;
+    private Gender gender;
+    private LocalDate birthDate;
+    private LocalDate deathDate;
+    private Human mother;
+    private Human father;
+    private List<Human> children;
+
+    public Human(String name, Gender gender, LocalDate birthDate, LocalDate deathDate, Human mother, Human father, List<Human> children) {
+        this.name = name;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.deathDate = deathDate;
+        this.mother = mother;
+        this.father = father;
+        this.children = children;
+    }
+
+    public Human(String name, Gender gender, LocalDate birthDate) {
+        this(name, gender, birthDate, null, null, null, new ArrayList<>());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public LocalDate getDeathDate() {
+        return deathDate;
+    }
+
+    public void setDeathDate(LocalDate deathDate) {
+        this.deathDate = deathDate;
+    }
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public void setMother(Human mother) {
+        this.mother = mother;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public void setFather(Human father) {
+        this.father = father;
+    }
+
+    public List<Human> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Human> children) {
+        this.children = children;
+    }
+
+    public void addChild(Human human) {
+        children.add(human);
+    }
+
+    @Override
+    public String toString() {
+        return "\nHuman{" +
+                "name ='" + name + '\'' +
+                ", gender = " + gender +
+                ", birthDate = " + birthDate +
+                ", deathDate = " + deathDate +
+                ", children = " + children +
+                '}';
+    }
+
+
+    public enum Gender {
+        male, female
+    }
+}
