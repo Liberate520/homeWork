a5119724d33a447489c9d0f146517907a2df915a
theRiven26 <demize26rus@gmail.com>
2023-04-05 22:57:24 +0300

    Init commit

--- src/FileProcessing.java
+++
@@ -1,6 +0,0 @@
-import java.io.IOException;
-
-public interface FileProcessing  {
-    void save(String fileAddress) throws IOException;
-    void read(String fileAddress) throws IOException, ClassNotFoundException;
-}
