c0556324b668e30ef129e938bafccf293094b93d
Dima <d.nikulin275@gmail.com>
2023-04-05 17:02:56 +0300

    Homework 2

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -1,17 +1,64 @@
+import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
-public class FamilyTree {
-    List<Human> family = new ArrayList<>();
 
-    public void printFamily() {
-        System.out.println("Члены семьи");
-        for (Human human : family) {
-            System.out.println(human + "\n");
-        }
+public class FamilyTree implements Serializable {
+    private ArrayList<FamilyTreeMemeber> memebers;
+
+    FamilyTree() {
+        this.memebers = new ArrayList<FamilyTreeMemeber>();
     }
 
-    public void addHuman(Human human) {
-        family.add(human);
+    public int total() {
+        return memebers.size();
+    }
+
+    public FamilyTreeMemeber getFirstMemeber() {
+        return memebers.get(0);
+    }
+
+    public FamilyTreeMemeber getMemeberById(int memeberId) {
+        return memebers.get(memeberId);
+    }
+
+    public FamilyTreeMemeber[] getAllFamilyMemebers() {
+        return memebers.toArray(new FamilyTreeMemeber[0]);
+    }
+
+    public FamilyTreeMemeber addChild(String name, String sex, Date birthDay, int[] parentIds) {
+        FamilyTreeMemeber[] parents = parentIds != null
+                ? new FamilyTreeMemeber[] { memebers.get(parentIds[0]), memebers.get(parentIds[1]) }
+                : null;
+        var child = new FamilyTreeMemeber(memebers.size(), name, sex, birthDay, parents);
+        memebers.add(child);
+        return child;
+    }
+
+    public FamilyTreeMemeber addChild(String name, String sex, Date birthDay) {
+        return addChild(name, sex, birthDay, null);
+    }
+
+    public FamilyTreeMemeber addSpouse(String name, Date birthDay, int memberId) throws Exception {
+        if (memberId >= memebers.size())
+            throw new Exception("Invalid memberId");
+        FamilyTreeMemeber member = memebers.get(memberId);
+        String sex = member.sex() == "мужской" ? "женский" : "мужской";
+        var spouse = new FamilyTreeMemeber(memebers.size(), name, sex, birthDay, null);
+        member.spouse(spouse);
+        memebers.add(spouse);
+        return spouse;
+    }
+
+    @Override
+    public String toString() {
+        var str = new StringBuilder();
+        for (FamilyTreeMemeber memeber : memebers)
+            str.append("\n" + memeber.toString());
+        return str.toString();
+    }
+
+    public boolean save(IFamilyTreeSaver saver) {
+        return saver.save(this);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,57 @@
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner sc;
     public static void main(String[] args) {
-        Human human1 = new Human("Олег", Gender.Male, 1970);
-        Human human2 = new Human("Ольга", Gender.Female, 1973);
-        Human human3 = new Human("Петя", Gender.Male, 1999);
-        Human human4 = new Human("Света", Gender.Female, 2018);
-        Human human5 = new Human("Илья", Gender.Male, 2011);
+        try {
+            sc = new Scanner(System.in);
+            FamilyTree familyTree = createFamilyTree();
+            System.out.printf("Генеалогическое древо состоит из %d человек\n" +
+                    "  Введите ID человека (цифра от 1 до %d) для просмотра информации о нем\n" +
+                    "  Введите 0 для просмотра информации обо всех\n" +
+                    "  Enter - выход\n", familyTree.total(), familyTree.total());
+            while (true) {
+                String input = sc.nextLine();
+                int num = Integer.parseInt(input) - 1;
+                if (num < 0) {
+                    System.out.println(familyTree);
+                } else if (num >= familyTree.total()) {
+                    System.out.println("Неверный ID");
+                } else {
+                    FamilyTreeMemeber memeber = familyTree.getMemeberById(num);
+                    System.out.println(memeber);
+                }
+            }
+        } catch (NumberFormatException e) {
+        } catch (Exception e) {
+            System.out.printf("Ошибка: %s", e.getMessage());
+        } finally {
+            if (sc != null)
+                sc.close();
+        }
 
-        human3.addFather(human1);
-        human3.addMother(human2);
-        human4.addFather(human1);
-        human4.addMother(human2);
+    }
 
-        FamilyTree tree = new FamilyTree();
-        tree.addHuman(human1);
-        tree.addHuman(human2);
-        tree.addHuman(human3);
-        tree.addHuman(human4);
+    private static FamilyTree createFamilyTree() throws Exception {
+        var serializer = new FamilyTreeSerializer();
+        FamilyTree familyTree = serializer.load();
+        if (familyTree != null)
+            return familyTree;
+        familyTree = new FamilyTree();
+        int[] ids = new int[2];
+        ids[0] = familyTree.addChild("Николай Лысенко", "мужской", date(5, 2, 40)).id();
+        ids[1] = familyTree.addSpouse("Светлана Петрова", date(20, 10, 45), ids[0]).id();
+        ids[0] = familyTree.addChild("Роман Лысенко", "мужской", date(4, 7, 65), ids).id();
+        ids[1] = familyTree.addSpouse("Мария Федорова", date(6, 3, 72), ids[0]).id();
+        ids[0] = familyTree.addChild("Иван Лысенко", "мужской", date(14, 8, 95), ids).id();
+        ids[1] = familyTree.addSpouse("Анна Владимирова", date(26, 6, 99), ids[0]).id();
+        ids[0] = familyTree.addChild("Ольга Лысенко", "женский", date(30, 9, 120), ids).id();
+        familyTree.save(serializer);
+        return familyTree;
+    }
 
-        tree.printFamily();
+    private static Date date(int day, int month, int year) {
+        return new Date(year, month, day);
     }
 }
