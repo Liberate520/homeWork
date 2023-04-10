86ee1fcb6353d17ecf64700bb3a7376fb7e048f5
dmitriy <machortovdima@mail.ru>
2023-04-10 19:59:48 +0300

    added project_hw4

--- src/Main.java
+++ src/Main.java
@@ -12,50 +12,50 @@ public class Main {
         public static void main(String[] args) {
                 Groupable<Person> familyTree= new FamilyTree<>();
 
-                familyTree.addPerson(new Person(1, "Сергеева Светлана", LocalDate.of(1934, 5, 21),
-                                LocalDate.of(2004, 7, 17),
-                                Gender.female, null, null, null));
+                // familyTree.addPerson(new Person(1, "Сергеева Светлана", LocalDate.of(1934, 5, 21),
+                //                 LocalDate.of(2004, 7, 17),
+                //                 Gender.female, null, null, null));
 
-                familyTree.addPerson(new Person(2, "Иванова Анастасия", LocalDate.of(1959, 9, 14),
-                                null, Gender.female, familyTree.getPersonByName("Сергеева Светлана"), null,
-                                null));
+                // familyTree.addPerson(new Person(2, "Иванова Анастасия", LocalDate.of(1959, 9, 14),
+                //                 null, Gender.female, familyTree.getPersonByName("Сергеева Светлана"), null,
+                //                 null));
 
-                familyTree.addPerson(new Person(3, "Иванов Петр", LocalDate.of(1956, 4, 11),
-                                null, Gender.male, null, null,
-                                null));
+                // familyTree.addPerson(new Person(3, "Иванов Петр", LocalDate.of(1956, 4, 11),
+                //                 null, Gender.male, null, null,
+                //                 null));
 
-                familyTree.addPerson(new Person(4, "Иванова Александра", LocalDate.of(1982, 3, 22),
-                                null, Gender.female, familyTree.getPersonByName("Иванова Анастасия"),
-                                familyTree.getPersonByName("Иванов Петр"),
-                                null));
+                // familyTree.addPerson(new Person(4, "Иванова Александра", LocalDate.of(1982, 3, 22),
+                //                 null, Gender.female, familyTree.getPersonByName("Иванова Анастасия"),
+                //                 familyTree.getPersonByName("Иванов Петр"),
+                //                 null));
 
-                familyTree.addPerson(new Person(5, "Иванов Иван", LocalDate.of(1986, 11, 16),
-                                null, Gender.male, familyTree.getPersonByName("Иванова Анастасия"),
-                                familyTree.getPersonByName("Иванов Петр"),
-                                null));
+                // familyTree.addPerson(new Person(5, "Иванов Иван", LocalDate.of(1986, 11, 16),
+                //                 null, Gender.male, familyTree.getPersonByName("Иванова Анастасия"),
+                //                 familyTree.getPersonByName("Иванов Петр"),
+                //                 null));
 
-                familyTree.addPerson(new Person(6, "Петров Владимир", LocalDate.of(1979, 6, 12),
-                                null, Gender.male, null, null,
-                                null));
+                // familyTree.addPerson(new Person(6, "Петров Владимир", LocalDate.of(1979, 6, 12),
+                //                 null, Gender.male, null, null,
+                //                 null));
 
-                familyTree.addPerson(new Person(7, "Петров Игорь", LocalDate.of(2005, 8,
-                                12), null, Gender.male, familyTree.getPersonByName("Иванова Александра"),
-                                familyTree.getPersonByName("Петров Владимир"),
-                                null));
-                familyTree.addPerson(new Person(8, "Петрова Людмила", LocalDate.of(2009, 12, 19),
-                                null, Gender.female, familyTree.getPersonByName("Иванова Александра"),
-                                familyTree.getPersonByName("Петров Владимир"),
-                                null));
+                // familyTree.addPerson(new Person(7, "Петров Игорь", LocalDate.of(2005, 8,
+                //                 12), null, Gender.male, familyTree.getPersonByName("Иванова Александра"),
+                //                 familyTree.getPersonByName("Петров Владимир"),
+                //                 null));
+                // familyTree.addPerson(new Person(8, "Петрова Людмила", LocalDate.of(2009, 12, 19),
+                //                 null, Gender.female, familyTree.getPersonByName("Иванова Александра"),
+                //                 familyTree.getPersonByName("Петров Владимир"),
+                //                 null));
 
                 String filenameOS = "FamilyTree.dat";
-                String filenameTXT = "FamilyTree.txt";
+                // String filenameTXT = "FamilyTree.txt";
                  
                 Writeable<Person> fileOS = new FilehandlerOS<>();
-                Writeable<Person> fileTXT = new FilehandlerTXT<>();
+                // Writeable<Person> fileTXT = new FilehandlerTXT<>();
 
 
-                fileOS.saveToFile(familyTree, filenameOS);
-                fileTXT.saveToFile(familyTree, filenameTXT);
+                // fileOS.saveToFile(familyTree, filenameOS);
+                // fileTXT.saveToFile(familyTree, filenameTXT);
                 familyTree= fileOS.LoadFromFile(filenameOS);
 
                 for (Person person : familyTree.getPersonList()) {
