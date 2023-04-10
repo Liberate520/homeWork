c63b4e6d2caf7a74b0fd75258e3dc1e4475a27bb
theRiven26 <demize26rus@gmail.com>
2023-04-10 21:51:13 +0300

    Delete src directory

--- src/FileProcessing.java
+++
@@ -1,6 +0,0 @@
-import java.io.IOException;
-
-public interface FileProcessing  {
-    void save(FamilyTree familyTree, String fileAddress) throws IOException;
-    FamilyTree read(String fileAddress) throws IOException, ClassNotFoundException;
-}
