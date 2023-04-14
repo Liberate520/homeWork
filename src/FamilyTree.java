47a146492f4dac98b2ad3b546d25ec9eb7156517
krasnoperov <runyugin@mail.ru>
2023-04-14 13:20:33 +0300

    homework_1_done

---
+++ src/FamilyTree.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+
+    private List<Human> tree;
+
+    public FamilyTree(List<Human> tree){
+        this.tree = tree;
+    }
+
+    public FamilyTree(){
+        this(new ArrayList<>());
+    }
+
+    public void addHuman(Human human) {
+        tree.add(human);
+    }
+
+    @Override
+    public String toString() {
+        return tree.toString();
+    }
+    
+}
