fb2b62c384c4be5d304cd815e981ecd03ebc2b96
Rooman255 <116148967+Rooman255@users.noreply.github.com>
2023-04-15 11:11:20 +0300

    Add files via upload
    DZ4

---
+++ src/Writable.java
@@ -0,0 +1,10 @@
+
+
+import java.io.Serializable;
+
+public interface Writable {
+    static final long serialVersionUID = 6529685098267757699L;
+    boolean save(Serializable serializable, String filename);
+
+    Object load(String filename);
+}
