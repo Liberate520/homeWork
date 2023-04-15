c9a864d2943d3f5d82fbf5433e1891f2885e7f0e
maxim <makfedorovtooo@gmail.com>
2023-04-15 23:56:13 +0300

    fix name

--- src/Home/Main.java
+++ src/Home/Main.java
@@ -6,41 +6,41 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        FamilyTree Igor = new FamilyTree("Igor",35);
-        FamilyTree Lida = new FamilyTree("Lida",35);
-        FamilyTree Max = new FamilyTree("Max",15);
-        FamilyTree Irina = new FamilyTree("Irina", 10);
-        FamilyTree Victoriya = new FamilyTree("Victoriya",65);
-        FamilyTree Vyacceslav = new FamilyTree("Vyacceslav",65);
-        FamilyTree Sergey = new FamilyTree("Sergey",65);
-        FamilyTree Liza = new FamilyTree("Liza",65);
+        FamilyTree igor = new FamilyTree("Igor",35);
+        FamilyTree lida = new FamilyTree("Lida",35);
+        FamilyTree max = new FamilyTree("Max",15);
+        FamilyTree irina = new FamilyTree("Irina", 10);
+        FamilyTree victoriya = new FamilyTree("Victoriya",65);
+        FamilyTree vyacceslav = new FamilyTree("Vyacceslav",65);
+        FamilyTree sergey = new FamilyTree("Sergey",65);
+        FamilyTree liza = new FamilyTree("Liza",65);
         List<Human> latest_generation = new ArrayList<>();
         List<Human> latest_generation_sisters = new ArrayList<>();
         List<Human> latest_generation_brothers = new ArrayList<>();
-        Lida.setMother(Victoriya);
-        Lida.setFather(Vyacceslav);
-        Igor.setFather(Sergey);
-        Igor.setMother(Liza);
-        Max.setFather(Igor);
-        Max.setMother(Lida);
-        Irina.setMother(Lida);
-        Irina.setFather(Igor);
-        latest_generation.add(Max);
-        latest_generation_brothers.add(Max);
-        Max.setSisters(latest_generation_sisters);
-        Irina.setBrothers(latest_generation_brothers);
-        latest_generation.add(Irina);
-        latest_generation_sisters.add(Irina);
-        Igor.setChildrens(latest_generation);
+        lida.setMother(victoriya);
+        lida.setFather(vyacceslav);
+        igor.setFather(sergey);
+        igor.setMother(liza);
+        max.setFather(igor);
+        max.setMother(lida);
+        irina.setMother(lida);
+        irina.setFather(igor);
+        latest_generation.add(max);
+        latest_generation_brothers.add(max);
+        max.setSisters(latest_generation_sisters);
+        irina.setBrothers(latest_generation_brothers);
+        latest_generation.add(irina);
+        latest_generation_sisters.add(irina);
+        igor.setChildrens(latest_generation);
 
-//        System.out.println(Irina.getFatherDetails());
-//        System.out.println(Irina.getMotherDetails());
-//        System.out.println(Irina.getSisterDetails());
-//        System.out.println(Irina.getBrothersDetails());
-        System.out.println(Max.getGrandmatherDetails());
-//        System.out.println(Igor);
-//        System.out.println(Igor.getChildrenDetails());
-//        System.out.println(Lida.getName());
+//        System.out.println(irina.getFatherDetails());
+//        System.out.println(irina.getMotherDetails());
+//        System.out.println(irina.getSisterDetails());
+//        System.out.println(irina.getBrothersDetails());
+//        System.out.println(lida.getGrandfatherDetails());
+        System.out.println(max);
+//        System.out.println(igor.getChildrenDetails());
+//        System.out.println(lida.getName());
 
     }
 }
