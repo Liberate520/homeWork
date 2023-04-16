3e823fec9f02db71ea80a561907e84f9762b2c45
mexatron <mexatron>
2023-04-16 21:39:15 +0200

    switch rebuild to string

--- src/view/Console.java
+++ src/view/Console.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Console implements View {
     Presenter presenter;
     Scanner scn = new Scanner(System.in);
-    int key;
+    String key;
     String nameFather;
     String nameMather;
     String name;
@@ -22,7 +22,7 @@ public class Console implements View {
 
     @Override
     public void start() {
-        while (key != 6) {
+        while (key != "6") {
             print(" Введите:\n " +
                     "0 - Вывести список членов семьи\n " +
                     "1 - Загрузить семью из файла\n " +
@@ -31,34 +31,35 @@ public class Console implements View {
                     "4 - Найти члена семьи по имени \n " +
                     "5 - Изменить данные члена семьи \n " +
                     "6 - Выйти");
-            key = scn.nextInt();
+            key = scn.next();
             switch (key) {
-                case 0:
+                case "0":
                     presenter.sortById();
                     print(presenter.getInfo());
                     break;
-                case 2:
+                case "2":
                     enterPersonData();
                     break;
-                case 1:
+                case "1":
                     presenter.load("family.txt");
                     break;
 
-                case 3:
+                case "3":
                     printChildren();
                     break;
-                case 4:
+                case "4":
                     findMember();
                     break;
-                case 5:
+                case "5":
                     editMember();
                     break;
-                case 6:
+                case "6":
                     presenter.save("family.txt");
                     print("Работа завершена");
-                    scn.close();
+                    key ="6";
                     break;
                 default:
+                    print("Такой команды нет");
                     break;
             }
         }
@@ -94,22 +95,25 @@ public class Console implements View {
                     "1 - Имя\n " +
                     "2 - Год рождения\n " +
                     "3 - Год смери\n ");
-            key = scn.nextInt();
+            key = scn.next();
             switch (key) {
-                case 1:
+                case "1":
                     print("Введите новое имя");
                     newName = scn.next();
                     presenter.editName(name, newName);
                     break;
-                case 2:
+                case "2":
                     print("Введите новый год рождения");
                     yearBirth = scn.nextInt();
                     presenter.editYearBirth(name, yearBirth);
                     break;
-                case 3:
+                case "3":
                     print("Введите новый год смерти");
                     yearDeath = scn.nextInt();
                     presenter.editYearDeath(name, yearDeath);
+                    break;
+                default:
+                    print("Такой команды нет");
                     break;
 
             }
