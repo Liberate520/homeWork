e607da6f5255bb72a4b34113ced977f7599fc4d4
Li <varlamova_e.t@mail.ru>
2023-04-17 12:33:14 +0300

    hjsd

--- src/Human.java
+++ src/Human.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Human {
     private String name;
-
+    private Human mother;
+    private Human father;
+    private List<Human> children = new ArrayList<>();
+    
     public Human(String name) {
         this.name = name;
+    }
+
+    public void addChild(Human human) {
+        children.add(human);
+    }
+
+    public void showChildren() {
+        System.out.println(children.toString());
     }
 
     public String toString() {
--- src/Program.java
+++ src/Program.java
@@ -2,6 +2,14 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("hi");
         FamilyTree tree = new FamilyTree();
+        Human h1 = new Human("Natalia");
+        tree.addMember(h1);
+        tree.showAllMembers();
         
+        Human h3 = new Human("Liza");
+        tree.addMember(h3);
+        tree.showAllMembers();
+        h1.addChild(h3);
+        h1.showChildren();
     }
 }
