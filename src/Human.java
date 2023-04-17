7f90095769a597aa19a0ab70fad418ad51be59db
YuriiAlekseev <ash030788@gmail.com>
2023-04-17 21:02:39 +0300

    доработка фунцтонала

--- src/Human.java
+++ src/Human.java
@@ -2,43 +2,60 @@ import java.util.List;
 
 public class Human {
     private Gender gender;
-    private String mather;
-    private String father;
-    private List<String> children;
-    private String firstName;
-    private String lastName;
+    private Human mather;
+    private Human father;
+    private List<Human> children;
+    private Human firstName;
+    private Human lastName;
 
-    public Human(Gender gender, String firstName, String lastName, String mather, String father ) {
+    public Human(Gender gender, Human firstName, Human lastName, Human mather, Human father, List<Human> children) {
         this.gender = gender;
         this.firstName = firstName;
         this.lastName = lastName;
         this.mather = mather;
         this.father = father;
-//        this.children.add(children);
+        this.children = children;
     }
-    public String getInformation(){
-        return gender + " " +lastName + " " + firstName + " " + mather + " " + father + " " + children;
+
+    public Human(Gender male, String firstName, String lastName, Object mather, Object father, List<Human> children1) {
+    }
+
+    public String getInformation() {
+        return toString();
     }
 
     public void setGender(Gender gender) {
         this.gender = gender;
     }
-    public void setFirstName(String firstName){
+
+    public void setFirstName(Human firstName) {
         this.firstName = firstName;
     }
-    public void setLastName(String lastName){
+
+    public void setLastName(Human lastName) {
         this.lastName = lastName;
     }
 
-    public void setMather(String mather) {
+    public void setMather(Human mather) {
         this.mather = mather;
     }
 
-    public void setFather(String father) {
+    public void setFather(Human father) {
         this.father = father;
     }
 
-    public void setChildren(String children) {
+    public void setChildren(Human children) {
         this.children.add(children);
     }
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        output.append("Name: " + this.firstName + " " + this.lastName + "\n");
+        output.append("Gender: " + this.gender + "\n");
+        output.append("Mather: " + this.mather + "\n");
+        output.append("Father: " + this.father + "\n");
+        output.append("children: " + this.children + "\n");
+        return output.toString();
+    }
+
 }
--- src/main.java
+++
@@ -1,6 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        Human human1 = new Human(Gender.Male,"Юрий","Алексеев", "Алекеева Валентина","Алексеев Николай");
-        System.out.println(human1.getInformation());
-    }
-}
