f1357b21cfa3cd631ed8b87bddc88680a13384ac
Li <varlamova_e.t@mail.ru>
2023-04-17 21:11:16 +0300

    child

--- src/Program.java
+++ src/Program.java
@@ -1,15 +1,22 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.println("hi");
         FamilyTree tree = new FamilyTree();
         Human h1 = new Human("Natalia");
+        Human h2 = new Human("Timur");
         tree.addMember(h1);
+        tree.addMember(h2);
         tree.showAllMembers();
         
         Human h3 = new Human("Liza");
         tree.addMember(h3);
+        Human h4 = new Human("Lenya");
+        tree.addMember(h4);
         tree.showAllMembers();
         h1.addChild(h3);
+        h1.addChild(h4);
+        h2.addChild(h3);
+        h2.addChild(h4);
         h1.showChildren();
+        h2.showChildren();
     }
 }
