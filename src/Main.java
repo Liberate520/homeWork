4d82f590087da2c1ca1a1a3055c75050d4a636e1
Iron Skull <ironskull89@mail.ru>
2023-04-19 18:15:50 +0300

    homework2 implement seriazable

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,23 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-//        Kinsman firstKinsman = new Kinsman("Рюрик", Sex.men);
-//        System.out.println(firstKinsman);
-//        Kinsman secondKinsman = new Kinsman("Анна", Sex.women);
-//        FamilyTree familyTree = new FamilyTree("Рюрики", firstKinsman);
-//        System.out.println(familyTree);
-//        familyTree.printFamily();
-//        familyTree.addParent(firstKinsman, secondKinsman);
-//        familyTree.printFamily();
-//        secondKinsman.printScions();
-//
-//        //Сериализация в файл с помощью класса ObjectOutputStream
-//        ObjectOutputStream objectOutputStream = new ObjectOutputStream(
-//                new FileOutputStream("familyTree.out"));
-//        objectOutputStream.writeObject(familyTree);
-//        objectOutputStream.writeObject(firstKinsman);
-//        objectOutputStream.writeObject(secondKinsman);
-//        objectOutputStream.close();
+        Kinsman firstKinsman = new Kinsman("Рюрик", Sex.men);
+        System.out.println(firstKinsman);
+        Kinsman secondKinsman = new Kinsman("Анна", Sex.women);
+        FamilyTree familyTree = new FamilyTree("Рюрики", firstKinsman);
+        System.out.println(familyTree);
+        familyTree.printFamily();
+        familyTree.addParent(firstKinsman, secondKinsman);
+        familyTree.printFamily();
+        secondKinsman.printScions();
+
+        //Сериализация в файл с помощью класса ObjectOutputStream
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+                new FileOutputStream("familyTree.out"));
+        objectOutputStream.writeObject(familyTree);
+        objectOutputStream.writeObject(firstKinsman);
+        objectOutputStream.writeObject(secondKinsman);
+        objectOutputStream.close();
 
         // Востановление из файла с помощью класса ObjectInputStream
         ObjectInputStream objectInputStream = new ObjectInputStream(
