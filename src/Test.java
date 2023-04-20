98bca8d8e5a2be9d952bc1fb6ff6b106612910b8
miku-death <yaneadolfik@gmail.com>
2023-04-20 13:35:08 +0900

    homework 1

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,16 @@
 public class Test {
+    public static void main(String[] args)
+    {
+        Human h1 = new Human("ivan", "karp", 1980, Gender.Male);
+        Human h2 = new Human("galina", "ivanova", 1983, Gender.Female);
+
+        h1.addChild("vasiliy", 1999, Gender.Male, h2);
+        h2.addChild("valentina", 2003, Gender.Female, h1);
+
+        Tree tree = new Tree(h1);
+
+        tree.getChildren();
+        tree.nextHuman(0);
+        tree.nextHuman(5);
+    }
 }
