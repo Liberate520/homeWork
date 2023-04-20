cb4181546000d8ef1e87029638f5ad0cd2506944
miku-death <yaneadolfik@gmail.com>
2023-04-20 13:38:22 +0900

    homework 1

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args)
     {
         Human h1 = new Human("ivan", "karp", 1980, Gender.Male);
-        Human h2 = new Human("galina", "ivanova", 1983, Gender.Female);
+        Human h2 = new Human("galina", "schuka", 1983, Gender.Female);
 
         h1.addChild("vasiliy", 1999, Gender.Male, h2);
         h2.addChild("valentina", 2003, Gender.Female, h1);
