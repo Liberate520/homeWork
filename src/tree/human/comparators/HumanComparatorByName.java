ff071725667c1135e16d7759634c50e9a0eb586f
Nikolay Tikhomirov <79159489230@yandex.ru>
2023-04-24 21:35:29 +0300

    little fixing

---
+++ src/tree/human/comparators/HumanComparatorByName.java
@@ -0,0 +1,13 @@
+package tree.human.comparators;
+
+import java.util.Comparator;
+
+import tree.human.Human;
+
+public class HumanComparatorByName implements Comparator<Human>{
+
+    @Override
+    public int compare(Human o1, Human o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+}
