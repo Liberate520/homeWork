a3ab30edb7c1f0751f4ab0cec1c470c3a9c4e786
Anna <annetka.kruz@gmail.com>
2023-04-26 21:27:01 +0500

    correct file Human

--- src/Human.java
+++ src/Human.java
@@ -69,7 +69,7 @@ public class Human implements Serializable {
 
     public String getInfoChildren(List<Human> children) {
         if (children == null) {
-            return "нет";
+            return "not";
         }
         int count = children.size();
         if (count > 0) {
@@ -80,24 +80,60 @@ public class Human implements Serializable {
             result += children.get(children.size() - 1).getName();
             return result.replaceAll(", $", "");
         }
-        return "нет";
+        return "not";
     }
     
 
     @Override
     public String toString() {
-        return name + " " + surname + " Дата рождения: " + dateOfBirth + " Пол: " + gender +
-                 " Информация о родителях:  Отец: " + father + " Мать: " + mother +
-                 " Информация о детях:  " + getInfoChildren(children);
+        return getInfo();
+        // name + " " + surname + " Дата рождения: " + dateOfBirth + " Пол: " + gender +
+        //          " Информация о родителях:  Отец: " + father + " Мать: " + mother +
+        //          " Информация о детях:  " + getInfoChildren(children);
     }
     
     public void addChildren(Human human) {
-         if (this.children == null) {
-             this.children = new ArrayList<>();
-             this.children.add(human);
-         }
+        if (this.children == null) {
+            this.children = new ArrayList<>();
+            this.children.add(human);
+        }
         this.children.size();
     }
+
+    public String getMotherInfo() {
+        String res = "mather: ";
+        if (mother != null) {
+            res += mother.getName();
+        } else {
+            res += "unknown";
+        }
+        return res;
+    }
     
+    public String getFatherInfo() {
+        String res = "father: ";
+        if (father != null) {
+            res += father.getName();
+        } else {
+            res += "unknown";
+        }
+        return res;
+    }
+
+    public String getInfo() {
+        StringBuilder info = new StringBuilder();
+        info.append("name: ");
+        info.append(name);
+        info.append(", ");
+        info.append(getFatherInfo());
+        info.append(", ");
+        info.append(getMotherInfo());
+        info.append(", children: ");
+        info.append(getInfoChildren(children));
+        info.append(". ");
+        return info.toString();
+    }
+
+
 
 }
