fd792579e090e8b1e1fb1ef124f1904181def067
jonmd87 <jonmd87@yandex.com>
2023-04-28 19:20:16 +0200

    done

--- src/Test.java
+++ src/Test.java
@@ -1,28 +1,12 @@
 public class Test {
     public static void main(String[] args) {
-        Human grandMa = new Human("GrandMa", "GrandMa", 1960, Gender.Male);
-        Human father = new Human("Ivan", "Ivanov", 1980, Gender.Male);
-        Human mother = new Human("Irina", "Irinova", 1984, Gender.Female);
-        mother.setMother(grandMa);
-
         FamilyTree familyTree = new FamilyTree();
-        familyTree.addHuman(mother);
-        familyTree.addHuman(father);
-        father.setPartner(mother);
-
-        for (int i = 0; i < 3; i++) {
-            Human child = new Human("child" + (i + 1), "child" + (i + 1),
-                    2005 + i, i % 2 == 0 ? Gender.Male : Gender.Female);
-            child.setFather(father);
-            child.setMother(mother);
-            familyTree.addHuman(child);
-        }
-        father.addChild(new Human("fatherChild", "fatherChild", 2008, Gender.Male));
-
         String path = "/home/evgen/prog/homeWork/familyTree.txt";
         FileWriteRead fileWriteRead = new FileWriteRead();
-        fileWriteRead.write(familyTree, path);
+//        fileWriteRead.write(familyTree, path);
         familyTree = fileWriteRead.read(path);
+
+
 
         System.out.println(familyTree.getFamilyInfo());
     }
