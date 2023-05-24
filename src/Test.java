fc6c3c4d7ae000d11889d4076b04d52ded8f52dc
miku-death <yaneadolfik@gmail.com>
2023-05-24 18:17:09 +0900

    homework 5

--- src/Test.java
+++ src/Test.java
@@ -4,38 +4,22 @@ import java.util.List;
 public class Test {
     public static void main(String[] args)
     {
-        Human h1 = new Human("m1", 1980, Gender.Male);
-        Human h2 = new Human("f1", 1985, Gender.Male);
-
-        Human c1 = new Human("m1c1", 2002, Gender.Male);
-        Human c2 = new Human("m1c2", 2003, Gender.Male);
-        Human c3 = new Human("m1c3", 2005, Gender.Female);
-        Human c4 = new Human("m1c4", 2001, Gender.Female);
-
-        h1.AddChild(c1);
-        h1.AddChild(c2);
-        h1.AddChild(c3);
-        h1.AddChild(c4);
-
-        h2.AddChild(c2);
-        h2.AddChild(c3);
-        h2.AddChild(c4);
+        Presenter presenter = new Presenter();
 
         List<Human> people = new ArrayList<>();
-        people.add(h1);
-        people.add(h2);
-        people.add(c1);
-        people.add(c2);
-        people.add(c3);
-        people.add(c4);
+        people.add(presenter.createHuman("m1", 1, Gender.Male));
+        people.add(presenter.createHuman("f1", 1, Gender.Female));
+        people.add(presenter.createHuman("m1c1", 1, Gender.Male));
+        people.add(presenter.createHuman("f1c2", 1, Gender.Female));
 
-        ParamTree<Human> tree = new ParamTree<Human>(people);
-        
-        for (Human h : tree) {
-            System.out.println(h);
+        for (Human human : people) {
+            System.out.println(presenter.humanToString(human));
         }
 
-        System.out.println(tree.sortedByBirthYear());
-        System.out.println(tree.sortedByName());
+        for (var item : presenter.createParamTree(people)) {
+            System.out.println(item.toString());
+        }
+
+        presenter.saveTree("out", presenter.createTree(people));
     }
 }
