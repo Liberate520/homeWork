7e8f6039c18917f9fb6d16fce3f3288be2cd2180
Alexershov86 <alexershov86@mail.ru>
2023-05-27 22:22:01 +0300

    homeWork1 v.05

--- src/FamilyTree/Family.java
+++ src/FamilyTree/Family.java
@@ -12,24 +12,16 @@ public class Family {
         this.familyName = familyName;
     }
 
-    public void setFamily(Human human) {
-        family.add(human);
+    public void setFamily(Human... human) {
+        for (Human someHuman : human)
+            family.add(someHuman);
     }
-    public void setFamily(Human human1, Human human2) {
-        family.add(human1);
-        setFamily(human2);
-    }
-    public void setFamily(Human human1, Human human2, Human human3) {
-        family.add(human1);
-        setFamily(human2, human3);
-    }
-
 
     @Override
     public String toString() {
         StringBuilder st = new StringBuilder();
         for (Human human : family)
             st.append(", " + human.name);
-        return familyName + ": (" + st.toString().substring(2, st.length()) + ")";
+        return familyName + ": (" + st.toString().substring(2) + ")";
     }
 }
--- src/FamilyTree/Main.java
+++ src/FamilyTree/Main.java
@@ -1,47 +1,28 @@
 package FamilyTree;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-
-        List<Human> people = new ArrayList<>();
 
             Human Misha = new Human("Misha", "man");
             Human Olga = new Human("Olga", "woman");
             Human Masha = new Human("Masha", "woman");
             Human Sasha = new Human("Sasha", "man");
             Human Nikita = new Human("Nikita", "man", Sasha, Masha);
-
-
-
-        people.add(Masha);
-        people.add(Misha);
-        people.add(Sasha);
-        people.add(Nikita);
-        System.out.println();
+            Human Vika = new Human("Vika", "woman");
         System.out.println(Nikita);
         System.out.println("------------->");
 
         Nikita.AddChild(Misha);
         Nikita.AddChild(Olga);
 
-
-    for (Human human : people) {
-        System.out.println(human);
-    }
-        System.out.println(Sasha.name + " его 'child' --> " + Sasha.children);
-        System.out.println(Nikita.children);
-
         System.out.println(Nikita.GetParent());
         System.out.println(Misha.GetParent());
 
         System.out.println(Nikita.GetChild());
         System.out.println("------------->");
+        Olga.AddParent(Vika);
         System.out.println(Olga);
         System.out.println(Olga.GetParent());
-        System.out.println(Nikita.GetChild());
 
         Olga.AddParent(Misha);
         System.out.println(Olga.GetParent());
@@ -50,8 +31,7 @@ public class Main {
 
         Family pipkins = new Family("Pipkins");
 
-        pipkins.setFamily(Sasha, Masha, Nikita);
-        pipkins.setFamily(Olga, Misha);
+        pipkins.setFamily(Sasha, Masha, Nikita, Olga, Misha, Vika);
 
         System.out.println(pipkins);
     }
