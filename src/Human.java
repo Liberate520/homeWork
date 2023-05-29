119a3de033de9e6919e323b42203c3c962983b32
1chello <underzero@bk.ru>
2023-05-29 18:09:00 +0300

    All done

--- src/Human.java
+++ src/Human.java
@@ -1,44 +1,46 @@
-import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.Period;
 
 public class Human {
-    private static int globalId;
-    private int id;
     private String name;
     private String surname;
     private LocalDate birthdate;
     private LocalDate deathDate;
     private Gender gender;
+    private String father;
+    private String mother;
+    private String sister;
+    private String brother;
+    private String grandFather;
+    private String grandMother;
 
-    public Human(String name, String surname, LocalDate birthdate, LocalDate deathDate, Gender gender) {
+    public Human(String name, String surname, LocalDate birthdate, LocalDate deathDate, Gender gender, String father, String mother, String sister, String brother, String grandFather, String grandMother) {
         this.name = name;
         this.surname = surname;
         this.birthdate = birthdate;
         this.deathDate = deathDate;
         this.gender = gender;
+        this.father = father;
+        this.mother = mother;
+        this.sister = sister;
+        this.brother = brother;
+        this.grandFather = grandFather;
+        this.grandMother = grandMother;
     }
 
-    /**
-     *
-     * TODO Не могу понять как реализовать возвращение возраста с учетом даты смерти,
-     */
-//    public String getAge() {
-//        LocalDate now = LocalDate.now();
-//        Period period = Period.between(birthdate, deathDate);
-//        int years = period.getYears();
-//        if(deathDate != null){
-//            return "Умер в возрасте " + years;
-//        }
-//        else{
-//        return ((Integer) Period.between(birthdate, now).getYears());
-//        }
-//    }
 
-    public  int getAge(){
-        LocalDate now = LocalDate.now();
-        return  Period.between(birthdate,now).getYears();
-    }
+    public Object getAge(LocalDate birthdate, LocalDate deathDate){
+       int age = Period.between(birthdate, LocalDate.now()).getYears();
+       if(deathDate == null){
+           return " " + age + " лет";
+       } else if (birthdate == null && deathDate == null) {
+           return "Такого персонажа ещее не существует";
+       } else {
+           int yearsBeforeDeath = Period.between(birthdate,deathDate).getYears();
+           return  " умер в возрасте " + yearsBeforeDeath + " лет";
+       }
+   }
+
     public String getName() {
         return name;
     }
@@ -79,15 +81,68 @@ public class Human {
         this.gender = gender;
     }
 
+    public String getFather() {
+        return "Имя отца: " + father;
+    }
+
+    public void setFather(String father) {
+        this.father = father;
+    }
+
+    public String getMother() {
+        return "Имя матери: " + mother;
+    }
+
+    public void setMother(String mother) {
+        this.mother = mother;
+    }
+
+    public String getSister() {
+        return "Имя сестры: " + sister;
+    }
+
+    public void setSister(String sister) {
+        this.sister = sister;
+    }
+
+    public String getBrother() {
+        return "Имя брата: " + brother;
+    }
+
+    public void setBrother(String brother) {
+        this.brother = brother;
+    }
+
+    public String getGrandFather() {
+        return "Имя деда: " + grandFather;
+    }
+
+    public void setGrandFather(String grandFather) {
+        this.grandFather = grandFather;
+    }
+
+    public String getGrandMother() {
+        return "Имя бабушки: " + grandMother;
+    }
+
+    public void setGrandMother(String grandMother) {
+        this.grandMother = grandMother;
+    }
+
     @Override
     public String toString() {
         return "Human{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
+                "name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
                 ", birthdate=" + birthdate +
                 ", deathDate=" + deathDate +
                 ", gender=" + gender +
+                ", father='" + father + '\'' +
+                ", mother='" + mother + '\'' +
+                ", sister='" + sister + '\'' +
+                ", brother='" + brother + '\'' +
+                ", grandFather='" + grandFather + '\'' +
+                ", grandMother='" + grandMother + '\'' +
                 '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,93 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-    Human person0 = new Human("Nikolay", "Kalugin", LocalDate.of(1938,03,01), null, Gender.MALE);
-    Human person1 = new Human("Nikolay", "Kalugin", LocalDate.of(1938,03,01), null, Gender.MALE);
-    Human person2 = new Human("Nikolay", "Kalugin", LocalDate.of(1938,03,01), null, Gender.MALE);
-    Human person3 = new Human("Nikolay", "Kalugin", LocalDate.of(1938,03,01), null, Gender.MALE);
+    LocalDate date =LocalDate.now();
+
+
+        Human person0 = new Human("Николай", "Калугин", LocalDate.of(1938,03,01), LocalDate.of(2015, 8,22), Gender.MALE, null, null,null,null,null,null);
+        Human person1 = new Human("Зинаида", "Калугина", LocalDate.of(1943, 10,15), null, Gender.FEMALE, null,null,null,null,null,null );
+        Human person2 = new Human("Анатолий", "Кондратьев", LocalDate.of(1939, 11,14), LocalDate.of(2018, 11, 10), Gender.MALE, null,null,null,null,null,null );
+        Human person3 = new Human("Александра", "Кондратьева", LocalDate.of(1938, 4,26), LocalDate.of(1994, 1, 5), Gender.FEMALE, null,null,null,null,null,null );
+        Human person4 = new Human("Олег", "Кондратьев", LocalDate.of(1965, 4,12), null, Gender.MALE, null,null,null,null,null,null );
+        Human person5 = new Human("Татьяна", "Кондратьева", LocalDate.of(1964, 3,11), null, Gender.FEMALE, null,null,null,null,null,null );
+        Human person6 = new Human("Татьяна", "Кондратьева", LocalDate.of(1974, 9,19), null, Gender.FEMALE, null,null,null,null,null,null );
+        Human person7 = new Human("Алксандр", "Кондратьев", LocalDate.of(1984, 3,22), null, Gender.MALE, null,null,null,null,null,null );
+        Human person8 = new Human("Анна", "Кондратьева", LocalDate.of(1986, 6,30), null, Gender.FEMALE, null,null,null,null,null,null );
+        Human person9 = new Human("Иван", "Кондратьев", LocalDate.of(1994, 6,21), null, Gender.MALE, null,null,null,null,null,null );
+        Human person10 = new Human("Денис", "Кондратьев", LocalDate.of(2004, 1,29), null, Gender.MALE, null,null,null,null,null,null );
+        Human person11 = new Human("Дарья", "Кондратьева", LocalDate.of(1993, 3,23), null, Gender.FEMALE, null,null,null,null,null,null );
+        Human person12 = new Human("Лев", "Кондратьев", LocalDate.of(2022, 6,11), null, Gender.MALE, null,null,null,null,null,null );
+
 
     FamilyTree familyTree = new FamilyTree();
     familyTree.addHuman(person0);
     familyTree.addHuman(person1);
     familyTree.addHuman(person2);
     familyTree.addHuman(person3);
-    Human findHuman = familyTree.findHumanByName("Nikolay");
+    familyTree.addHuman(person4);
+    familyTree.addHuman(person5);
+    familyTree.addHuman(person6);
+    familyTree.addHuman(person7);
+    familyTree.addHuman(person8);
+    familyTree.addHuman(person9);
+    familyTree.addHuman(person10);
+    familyTree.addHuman(person11);
+    familyTree.addHuman(person12);
+
+        person12.setFather(person9.getName());
+        person12.setMother(person11.getName());
+
+        person10.setFather(person4.getName());
+        person10.setMother(person6.getName());
+        person10.setBrother(person9.getName());
+        person10.setBrother(person7.getName());
+        person10.setSister(person8.getName());
+        person10.setGrandFather(person2.getName());
+        person10.setGrandFather(person0.getName());
+        person10.setGrandMother(person3.getName());
+        person10.setGrandMother(person1.getName());
+
+        person9.setFather(person4.getName());
+        person9.setMother(person6.getName());
+        person9.setBrother(person10.getName());
+        person9.setBrother(person7.getName());
+        person9.setSister(person8.getName());
+        person9.setGrandFather(person2.getName());
+        person9.setGrandFather(person0.getName());
+        person9.setGrandMother(person3.getName());
+        person9.setGrandMother(person1.getName());
+
+        person8.setFather(person4.getName());
+        person8.setMother(person5.getName());
+        person8.setBrother(person10.getName());
+        person8.setBrother(person7.getName());
+        person8.setBrother(person9.getName());
+        person8.setGrandFather(person2.getName());
+        person8.setGrandMother(person3.getName());
+
+        person7.setFather(person4.getName());
+        person7.setMother(person5.getName());
+        person7.setBrother(person10.getName());
+        person7.setSister(person8.getName());
+        person7.setBrother(person9.getName());
+        person7.setGrandFather(person2.getName());
+        person7.setGrandMother(person3.getName());
+
+        person6.setFather(person0.getName());
+        person6.setMother(person1.getName());
+
+        person4.setFather(person2.getName());
+        person4.setMother(person3.getName());
+
         System.out.println(familyTree.getHumanList());
         System.out.println("-----------------------");
-        System.out.println(person0.getAge());
+        System.out.println(person0.getName() + person0.getAge(person0.getBirthdate(),person0.getDeathDate()));
+        System.out.println("-----------------------");
+        System.out.println(person1.getName() + person1.getAge(person1.getBirthdate(), person1.getDeathDate()));
+        System.out.println("-----------------------");
+        System.out.println(person12.getFather());
+        System.out.println(person12.getMother());
+        System.out.println(person9.getBrother());
+        System.out.println(person6.getMother());
     }
 }
