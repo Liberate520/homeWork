43c32ab3e4bf86143fd4e733226af3b99a3055ac
Brightvl <ziro-x-100@mail.ru>
2023-05-29 20:34:34 +0300

    commit_6. HW1. FamilyTree. Added dates of birth for people to have a more correct assignment.

--- src/HW1/Main.java
+++ src/HW1/Main.java
@@ -5,10 +5,11 @@ import static HW1.Trees.ruriks;
 public class Main {
     public static void main(String[] args) {
 
+        
         ruriks.showParents("Рюрик",879);
         ruriks.showChildren("Рюрик",879);
         ruriks.showChildren("Игорь",945);
-        ruriks.showChildren("Святослав",972);
+        ruriks.showChildren("святослав",972);
         ruriks.showParents("святослав",972);
         System.out.println(ruriks.getPersonFromTree("Ольга",969));
         System.out.println(ruriks.getPersonFromTree("игорь",945));
