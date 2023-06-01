9c177351b4bcc8c3dc793c89bafdd30e4ed5ed95
yanst <stepuro.yan@gmail.com>
2023-06-01 18:09:48 +0300

    hw1 familytree

---
+++ src/FamilyTree/FamilyTree.java
@@ -0,0 +1,50 @@
+package FamilyTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+    private List<Person> personList;
+
+    public FamilyTree(List<Person> personList) {
+        this.personList = personList;
+    }
+    public FamilyTree() { this(new ArrayList<>()); }
+
+    public boolean add(Person person) {
+        if (person == null) {
+            return false;
+        }
+        if (!personList.contains(person)) {
+            personList.add(person);
+            if (person.getFather() != null) {
+                person.getFather().addChild(person);
+            }
+            if (person.getMother() != null) {
+                person.getMother().addChild(person);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public Person getByName(String name) {
+        for(Person person : personList) {
+            if (person.getName().equals(name)) {
+                return person;
+            }
+        }
+        return null;
+    }
+
+    public String getFamilyTree() {
+        StringBuilder sb = new StringBuilder();
+        for (Person person : personList) {
+            sb.append(person.getPersonInfo());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+
+}
---
+++ src/FamilyTree/Gender.java
@@ -0,0 +1,5 @@
+package FamilyTree;
+
+public enum Gender {
+    male, female
+}
