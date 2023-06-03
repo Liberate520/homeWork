e324f1bc4bf19afc1f9a1aa9369f3bd70e3c0e79
IliaBu <117571487+IliaBu@users.noreply.github.com>
2023-06-03 19:44:02 +0500

    Update ChildrenComparatorByAge.java

--- src/comparators/ChildrenComparatorByAge.java
+++ src/comparators/ChildrenComparatorByAge.java
@@ -1,1 +1,14 @@
+package comparators;
+
+import tree.FamilyTreeItem;
+
+import java.util.Comparator;
+
+public class ChildrenComparatorByAge<F extends FamilyTreeItem> implements Comparator<F> {
+
+    @Override
+    public int compare(F o1, F o2) {
+        return Integer.compare(o1.getAge(), o2.getAge());
+    }
+}
 
