f366de8724a3b6f6cece6cc30eb692bcd7af1d85
IliaBu <117571487+IliaBu@users.noreply.github.com>
2023-06-03 19:44:40 +0500

    Update ChildrenComparatorByName.java

--- src/comparators/ChildrenComparatorByName.java
+++ src/comparators/ChildrenComparatorByName.java
@@ -1,1 +1,14 @@
+package comparators;
+
+import tree.FamilyTreeItem;
+
+import java.util.Comparator;
+
+public class ChildrenComparatorByName<F extends FamilyTreeItem> implements Comparator<F> {
+
+    @Override
+    public int compare(F o1, F o2) {
+        return o1.getFirstName().compareTo(o2.getFirstName());
+    }
+}
 
