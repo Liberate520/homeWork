673f9118aa185117e817a46bdd5b2dfeb7336d04
Brightvl <ziro-x-100@mail.ru>
2023-06-03 14:26:42 +0300

    commit_1. HW3. FamilyTree.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 
 import static FamilyTrees.Trees.ruriks;
 
+/**
+ * Реализовать интерфейс Iterable для дерева.
+ * Создать методы сортировки списка людей перед выводом, например по имени или по дате рождения (не менее 2)
+ */
 
 public class Main {
     public static void main(String[] args) {
