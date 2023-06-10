2fa6d66900a184cfc18de502f57ca55c4c83ad83
IliaBu <117571487+IliaBu@users.noreply.github.com>
2023-06-10 19:49:40 +0500

    Create AddHuman.java
    add Human in menu

---
+++ src/ui/AddHuman.java
@@ -0,0 +1,19 @@
+package ui;
+
+public class AddHuman implements Command {
+    private Console console;
+
+    public AddHuman(Console console) {
+        this.console = console;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Добавить человека";
+    }
+
+    @Override
+    public void execute() {
+        console.addHuman();
+    }
+}
