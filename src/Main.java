99770b67f033ce9f97b469b8eb0fda673ba85fd4
IliaBu <117571487+IliaBu@users.noreply.github.com>
2023-06-10 20:08:49 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import human.Gender;
 import human.Human;
 import presenter.Presenter;
 import serializer.TreeSerializer;
@@ -7,20 +6,13 @@ import tree.Service;
 import ui.Console;
 import ui.View;
 
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
 
         FamilyTree<Human> familyTree = new FamilyTree<>();
         View view = new Console();
         Service service = new Service(familyTree);
-        service.addHuman("Василий", "Иванов", Gender.Male, LocalDate.of(1991, 12, 25));
-        service.addHuman("Кристина", "Иванова", Gender.Female, LocalDate.of(1988, 1, 2));
-        service.addHuman("Ольга", "Иванова",Gender.Female, LocalDate.of(1988, 1, 2));
-        service.addHuman("Иван", "Иванов",Gender.Male, LocalDate.of(1988, 5, 6));
-        service.addHuman("Сергей", "Иванов",Gender.Male, LocalDate.of(1989, 4, 9));
-
+      
         service.sortByFirstName();
         //service.sortByAge();
 
