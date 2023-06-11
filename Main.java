46002ec802677e9de12153d06a6bd1f23a770d64
ArtemKostornov <121346457+ArtemKostornov@users.noreply.github.com>
2023-06-12 00:15:59 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,27 @@
+package three;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+
 public class Main {
     public static void main(String[] args) {
-        Human father = new Human(33,"Shrack","male",0);
-        Human mother = new Human(28,"Fiona","female",1);
-        Human grandpa = new Human(63,"Max", "male",5);
-        Human grandma = new Human(59,"Mary","female",4);
-        Human father1 = new Human(23,"Mike","male",1);
-        Human mother1 = new Human(25,"Jane","female",3);
+        Human father = new Human("Shrack","male",0,null,null);
+        Human mother = new Human("Fiona","female",1,null,null);
+        Human grandpa = new Human("Max", "male",5,null,null);
+        Human grandma = new Human("Mary","female",4,null,null);
+        Human father1 = new Human("Mike","male",1,null,null);
+        Human mother1 = new Human("Jane","female",3,null,null);
 
         FamilyThree familyThree = new FamilyThree();
-        familyThree.addFamily(father1);
-        familyThree.addFamily(father);
-        familyThree.addFamily(mother1);
-        familyThree.addFamily(mother);
-        familyThree.addFamily(grandma);
-        familyThree.addFamily(grandpa);
+        familyThree.add(father1);
+        familyThree.add(father);
+        familyThree.add(mother1);
+        familyThree.add(mother);
+        familyThree.add(grandma);
+        familyThree.add(grandpa);
 
         System.out.println(familyThree.findHumanName("Shrack"));
-        System.out.println(familyThree.findHumanSisterBrother(1));
-        System.out.println(familyThree.getFamilyList());
+        System.out.println(familyThree.getInfo());
 
     }
 }
