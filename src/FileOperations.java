9c17164cd582d7a90dcf6e9337a2de6f3d6c600e
Aryom Smirnov <expertvideo.as@gmail.com>
2023-06-14 17:50:48 +0300

    удалил файлы Children.java + Parents.java + Test.java

--- src/FileOperations.java
+++ src/FileOperations.java
@@ -3,7 +3,7 @@ import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.io.Serializable;
 
-public interface FileOperations extends Serializable {
+public interface FileOperations  {
     void writeExternal(Human human, String filePath) throws IOException;
 
     Human readExternal(String filePath) throws IOException, ClassNotFoundException;
