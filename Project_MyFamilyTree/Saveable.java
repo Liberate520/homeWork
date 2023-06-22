4d95b6a99bc7cdb07a4b939b030efd49c6f6b690
SergeyK14 <116436295+SergeyK14@users.noreply.github.com>
2023-06-22 19:18:22 +0300

    Delete Project_MyFamilyTree directory

--- Project_MyFamilyTree/Saveable.java
+++
@@ -1,8 +0,0 @@
-package Project_MyFamilyTree;
-
-import java.io.Serializable;
-
-public interface Saveable {
-    public boolean saveMethod(Serializable serializable, String filesave);
-    public Object readMethod(String filesave);
-}
