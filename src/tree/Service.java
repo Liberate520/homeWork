7267ae0e32a60d6adeeac19ba7bbbbb8e8ddb376
IliaBu <117571487+IliaBu@users.noreply.github.com>
2023-06-23 22:04:00 +0500

    Update Service.java

--- src/tree/Service.java
+++ src/tree/Service.java
@@ -1,36 +1,19 @@
 package tree;
-
-import human.Gender;
-import serializer.Serializer;
 import human.Human;
-import java.io.Serializable;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import serializer.TreeSerializer;
+import ui.View;
 
-public class Service implements Serializable {
+public class Service{
+    private View view;
+    public FamilyTree<Human> familyTree;
+    private FamilyTrees familytrees;
+    private TreeSerializer serializ;
+    private String filePath = "FamilyTree.bin";
 
-    private FamilyTree<Human> familyTree;
-    private Serializer serializ;
-    private final String filePath = "tree.dat";
-
-    public Service(Serializer serializ) {
-        this.familyTree = new FamilyTree<>();
-        this.serializ = serializ;
-    }
-
-    public Service(FamilyTree<Human> tree) {
-        this.familyTree = tree;
-
-        addHuman("Василий", "Иванов", Gender.Male, LocalDate.of(1991, 12, 25), null, null, null);
-        addHuman("Кристина", "Иванова", Gender.Female, LocalDate.of(1988, 1, 2), null, null, null);
-        addHuman("Ольга", "Иванова",Gender.Female, LocalDate.of(1988, 1, 2), null, tree.getByFirstName("Василий"), tree.getByFirstName("Кристина"));
-        addHuman("Иван", "Иванов",Gender.Male, LocalDate.of(1988, 5, 6), null, tree.getByFirstName("Василий"), tree.getByFirstName("Кристина"));
-        addHuman("Сергей", "Иванов",Gender.Male, LocalDate.of(1989, 4, 9), null, tree.getByFirstName("Василий"), tree.getByFirstName("Кристина"));
-    }
-    
-    public void addHuman(String firstName, String lastname, Gender gender, LocalDate birthDate, LocalDate deathDate, Human father, Human mother) {
-        familyTree.add(new Human(firstName, lastname, gender, birthDate, deathDate, father, mother));
+    public Service(){
+        serializ = new TreeSerializer(filePath);
+        familytrees = new FamilyTrees();
+        familyTree = familytrees.tree1();
     }
 
     public String getInfoHuman() {
@@ -42,37 +25,22 @@ public class Service implements Serializable {
         return sb.toString();
     }
 
-    public void sortByFirstName() {
-        familyTree.sortByFirstName();
+    public void saveFile() {
+        this.serializ.setFilePath(filePath);
+        serializ.save(familyTree, filePath);
     }
 
-    public void sortByAge() {
-        familyTree.sortByAge();
+    public void loadFile(String filePath) {
+        this.filePath = filePath;
+        this.serializ.setFilePath(filePath);
+        this.familyTree = serializ.load(filePath);
     }
 
-    public void setWritable(Serializer writable) {
-        this.serializ = writable;
-    }
-
-    public void save() {
-        serializ.save(familyTree,filePath);
-    }
-
-    public Service load() {
-        if (serializ != null) {
-            if (serializ instanceof Service) {
-                if (serializ.load(filePath) == null) {
-                    System.out.println("Дерева в файле нет! Создаём новое дерево.");
-                    return new Service(serializ);
-                } else {
-                    System.out.println("Дерево загружено из файла.");
-                    return (Service) serializ.load(filePath);
-                }
-            }
-        } else {
-            System.out.println("Файл не загружен!");
-            return null;
+    public void sortTree(String choice) {
+        switch (choice) {
+            case "1" -> familyTree.sortByFirstName();
+            case "2" -> familyTree.sortByAge();
+            default -> System.out.println("Ошибка ввода");
         }
-        return null;
     }
 }
