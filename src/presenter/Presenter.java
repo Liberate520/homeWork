5e547c1bc872e3a89ac23d62284db446d47244e6
IliaBu <117571487+IliaBu@users.noreply.github.com>
2023-06-23 22:01:27 +0500

    Update Presenter.java

--- src/presenter/Presenter.java
+++ src/presenter/Presenter.java
@@ -1,34 +1,45 @@
 package presenter;
-import java.io.Serializable;
 import java.time.LocalDate;
-
 import human.Gender;
+import human.Human;
 import tree.Service;
 import ui.View;
 
-public class Presenter implements Serializable {
+public class Presenter {
     private View view;
     private Service service;
 
-    public Presenter(View view, Service service) {
-        this.view = view;
+    public Presenter(View view, Service service)
+    {
         this.service = service;
+        this.view = view;
         view.setPresenter(this);
+    }
+
+    public void saveFile()
+    {
+        service.saveFile();
+        view.print("Семейное древо сохранено !");
+    }
+
+    public void addHuman(String firstName, String lastname, Gender gender, LocalDate birthDate)
+    {
+        service.familyTree.add(new Human(firstName, lastname, gender, birthDate));
+        view.print("Новый член семьи добавлен !");
+    }
+
+    public void loadFile(String filePath)
+    {
+        service.loadFile(filePath);
+        view.print("Семейное древо загружено !");
     }
 
     public void getInfo() {
         view.print(service.getInfoHuman());
     }
 
-    public void addHuman(String firstName, String lastName, Gender gender, LocalDate birthDate, LocalDate deathDate, Human father, Human mother){
-        service.addHuman(firstName, lastName, gender, birthDate, deathDate, father, mother);
-    }
-    
-    public void saveFile() {
-        service.save();
+    public void sortTree(String choice) {
+        service.sortTree(choice);
     }
 
-    public void loadFile() {
-        this.service.load();
-    }
 }
