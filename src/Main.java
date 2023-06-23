7d1d728cd935fcb308d9f2681f0d624a8c6c8608
IliaBu <117571487+IliaBu@users.noreply.github.com>
2023-06-23 22:13:48 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,15 @@
-import human.Human;
 import presenter.Presenter;
-import serializer.TreeSerializer;
-import tree.FamilyTree;
 import tree.Service;
 import ui.Console;
 import ui.View;
 
 public class Main {
+    
     public static void main(String[] args) {
-
-        FamilyTree<Human> familyTree = new FamilyTree<>();
         View view = new Console();
-        Service service = new Service(familyTree);
-      
-        service.sortByFirstName();
-        //service.sortByAge();
-
-        service.setWritable(new TreeSerializer());
-
+        Service service = new Service();
         new Presenter(view, service);
         view.start();
     }
+    
 }
