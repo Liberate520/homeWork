2ba94e411d87cb6d79d7fcdf786fd0b352d0d26c
yanst <stepuro.yan@gmail.com>
2023-07-01 22:42:37 +0300

    hw7 familytree udp

--- src/FamilyTree/Main.java
+++ src/FamilyTree/Main.java
@@ -2,12 +2,8 @@ package FamilyTree;
 
 
 import FamilyTree.model.Service;
-import FamilyTree.model.fileHandler.FileHandler;
-import FamilyTree.model.fileHandler.SaveLoader;
 import FamilyTree.model.person.Gender;
 import FamilyTree.model.person.Person;
-import FamilyTree.model.pet.Pet;
-import FamilyTree.model.tree.FamilyTree;
 import FamilyTree.presenter.Presenter;
 import FamilyTree.view.ConsoleUI;
 import FamilyTree.view.View;
@@ -27,27 +23,6 @@ public class Main {
         familyTree.addPerson(new Person("Митрофанов Константин Дмитриевич", Gender.male, LocalDate.of(1981, 8, 25),
                 familyTree.getByName("Иванова Кристина Ивановна"),
                 familyTree.getByName("Митрофанов Дмитрий Васильевич")));
-
-//        String path = "src/FamilyTree/model/file.txt";
-//        familyTree.saveTree(path);
-//        familyTree.loadTree(path);
-//        System.out.println(familyTree.getFamilyTreeInfo());
-//
-//        familyTree.sortByName();
-//        System.out.println(familyTree.getFamilyTreeInfo());
-//
-//        familyTree.sortByAge();
-//        System.out.println(familyTree.getFamilyTreeInfo());
-
-        FamilyTree<Pet> petTree = new FamilyTree<>();
-        petTree.addPerson(new Pet("Маря", LocalDate.of(2018, 5, 20)));
-        petTree.addPerson(new Pet("Джей", LocalDate.of(2016, 8, 15)));
-        String path2 = "src/FamilyTree/model/pets.txt";
-        SaveLoader fileHandler = new FileHandler();
-        fileHandler.save(petTree, path2);
-        fileHandler.load(path2);
-        System.out.println(petTree.getFamilyTreeInfo());
-
 
         View familyTreeUI = new ConsoleUI();
         Presenter presenter = new Presenter(familyTreeUI, familyTree);
