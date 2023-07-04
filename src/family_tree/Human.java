180789b8d8ae8983998e9f448285edf04edb076d
levineserg <levineserg@gmail.com>
2023-07-04 07:43:24 +0400

    first commit

---
+++ src/family_tree/Human.java
@@ -0,0 +1,173 @@
+package family_tree;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Human {
+    private String name;
+    private Gender gender;
+    private LocalDate birthDate;
+    private LocalDate deathDate;
+    private List<Human> parents;
+    private List<Human> children;
+
+    public Human(String name, Gender gender, LocalDate birthDate, LocalDate deathDate, Human father, Human mother){
+        this.name = name;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.deathDate = deathDate;
+        parents = new ArrayList<>();
+        if (father != null){
+            parents.add(father);
+        }
+        if (mother != null){
+            parents.add(mother);
+        }
+        children = new ArrayList<>();
+    }
+    public Human(String name, Gender gender, LocalDate birthDate){
+        this(name, gender, birthDate, null, null, null);
+    }
+    public Human(String name, Gender gender, LocalDate birthDate, Human father, Human mother){
+        this(name, gender, birthDate, null, father, mother);
+    }
+    public boolean addChild(Human child){
+        if(!children.contains(child)){
+            children.add(child);
+            return true;
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public boolean addParent(Human parent){
+        if(!parents.contains(parent)){
+            parents.add(parent);
+            return true;
+        }
+        return false;
+        }
+    public LocalDate getBirthDate(){
+        return birthDate;
+    }
+    public LocalDate getDeathDate(){
+        return deathDate;
+    }
+    public Human getFather(){
+        for (Human parent: parents){
+            if(parent.getGender() == Gender.Male) return parent;
+        }
+        return null;
+    }
+    public Human getMother(){
+        for (Human parent: parents){
+            if(parent.getGender() == Gender.Female) return parent;
+        }
+        return null;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public List<Human> getParents() {
+        return parents;
+    }
+
+    public List<Human> getChildren() {
+        return children;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setDeathDate(LocalDate deathDate) {
+        this.deathDate = deathDate;
+    }
+    @Override
+    public String toString(){
+        return getInfo();
+    }
+    public String getInfo(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("имя: ");
+        sb.append(name);
+        sb.append(", пол: ");
+        sb.append(getGender());
+        sb.append(", возраст: ");
+        sb.append(getAge());
+        sb.append(", ");
+        sb.append(getMotherInfo());
+        sb.append(", ");
+        sb.append(getFatherInfo());
+        sb.append(", ");
+        sb.append(getChildrenInfo());
+        return sb.toString();
+    }
+    public String getMotherInfo(){
+        String res = "мать: ";
+        Human mother = getMother();
+        if (mother != null){
+            res += mother.getName();
+        } else {
+            res += "неизвестна";
+        }
+        return res;
+    }
+    public String getFatherInfo(){
+        String res = "отец: ";
+        Human father = getFather();
+        if (father != null){
+            res += father.getName();
+        } else {
+            res += "неизвестен";
+        }
+        return res;
+    }
+    public String getChildrenInfo() {
+        StringBuilder res = new StringBuilder();
+        res.append("дети: ");
+        if (children.size() != 0) {
+            res.append(children.get(0).getName());
+            for (int i = 1; i < children.size(); i++) {
+                res.append(", ");
+                res.append(children.get(i).getName());
+            }
+        } else {
+            res.append("отсутствуют");
+        }
+        return res.toString();
+    }
+    public int getAge(){
+        if (deathDate == null){
+            return getPeriod(birthDate, LocalDate.now());
+        } else {
+            return getPeriod(birthDate, deathDate);
+        }
+    }
+    private int getPeriod(LocalDate birthDate, LocalDate deathDate){
+        Period diff = Period.between(birthDate, deathDate);
+        return diff.getYears();
+    }
+    @Override
+    public boolean equals(Object obj) {
+        boolean result;
+        if (this == obj) {
+            result = true;
+        } else if (!(obj instanceof Human)) {
+            result = false;
+        } else {
+            Human human = (Human) obj;
+            result = human.getName().equals(getName());
+        }
+        return result;
+    }
+
+}
+
+
