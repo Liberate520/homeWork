481a0d715430b2a87363bca6f995607a24c0b75b
suntsovpavel <suntsovpavel@mail.ru>
2023-07-04 20:52:23 +0300

    some edit

--- src/SaveLoad.java
+++ src/SaveLoad.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.Serializable;
 
 public interface SaveLoad {
+    //первый аргумент относится к сохраняемым данным, второй - дополнительный
     public void SaveTo(Object obj, Object[] s) throws IOException;
 
     public Object LoadFrom(Object[] s) throws IOException, ClassNotFoundException;
