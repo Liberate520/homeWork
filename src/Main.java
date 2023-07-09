6bf6e64bbb99199a9effef61eb4fa0436e9d8623
Igor <eigsn@yandex.ru>
2023-07-10 02:32:34 +0300

    Continue3

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args){
 
         FamilyTree tree =new FamilyTree();
-        Human as=new Human(1,"Пушкин","","Александр","Сергеевич",male, 1799 - 06.05,10.02-1837,null,null);
-        Human nn=new Human(2,"Пушкина","Гончарова","Наталья","Николаевна",female,8-9-1812,8-12-1863);
+        Human as=new Human(1,"Пушкин","","Александр","Сергеевич",male, "2nd October 2007","1837- 01 -26",null,null);
+        //Human nn=new Human(2,"Пушкина","Гончарова","Наталья","Николаевна",female,8-9-1812,8-12-1863);
 
         tree.add(as);
     }
