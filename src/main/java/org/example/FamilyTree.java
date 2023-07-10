bc00d88e917e039d9136f512861098202068222d
BelovEA <vrachea676@gmail.com>
2023-07-10 21:03:09 +0300

    commit lesson1_hw

---
+++ src/main/java/org/example/FamilyTree.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+    private static List<Human> humanList;
+    private int id;
+    private int humanId;
+
+
+    public FamilyTree(int id) {
+        this.id = id;
+        humanList = new ArrayList<>();
+    }
+
+    public void addHuman(Human human) {
+        human.setId(humanId++);
+        humanList.add(human);
+        // addToChildren(human);
+        //addToParent(human);
+    }
+    /*public void addToParent(Human human){
+        if (human.getFather()!=null) human.getFather().addChild(human);
+        if (human.getMother()!=null) human.getMother().addChild(human);
+    }
+    public void addToChildren(Human human){
+        for (Human child:human.getChildren()) {
+            child.addParent(human);
+        }
+    }
+    */
+    public static String getHumanInfo() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Family tree:\n");
+        for (Human human: humanList){
+            stringBuilder.append(human);
+            stringBuilder.append("\n");
+        }
+        //stringBuilder.append(getChildrenInfo);
+        return stringBuilder.toString();
+    }
+}
