c49dc338c9974cf3b7a136cf9dc8bf13c1f13530
Ivan Panaitov <PanaitovIV@gmail.com>
2023-07-10 15:47:16 +0300

    внес изменения

--- src/family_tree/HumanIterator.java
+++ src/family_tree/HumanIterator.java
@@ -1,2 +1,22 @@
-package family_tree;public class HumanIterator {
+package family_tree;
+
+import human.Human;
+import java.util.Iterator;
+import java.util.List;
+
+public class HumanIterator implements Iterator<Human> {
+    private int index;
+    private List<Human> list;
+
+    public HumanIterator(List<Human> list) {
+        this.list = list;
+    }
+    @Override
+    public boolean hasNext() {
+        return list.size() > index;
+    }
+    @Override
+    public Human next() {
+        return list.get(index++);
+    }
 }
