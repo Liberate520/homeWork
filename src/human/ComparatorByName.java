975471435e596ab79bf37decbffffd7b626a0e20
Ivan Panaitov <PanaitovIV@gmail.com>
2023-07-10 15:48:18 +0300

    внес изменения

--- src/human/ComparatorByName.java
+++ src/human/ComparatorByName.java
@@ -1,2 +1,11 @@
-package human;public class ComparatorByName {
+package human;
+
+import human.Human;
+import java.util.Comparator;
+
+public class ComparatorByName implements Comparator<Human> {
+    @Override
+    public  int compare(Human o1, Human o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
 }
