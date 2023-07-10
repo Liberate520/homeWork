02734b3a857bfdffbe17c06710fb1bbc76f444bd
Mikhail Latypov <mikhaillat@yandex.ru>
2023-07-10 09:41:47 +0300

    Changing the class Human

--- src/Human.java
+++ src/Human.java
@@ -1,2 +1,9 @@
+import java.time.LocalDate;
+import java.util.List;
+
 public class Human {
+    LocalDate birthDate;
+    LocalDate deathDate;
+    Human father, mother;
+    List<Human> children;
 }
