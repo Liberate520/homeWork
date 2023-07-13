6c1f5e869642a125fff93f6a714be6d4fb313d13
Ivan Panaitov <PanaitovIV@gmail.com>
2023-07-13 15:27:15 +0300

    внес изменения

--- src/human/ComparatorByName.java
+++ src/human/ComparatorByName.java
@@ -1,11 +1,13 @@
 package human;
 
 import human.Human;
+import family_tree.FamilyTreeItem;
+
 import java.util.Comparator;
 
-public class ComparatorByName implements Comparator<Human> {
+public class ComparatorByName<T extends FamilyTreeItem> implements Comparator<T> {
     @Override
-    public  int compare(Human o1, Human o2) {
+    public  int compare(T o1, T o2) {
         return o1.getName().compareTo(o2.getName());
     }
 }
