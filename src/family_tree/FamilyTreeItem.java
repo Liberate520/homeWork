db56e7ad420ba02638fd68a95c08e298e13a14ec
Ivan Panaitov <PanaitovIV@gmail.com>
2023-07-13 15:26:05 +0300

    внес изменения

--- src/family_tree/FamilyTreeItem.java
+++ src/family_tree/FamilyTreeItem.java
@@ -1,2 +1,26 @@
-package family_tree;public interface FamilyTreeItem {
+package family_tree;
+
+import human.Gender;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface FamilyTreeItem<E> extends Serializable {
+    int getId();
+    void setId(int id);
+    int getAge();
+    String getName();
+    String getSurname();
+    String getPatronymic();
+    LocalDate getBirth();
+    Gender getGender();
+    E getMother();
+    E getFather();
+    List<E> getChildren();
+
+    <E extends FamilyTreeItem<E>> void setChildren(List<E> children);
+
+    void setFather(E father);
+
+    void setMother(E mother);
 }
