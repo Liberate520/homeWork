7ebf7576d4d388aa7f5d5802010b6d5cf49f3841
Ivan Panaitov <PanaitovIV@gmail.com>
2023-07-13 15:27:04 +0300

    внес изменения

--- src/human/ComparatorByAge.java
+++ src/human/ComparatorByAge.java
@@ -1,10 +1,13 @@
 package human;
 
+import human.Human;
+import family_tree.FamilyTreeItem;
+
 import java.util.Comparator;
 
-public class ComparatorByAge implements Comparator<Human> {
+public class ComparatorByAge<T extends FamilyTreeItem> implements Comparator<T> {
     @Override
-    public int compare (Human o1, Human o2) {
-        return o1.getAge().compareTo(o2.getAge());
+    public int compare (T o1, T o2) {
+        return Integer.compare(o1.getAge(), o2.getAge());
     }
 }
