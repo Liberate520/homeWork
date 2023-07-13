afebaed7d1ca20c5d88d78daf25ba8f98f60b104
EduardBoyarchuk <boyarchuk.eduard@list.ru>
2023-07-13 22:45:15 +0300

    Generici1

--- src/familytree/Tree/GetByOn.java
+++ src/familytree/Tree/GetByOn.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 public interface GetByOn {
     String getName();
-
     String getSurname();
-    List<Person> getChildren();
-    List<Person> addParent();
-    List<Person> addChild();
-    List<Person> addPartner();
+  
+    <E> void addChild(E child);
+
+    <E> void addPartner(E partner);
+    <E> void addParent(E parent);
+
+    Person[] getChildren();
 }
