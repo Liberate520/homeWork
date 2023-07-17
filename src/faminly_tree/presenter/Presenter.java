a341e1babab2c2df2a58ec3a9f202baccceeb26f
Ivan Panaitov <PanaitovIV@gmail.com>
2023-07-17 14:50:56 +0300

    внес изменения

--- src/faminly_tree/presenter/Presenter.java
+++ src/faminly_tree/presenter/Presenter.java
@@ -1,8 +1,8 @@
-package model.presenter;
+package faminly_tree.presenter;
 
-import model.human.Gender;
-import model.service.Service;
-import model.view.View;
+import faminly_tree.model.human.Gender;
+import faminly_tree.model.service.Service;
+import faminly_tree.view.View;
 
 import java.time.LocalDate;
 
@@ -13,8 +13,8 @@ public class Presenter {
         this.view = view;
         service = new Service();
     }
-    public void addHuman(String name, Gender gender, LocalDate date) {
-        service.addHuman(name, gender, date);
+    public void addHuman(String name, String surname, String patronymic, Gender gender, LocalDate date) {
+        service.addHuman(name, surname, patronymic, gender, date);
     }
     public void sortByAge() {
         service.sortByAge();
@@ -22,7 +22,7 @@ public class Presenter {
     public void sortByName() {
         service.sortByName();
     }
-    public void sortByID(){
+    public void sortByID() {
         service.sortByID();
     }
     public void showAllTree() {
@@ -37,10 +37,11 @@ public class Presenter {
     public void downland(String path) {
         service.dowland(path);
     }
-    public void nextOfKin(int humanID) {
-        service.nextOfKin(humanID);
-    }
-    public int treeIsEmpty(){
+
+    public int treeIsEmpty() {
         return service.treeIsEmpty();
+    }
+
+    public void nextOfKin(int humanID) {
     }
 }
