7e692e846c54c4a6f7a5969bd3ca9d188be940b1
Ivan Panaitov <PanaitovIV@gmail.com>
2023-07-17 15:06:45 +0300

    внес изменения

--- src/faminly_tree/view/commands/AddConnection.java
+++ src/faminly_tree/view/commands/AddConnection.java
@@ -1,2 +1,14 @@
-package faminly_tree.view.commands;public class AddConnection {
+package faminly_tree.view.commands;
+
+import faminly_tree.view.ConsoleUI;
+
+public class AddConnection extends Command{
+    public AddConnection(ConsoleUI console) {
+        super(console);
+        description = "Добавить связь ребенок-родитель для имеющихся в дереве людей";
+    }
+    @Override
+    public void execute() {
+        console.addConnection();
+    }
 }
