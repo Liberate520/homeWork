1be64e18b5882aab8156ed281129c925e8e5af08
kirill <golovinkiruxa@gmail.com>
2023-07-18 22:26:44 +0200

    familytree

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+
+import model.familyTree.FamilyTree;
+import model.human.Gender;
+import model.human.Human;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        FamilyTree tree = new FamilyTree();
+
+        Human vasya = new Human("Василий", Gender.Male, LocalDate.of(1963, 12, 10));
+        Human masha = new Human("Мария", Gender.Female, LocalDate.of(1965, 9, 15));
+        Human christina = new Human("Кристина", Gender.Female, LocalDate.of(1988, 7, 5),
+                vasya, masha);
+        Human semyon = new Human("Семен", Gender.Male, LocalDate.of(1991, 1, 25),
+                vasya, masha);
+
+        tree.add(vasya);
+        tree.add(masha);
+        tree.add(christina);
+        tree.add(semyon);
+
+        Human grandMother = new Human("Лариса", Gender.Female, LocalDate.of(1945, 9, 1));
+        grandMother.addChild(vasya);
+
+        tree.add(grandMother);
+
+        System.out.println(tree);
+    }
+}
