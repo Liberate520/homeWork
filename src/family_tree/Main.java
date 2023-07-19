97a0d98d5173e824c0d44d5ab5d8322fdb67adad
aleksey.krasnov <krasnov.80@mail.ru>
2023-07-19 12:23:43 +0300

    версия с семинара

---
+++ src/family_tree/Main.java
@@ -0,0 +1,34 @@
+package family_tree;
+
+import family_tree.family_tree.FamilyTree;
+import family_tree.human.Gender;
+import family_tree.human.Human;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        FamilyTree tree = testTree();
+        System.out.println(tree);
+    }
+    static FamilyTree testTree(){
+        FamilyTree tree = new FamilyTree();
+        Human vasya = new Human("Василий", Gender.Male, LocalDate.of(1963,12,10));
+        Human masha = new Human("Мария", Gender.Female, LocalDate.of(1963,9,15));
+        tree.add(vasya);
+        tree.add(masha);
+        tree.setWedding(vasya.getId(),masha.getId());
+
+        Human christina = new Human("Кристина",Gender.Female,LocalDate.of(1988,7,5),
+                vasya, masha);
+        Human semyon = new Human("Семен",Gender.Male,LocalDate.of(1988,7,5),
+                vasya,masha);
+        tree.add(christina);
+        tree.add(semyon);
+        Human larisa = new Human("Лариса",Gender.Female,LocalDate.of(1945,9,1));
+        larisa.addChild(vasya);
+        tree.add(larisa);
+        return tree;
+
+    }
+}
---
+++ src/family_tree/family_tree/FamilyTree.java
@@ -0,0 +1,136 @@
+package family_tree.family_tree;
+
+import family_tree.human.Human;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+    public long humansId;
+    private List<Human> humanList;
+
+    public FamilyTree(List<Human> humanList) {
+        this.humanList=humanList;
+    }
+
+    public FamilyTree(){
+        this(new ArrayList<>());
+    }
+
+    public boolean add(Human human){
+        if(human == null){
+            return false;
+        }
+        if(!humanList.contains(human)){
+            humanList.add(human);
+            human.setId(humansId++);
+
+            addToParents(human);
+            addToCildren(human);
+
+            return true;
+        }
+        return false;
+    }
+    private void addToParents(Human human){
+        for(Human parent:human.getParents()){
+            parent.addChild(human);
+        }
+    }
+    private void addToCildren(Human human){
+        for(Human child:human.getChildren()){
+            child.addParent(human);
+        }
+    }
+    public List<Human> getSiblings(int id){
+        Human human = getById(id);
+        if (human==null){
+            return null;
+        }
+        List<Human> res = new ArrayList<>();
+        for(Human parent:human.getParents()){
+            for (Human child:parent.getChildren()){
+                if(!child.equals(human)){
+                    res.add(child);
+                }
+            }
+        }
+        return res;
+    }
+    public List<Human> getByName(String name){
+        List<Human> res = new ArrayList<>();
+        for (Human human:humanList){
+            if(human.getName().equalsIgnoreCase(name)){
+                res.add(human);
+            }
+        }
+        return res;
+    }
+
+    public boolean setWedding(long humanId1, long humasId2){
+        if(checkId(humanId1) && checkId(humasId2)){
+            Human human1 = getById(humanId1);
+            Human human2 = getById(humasId2);
+            if(human1.getSpouse() == null&& human2.getSpouse()==null){
+                human1.setSpouse(human2);
+                human2.setSpouse(human1);
+            }else{
+                return false;
+            }
+        }
+        return false;
+    }
+    public boolean setDivorce(long humanId1, long humanId2){
+        Human human1=getById(humanId1);
+        Human human2=getById(humanId2);
+        if (human1.getSpouse()!=null && human2.getSpouse() != null){
+            human1.setSpouse(null);
+            human2.setSpouse(null);
+        } else {
+            return false;
+        }
+        return false;
+    }
+    public boolean remove(long humansId){
+        if(checkId(humansId)){
+            Human e = getById(humansId);
+            return humanList.remove(e);
+        }
+        return false;
+    }
+    private boolean checkId(long id){
+        if(id>= humansId||id<0){
+            return false;
+        }
+        for (Human human: humanList){
+            if (human.getId()==id){
+                return true;
+            }
+        }
+        return false;
+    }
+    public Human getById(long id){
+        for (Human human: humanList){
+            if (human.getId()==id){
+                return human;
+            }
+        }
+        return null;
+    }
+    public String getInfo(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("В дереве ");
+        sb.append(humanList.size());
+        sb.append(" объектов: \n");
+        for(Human human:humanList){
+            sb.append(human);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return getInfo();
+    }
+}
