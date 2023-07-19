62d3940a648529688275e8c41a217cc79eb70d3a
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-07-19 16:00:15 +0300

    first dz oop

--- src/GenealogyResearchApp.java
+++ src/GenealogyResearchApp.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 class Person {
     private String name;
@@ -33,6 +34,10 @@ class FamilyTree {
     public Person getRoot() {
         return root;
     }
+
+    public void setRoot(Person root) {
+        this.root = root;
+    }
 }
 
 public class GenealogyResearchApp {
@@ -51,13 +56,91 @@ public class GenealogyResearchApp {
         mary.addChild(alice);
         peter.addChild(david);
 
-        // Проводим исследование, получаем всех детей выбранного человека
-        List<Person> children = familyTree.getRoot().getChildren();
+        Scanner scanner = new Scanner(System.in);
+        int choice;
 
-        // Выводим результат исследования
-        System.out.println("Дети выбранного человека:");
+        do {
+            // Выводим меню
+            System.out.println("\nВыберите действие:");
+            System.out.println("1. Изучить детей выбранного человека");
+            System.out.println("2. Добавить нового человека с детьми");
+            System.out.println("3. Вывести список объектов");
+            System.out.println("4. Выйти");
+
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    // Изучаем детей выбранного человека
+                    System.out.print("Введите имя человека для изучения: ");
+                    String name = scanner.next();
+                    Person person = findPerson(familyTree.getRoot(), name);
+                    if (person != null) {
+                        List<Person> children = person.getChildren();
+                        if (children.isEmpty()) {
+                            System.out.println("У выбранного человека нет детей.");
+                        } else {
+                            System.out.println("Дети выбранного человека:");
+                            for (Person child : children) {
+                                System.out.println(child.getName());
+                            }
+                        }
+                    } else {
+                        System.out.println("Человек с таким именем не найден.");
+                    }
+                    break;
+                case 2:
+                    // Добавляем нового человека с детьми
+                    System.out.print("Введите имя нового человека: ");
+                    String newName = scanner.next();
+                    Person newPerson = new Person(newName);
+                    System.out.print("Введите количество детей у нового человека: ");
+                    int numChildren = scanner.nextInt();
+                    for (int i = 0; i < numChildren; i++) {
+                        System.out.print("Введите имя ребенка " + (i + 1) + ": ");
+                        String childName = scanner.next();
+                        Person child = new Person(childName);
+                        newPerson.addChild(child);
+                    }
+                    familyTree.getRoot().addChild(newPerson);
+                    System.out.println("Новый человек успешно добавлен.");
+                    break;
+                case 3:
+                    // Выводим список объектов
+                    System.out.println("Список объектов в генеалогическом древе:");
+                    printPerson(familyTree.getRoot());
+                    break;
+                case 4:
+                    // Выходим из программы
+                    System.out.println("Программа завершена.");
+                    break;
+                default:
+                    System.out.println("Некорректный ввод. Попробуйте еще раз.");
+                    break;
+            }
+        } while (choice != 4);
+    }
+
+    public static Person findPerson(Person person, String name) {
+        if (person.getName().equals(name)) {
+            return person;
+        } else {
+            List<Person> children = person.getChildren();
+            for (Person child : children) {
+                Person foundPerson = findPerson(child, name);
+                if (foundPerson != null) {
+                    return foundPerson;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void printPerson(Person person) {
+        System.out.println(person.getName());
+        List<Person> children = person.getChildren();
         for (Person child : children) {
-            System.out.println(child.getName());
+            printPerson(child);
         }
     }
 }
