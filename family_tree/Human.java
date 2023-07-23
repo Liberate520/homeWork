001ed8da521d46ca080a234a7a35b3560df709e9
Sergey <saizotov1@gmail.com>
2023-07-23 17:56:12 +0500

    second commit

--- family_tree/Human.java
+++ family_tree/Human.java
@@ -1,6 +1,7 @@
 package homeWork.family_tree;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,5 +53,141 @@ public class Human {
         return false;
     }
 
+    public Human getFather; {
+        for (Human parent : parents){
+            if (parent.getGender() == Gender.Male){
+                return parent;
+            }
+        }
+        return null;
+    }
+    public Human getMother; {
+        for (Human parent : parents) {
+            if (parent.getGender() == Gender.Female) {
+                return parent;
+            }
+        }
+        return null;
+    }
+    public int getAge(){
+        if (deathDate == null){
+            return getPeriod(birthDate, LocalDate.now());
+        }
+        else {
+            return getPeriod(birthDate, deathDate);
+        }
+    }
+    private int getPeriod(LocalDate birthDate, LocalDate deathDate){
+        Period diff = Period.between(birthDate,deathDate);
+        return diff.getYears();
+    }
+    public void setSpouse(Human spouse) {this.spouse = spouse;}
 
+    public Human getSpouse() {return spouse;}
+
+    public String getName() {return name;}
+
+    public long getId() {return id;}
+
+    public void setId(long id) {this.id = id;}
+
+    public LocalDate getBirthDate() {return birthDate;}
+
+    public LocalDate getDeathDate() {return deathDate;}
+
+    public List<Human> getParents() {return parents;}
+
+    public List<Human> getChildren() {return children;}
+
+    public void setBirthDate(LocalDate birthDate) {this.birthDate = birthDate;}
+
+    public void setDeathDate(LocalDate deathDate) {this.deathDate = deathDate;}
+    public Gender getGender(){return gender;}
+
+    @Override
+    public String toString() {return getInfo();}
+    public String getInfo(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("id: ");
+        sb.append(id);
+        sb.append(", имя: ");
+        sb.append(name);
+        sb.append(", пол: ");
+        sb.append(getGender());
+        sb.append(", возраст: ");
+        sb.append(getAge());
+        sb.append(", ");
+        sb.append(getSpouseInfo());
+        sb.append(", ");
+        sb.append(getMotherInfo());
+        sb.append(", ");
+        sb.append(getFatherInfo());
+        sb.append(", ");
+        sb.append(getChildrenInfo());
+        return sb.toString();
+    }
+    public String getSpouseInfo(){
+        String res = "супруг(а): ";
+        if (spouse == null){
+            res += "нет";
+        }
+        else {
+            res += spouse.getName();
+        }
+        return res;
+    }
+    public String getMotherInfo(){
+        String res = "мать: ";
+        Human mother = getMother();
+        if(mother != null){
+            res += mother.getName();
+        }
+        else {
+            res += "неизвестна";
+        }
+        return res;
+    }
+    public String getFatherInfo(){
+        String res = "отец: ";
+        Human father = getFather();
+        if(father != null){
+            res += father.getName();
+        }
+        else {
+            res += "неизвестен";
+        }
+        return res;
+    }
+    public String getChildrenInfo(){
+        StringBuilder res = new StringBuilder();
+        res.append ("дети: ");
+        if(children.size() != 0){
+            res.append(children.get(0).getName());
+            for (int i = 1; i < children.size(); i++) {
+                res.append (", ");
+                res.append(children.get(1).getName());
+            }
+        }
+        else {
+            res.append("отсутствует");
+        }
+        return res.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (!(obj instanceof Human)){
+            return false;
+        }
+        Human human = (Human) obj;
+        return human.getId() == getId();
+    }
 }
+
+
+
+
+
