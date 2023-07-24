7540740b66f2bfb6e4882643d020c251699d9d8a
Xenia-Golb <123086934+Xenia-Golb@users.noreply.github.com>
2023-07-24 23:37:48 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,43 +1,50 @@
 package homeWork;
+// package homeWork.model;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.security.Provider.Service;
-import java.time.LocalDate;
-import java.util.logging.FileHandler;
-import homeWork.FamilyTree.FamilyTree;
-import homeWork.gender.Gender;
-import homeWork.human.Human;
+// import java.io.FileInputStream;
+// import java.io.FileOutputStream;
+// import java.io.IOException;
+// import java.io.ObjectInputStream;
+// import java.io.ObjectOutputStream;
+// import java.io.Serializable;
+// import java.security.Provider.Service;
+// import java.time.LocalDate;
+// import java.util.logging.FileHandler;
+
+// import homeWork.model.FamilyTree.FamilyTree;
+// import homeWork.model.human.Human;
+// import homeWork.model.human.gender.Gender;
 
 public class Main {
     public static void main(String[] args) {
-        Service service = new Service();
-        // FileHandler fileHandler = new FileHandler();
-        // String filePath = "homeWork/fileHandler/tree.out";
-
-        Human andrey = new Human("Андрей", Gender.Male, LocalDate.of(1980, 12, 30),
-                43, 1);
-        service.addHuman();
-        Human maria = new Human("Мария", Gender.Female, LocalDate.of(1985, 9, 15), 38, 2);
-        service.addHuman(maria);
-        Human kristina = new Human("Кристина", Gender.Female, LocalDate.of(2008, 7, 5), 15, 3);
-        service.addHuman(kristina);
-        Human nikita = new Human("Никита", Gender.Male, LocalDate.of(2011, 1, 25), 12, 4);
-        service.addHuman(nikita);
-
-        System.out.println(service.getHumanInfo());
-
-        // fileHandler.save(tree, "homeWork/fileHandler/tree.out");
-
-        service.sortByName();
-        System.out.println(service.getHumanInfo());
-        service.sortByAge();
-        System.out.println(service.getHumanInfo());
 
     }
-
 }
+// Service service = new Service();
+// // FileHandler fileHandler = new FileHandler();
+// // String filePath = "homeWork/fileHandler/tree.out";
+
+// Human andrey = new Human("Андрей", Gender.Male, LocalDate.of(1980, 12, 30),
+// 43, 1);
+// service.addHuman(andrey);
+// Human maria = new Human("Мария", Gender.Female, LocalDate.of(1985, 9, 15),
+// 38, 2);
+// service.addHuman(maria);
+// Human kristina = new Human("Кристина", Gender.Female, LocalDate.of(2008, 7,
+// 5), 15, 3);
+// service.addHuman(kristina);
+// Human nikita = new Human("Никита", Gender.Male, LocalDate.of(2011, 1, 25),
+// 12, 4);
+// service.addHuman();
+
+// System.out.println(service.getHumanInfo());
+
+// // fileHandler.save(tree, "homeWork/fileHandler/tree.out");
+// service.sortByName();
+// System.out.println(service.getHumanInfo());
+// service.sortByAge();
+// System.out.println(service.getHumanInfo());
+
+// }
+
+// }
