6dd811244bf4ae3cba0de5a23c1c145352c10ffa
Winniebob <vilksmax@gmail.com>
2023-07-24 20:08:14 +0300

    Создал Генеологическое древо, добавив возможность читать и записывать его в файл

---
+++ src/genealogy_tree/Main.java
@@ -0,0 +1,22 @@
+package genealogy_tree;
+import genealogy_tree.WriterAndReader.*;
+
+
+
+public class Main {
+    public static void main (String[] args) {
+        /*VeneologiTree geneologiTree = new VeneologiTree();
+        Human1 human1 = new Human1();
+        Human2 human2 = new Human2();
+        Human3 human3 = new Human3();
+
+        geneologiTree.addHuman(human1);
+        geneologiTree.addHuman(human3);
+        geneologiTree.addHuman(human2);*/
+        new Writer();
+        new Reader();
+
+
+    }
+
+}
