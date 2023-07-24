930b98cc857631bf36b8da08c9dfd7965cca3b01
Xenia-Golb <123086934+Xenia-Golb@users.noreply.github.com>
2023-07-24 23:39:38 +0300

    Delete fileHandler directory

--- fileHandler/Writable.java
+++
@@ -1,9 +0,0 @@
-package homeWork.fileHandler;
-
-import java.io.Serializable;
-
-public interface Writable {
-    boolean save(Serializable serializable, String filePath);
-
-    Object read(String filePath);
-}
