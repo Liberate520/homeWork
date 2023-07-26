762bce5c16565a544845711bdc73626d18e29862
unknown <an.techkina@yandex.ru>
2023-07-26 21:01:16 +0400

    add task1

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,29 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test {
+
+    public static StringBuilder getCondition(Map<String, String> params) {
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, String> pair : params.entrySet()) {
+            if (pair.getValue() != null) {
+                if (result.toString().equals("")) {
+                    result.append(pair.getKey() + " = '" + pair.getValue() + "'");
+                } else {
+                    result.append(" and " + pair.getKey() + " = '" + pair.getValue() + "'");
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("name", "Ivanov");
+        params.put("country", "Ukraine");
+        params.put("city", "Kiev");
+        params.put("age", null);
+        System.out.println(getCondition(params));
+    }
 }
