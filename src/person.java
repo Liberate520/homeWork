51aebb4d72ff27a7cee6a1f58f97fe64a000a2c3
Alexander <bulgakov453@gmail.com>
2023-07-30 11:53:41 +0500

    FamilyTree

--- src/person.java
+++ src/person.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class person {
+class Person {
+    private final String firstName;
+    private final String lastName;
+    private int age;
+    private final String gender;
+    private Person child;
+
+    public Person(String firstName, String lastName, int age, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String toString() {
+        String childString = child == null ? "No child" : child.getFirstName() + " " + child.getLastName();
+        return firstName + " " + lastName + " (" + age + ") - " + gender + " - " + childString;
+    }
 }
