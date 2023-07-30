d51f061a4bef2ed02dc4a7e18ad421b163b3c1b5
Xenia-Golb <123086934+Xenia-Golb@users.noreply.github.com>
2023-07-30 20:43:15 +0300

    Add files via upload

--- service/Service.java
+++ service/Service.java
@@ -4,12 +4,15 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 import homeWork.model.FamilyTree.FamilyTree;
+import homeWork.model.fileHandler.FileHandler;
+import homeWork.model.fileHandler.Writable;
 import homeWork.model.human.Human;
 import homeWork.model.human.gender.Gender;
 
-public class Service implements Serializable {
+public class Service implements Serializable, Writable {
     private long id;
     private FamilyTree<Human> familyTree;
+    FileHandler fileHandler = new FileHandler();
 
     public Service() {
         familyTree = new FamilyTree<>();
@@ -43,5 +46,39 @@ public class Service implements Serializable {
     public void addHuman(String name, int age) {
         Human human = new Human(name, age);
         familyTree.addHuman(human);
+    }
+
+    public void saveInFile() {
+        fileHandler.save(familyTree, "/Users/xenia_golb/Desktop/familyTree/homeWork");
+        System.out.println("экспорт (tree.out) завершен успешно");
+
+    }
+
+    public void addChild(String name, int age) {
+        Human human = new Human(name, age);
+        familyTree.addHuman(human);
+    }
+
+    public void addParent(String name, int age) {
+        Human human = new Human(name, age);
+        familyTree.addHuman(human);
+    }
+
+    public void readFromFile() {
+        FamilyTree familyTree = (FamilyTree) fileHandler.read("/Users/xenia_golb/Desktop/familyTree/homeWork");
+        System.out.println("Импорт данных выполнен  >>>>>>  " + familyTree);
+
+    }
+
+    @Override
+    public boolean save(Serializable serializable, String filePath) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public Object read(String filePath) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'read'");
     }
 }
