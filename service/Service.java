3d4ead389388819897ca8991a82aa291226b6623
Xenia-Golb <123086934+Xenia-Golb@users.noreply.github.com>
2023-08-02 14:50:37 +0300

    Add files via upload

--- service/Service.java
+++ service/Service.java
@@ -1,25 +1,22 @@
 package homeWork.model.service;
 
-import java.io.Serializable;
 import java.time.LocalDate;
-
 import homeWork.model.FamilyTree.FamilyTree;
-import homeWork.model.fileHandler.FileHandler;
 import homeWork.model.fileHandler.Writable;
 import homeWork.model.human.Human;
 import homeWork.model.human.gender.Gender;
 
-public class Service implements Serializable, Writable {
-    private long id;
+public class Service {
+    private int id;
     private FamilyTree<Human> familyTree;
-    FileHandler fileHandler = new FileHandler();
+    Writable wr;
 
     public Service() {
         familyTree = new FamilyTree<>();
     }
 
-    public void addHuman(String name, Gender gender, LocalDate ofLocalDate, int age, int id) {
-        Human human = new Human(name, gender, ofLocalDate, age, id++);
+    public void addHuman(String name, Gender gender, LocalDate birthdate, int age, int id) {
+        Human human = new Human(name, gender, birthdate, age, id++);
         familyTree.addHuman(human);
     }
 
@@ -43,42 +40,22 @@ public class Service implements Serializable, Writable {
         familyTree.sortByAge();
     }
 
-    public void addHuman(String name, int age) {
-        Human human = new Human(name, age);
-        familyTree.addHuman(human);
-    }
-
     public void saveInFile() {
-        fileHandler.save(familyTree, "/Users/xenia_golb/Desktop/familyTree/homeWork");
-        System.out.println("экспорт (tree.out) завершен успешно");
-
+        wr.save(familyTree, "/Users/xenia_golb/Desktop/familyTree/homeWork");
     }
 
     public void addChild(String name, int age) {
-        Human human = new Human(name, age);
+        Human human = new Human(name, null, null, age, id);
         familyTree.addHuman(human);
     }
 
     public void addParent(String name, int age) {
-        Human human = new Human(name, age);
+        Human human = new Human(name, null, null, age, id);
         familyTree.addHuman(human);
     }
 
     public void readFromFile() {
-        FamilyTree familyTree = (FamilyTree) fileHandler.read("/Users/xenia_golb/Desktop/familyTree/homeWork");
-        System.out.println("Импорт данных выполнен  >>>>>>  " + familyTree);
+        FamilyTree familyTree = (FamilyTree) wr.read("/Users/xenia_golb/Desktop/familyTree/homeWork");
 
-    }
-
-    @Override
-    public boolean save(Serializable serializable, String filePath) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
-    }
-
-    @Override
-    public Object read(String filePath) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'read'");
     }
 }
