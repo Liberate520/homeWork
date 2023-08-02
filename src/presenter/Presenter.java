6e1002c2a53da3f0c86b3afcc9ea8675646f3a5b
Egor Menshikov <r3born.fs@gmail.com>
2023-08-02 13:21:08 +0400

    added Service.java

--- src/presenter/Presenter.java
+++ src/presenter/Presenter.java
@@ -1,9 +1,20 @@
 package presenter;
 
 import model.familyTree.FamilyTree;
+import model.familyTree.TreeNode;
+import model.service.Service;
 import view.View;
 
 public class Presenter {
     private View view;
-    private FamilyTree service;
+    private Service service;
+
+    public Presenter(View view) {
+        this.view = view;
+        this.service = new Service();
+    }
+
+    public void addPerson (TreeNode person) {
+        service.addPerson(person);
+    }
 }
--- src/view/ConsoleUI.java
+++ src/view/ConsoleUI.java
@@ -1,5 +1,33 @@
 package view;
 
-public class ConsoleUI {
+import model.person.Gender;
+import presenter.Presenter;
 
+import java.util.Scanner;
+
+public class ConsoleUI implements View {
+
+    private Presenter presenter;
+    private Scanner scanner;
+
+    public ConsoleUI() {
+        this.scanner = new Scanner(System.in);
+        this.presenter = new Presenter(this);
+    }
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void printOutput(String text) {
+        System.out.println(text);
+    }
+
+    public void addPerson () {
+        String name = scanner.nextLine();
+        String surname = scanner.nextLine();
+        String patronymic = scanner.nextLine();
+
+    }
 }
