14d595f62cea1deb710440c9b68d97df9f8b79e4
xurshed_2016@mail.ru <xurshed_2016@mail.ru>
2023-08-02 09:35:05 +0300

    update and added new elements

--- FileHandler.java
+++
@@ -1,9 +0,0 @@
-package homeWork;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface FileHandler {
-    void writeToFile(List<Human> humans, String filename) throws IOException;
-    List<Human> readFromFile(String filename) throws IOException;
-}
--- Main.java
+++ Main.java
@@ -1,60 +1,25 @@
 package homeWork;
 
-import homeWork.Human;
-import homeWork.Tree;
-import java.time.LocalDate;
-import java.util.List;
+import familytree.Tree;
+import filehandling.FamilyTreeFileHandler;
+import filehandling.FileHandler;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         Tree tree = new Tree();
 
         // Adding members to the family tree
-        LocalDate johnDateOfBirth = LocalDate.of(1988, 5, 15);
-        Human john = new Human("John", johnDateOfBirth, Human.Gender.MALE);
-
-        LocalDate maryDateOfBirth = LocalDate.of(1990, 8, 25);
-        Human mary = new Human("Mary", maryDateOfBirth, Human.Gender.FEMALE);
-
-        LocalDate aliceDateOfBirth = LocalDate.of(2010, 2, 3);
-        Human alice = new Human("Alice", aliceDateOfBirth, Human.Gender.FEMALE);
-
-        LocalDate bobDateOfBirth = LocalDate.of(2012, 11, 20);
-        Human bob = new Human("Bob", bobDateOfBirth, Human.Gender.MALE);
-
-        LocalDate chrisDateOfBirth = LocalDate.of(2015, 9, 8);
-        Human chris = new Human("Chris", chrisDateOfBirth, Human.Gender.MALE);
-
-        tree.addMember(john);
-        tree.addMember(mary);
-        tree.addChildToParent("John", alice);
-        tree.addChildToParent("John", bob);
-        tree.addChildToParent("John", chris);
-        tree.addChildToParent("Mary", alice);
-        tree.addChildToParent("Mary", bob);
-        tree.addChildToParent("Mary", chris);
-
-        // Research: Finding all children of a selected person
-        String selectedPerson = "John";
-        List<Human> children = tree.findChildren(selectedPerson);
-
-        if (children != null && !children.isEmpty()) {
-            System.out.print("The children of " + selectedPerson + " are: ");
-            for (Human child : children) {
-                System.out.print(child.getName() + ", ");
-            }
-            System.out.println();
-        } else {
-            System.out.println(selectedPerson + " is not in the family tree.");
-        }
 
         // File handling
         FileHandler fileHandler = new FamilyTreeFileHandler();
-        fileHandler.writeToFile(tree.getAllMembers(), "family_tree_data.txt");
-        List<Human> restoredHumans = fileHandler.readFromFile("family_tree_data.txt");
-        System.out.println("Restored family tree data:");
-        for (Human human : restoredHumans) {
-            System.out.println("Name: " + human.getName() + ", Date of Birth: " + human.getDateOfBirth() + ", Gender: " + human.getGender());
+        try {
+            fileHandler.writeToFile(tree, "family_tree_data.txt");
+            Tree restoredTree = fileHandler.readFromFile("family_tree_data.txt");
+            System.out.println("Restored family tree data:");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
