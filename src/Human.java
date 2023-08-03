7952d72e09e90b47c6a3c23c1e50cf3f03451983
DenisAFW <denis.afinogenow@yandex.ru>
2023-08-03 18:12:40 +0300

    Разделил на классы

---
+++ src/Human.java
@@ -0,0 +1,26 @@
+
+public class Human {
+    public String name;
+    public int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/Relationship.java
@@ -0,0 +1,3 @@
+public enum Relationship {
+    parent, children, wife, husband
+}
