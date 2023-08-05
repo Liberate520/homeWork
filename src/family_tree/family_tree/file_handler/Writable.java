10d875c14fcf695c3e4c984c5cf82ce9728c2b43
Hayjk <tosts71267@gmail.com>
2023-08-05 20:05:55 +0300

    end

--- src/family_tree/family_tree/file_handler/Writable.java
+++
@@ -1,8 +0,0 @@
-package family_tree.family_tree.file_handler;
-
-import java.io.Serializable;
-
-public interface Writable {
-    void save(String path, Serializable obj);
-    Serializable load(String path);
-}
--- src/family_tree/human/Gender.java
+++
@@ -1,5 +0,0 @@
-package family_tree.human;
-
-public enum Gender {
-    Male, Female
-}
