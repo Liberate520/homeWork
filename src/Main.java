8486022288fcc76ff248d3f75d3dbf2b85b384d8
Egor Menshikov <r3born.fs@gmail.com>
2023-08-07 10:53:01 +0400

    cleaned up main

--- src/Main.java
+++ src/Main.java
@@ -3,40 +3,8 @@ import view.ConsoleUI;
 
 public class Main {
     public static void main(String[] args) {
-//        Service<Person> service = makeTree();
-//        System.out.println(service.printTree());
-//        System.out.println(service.sortByAge());
         ConsoleUI view = new ConsoleUI();
         view.start();
     }
-
-//    public static Service<Person> makeTree() {
-//        Service<Person> service = new Service<>();
-//        Person egor = new Person("Егор", "Меньшиков", "Леонидович",
-//                Gender.MALE, LocalDate.of(1986, 3, 13));
-//        Person petr = new Person("Петр", "Меньшиков", "Леонидович",
-//                Gender.MALE, LocalDate.of(1988, 8, 11));
-//        Person marina = new Person("Марина", "Меньшикова", "Рудольфовна",
-//                Gender.FEMALE, LocalDate.of(1957, 11, 30));
-//        Person leonid = new Person("Леонид", "Меньшиков", "Иеронимович",
-//                Gender.MALE, LocalDate.of(1954, 10, 18));
-//        service.addPerson(egor);
-//        service.addPerson(petr);
-//        service.addPerson(leonid);
-//        service.addPerson(marina);
-//        service.putConnection(egor, ConnectionType.PARENT, marina);
-//        service.putConnection(egor, ConnectionType.SIBLING, petr);
-//        service.putConnection(egor, ConnectionType.PARENT, leonid);
-//        service.putConnection(marina, ConnectionType.SPOUSE, leonid);
-//        service.putConnection(marina, ConnectionType.CHILD, petr);
-//        service.putConnection(leonid, ConnectionType.CHILD, petr);
-//        return service;
-//    }
-//
-//    public static Service<Person> readTree() {
-//        Service<Person> result = new Service<>();
-//        result.read();
-//        return result;
-//    }
 }
 
