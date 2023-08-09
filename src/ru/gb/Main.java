a21afde204d74e5857ce9fbd112fa0e806df9baf
Leonid Malyshev <lokiwish@list.ru>
2023-08-09 23:15:30 +0300

    Удалил лишний код с Main

--- src/ru/gb/Main.java
+++ src/ru/gb/Main.java
@@ -9,36 +9,5 @@ public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         View view = new ConsoleUI();
         view.start();
-
-        /*
-        FamilyTree<Human> familyTree = new FamilyTree<>();
-        LocalDate birthday1 = LocalDate.of(1970, 5, 20);
-        Human Sergey = new Human("Sergey", birthday1, Gender.Male);
-        LocalDate birthday4 = LocalDate.of(1972, 3, 18);
-        Human Yulia = new Human("Yulia", birthday4, Gender.Female);
-
-
-        LocalDate birthday2 = LocalDate.of(2015, 2, 15);
-        Human Sasha = new Human("Sasha", birthday2, Gender.Female);
-        LocalDate birthday3 = LocalDate.of(2012, 2, 15);
-        Human Ivan = new Human("Ivan", birthday3, Gender.Male);
-        LocalDate birthday5 = LocalDate.of(1996, 2, 12);
-        Human Daniil = new Human("Daniil", birthday5, Gender.Male);
-
-        familyTree.addHuman(Daniil);
-
-        familyTree.setConnection(Sasha, Sergey);
-        familyTree.setConnection(Sasha, Yulia);
-        familyTree.setConnection(Ivan, Sergey);
-        familyTree.setConnection(Ivan, Yulia);
-
-        System.out.println(familyTree);
-         */
-
-        /*
-        FileHandler handler = new FileHandler();
-        handler.save(familyTree);
-        handler.load();
-         */
     }
 }
