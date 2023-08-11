0b6414f0f5efb636122cdbc0c1597b3dcd38c8a7
Sergey <saizotov1@gmail.com>
2023-08-11 20:45:51 +0500

    mvp 5 commit

--- family_tree/view/ConsoleUI.java
+++ family_tree/view/ConsoleUI.java
@@ -4,7 +4,7 @@ import homeWork.family_tree.presenter.Presenter;
 
 import java.util.Scanner;
 
-public class ConsoleUI implements View{
+public class ConsoleUI implements View {
     private static final String errorInput = "Вы ввели неверное значение";
     private Presenter presenter;
     private final Scanner scanner;
@@ -19,15 +19,24 @@ public class ConsoleUI implements View{
     }
 
     @Override
+    public void printAnswer(String text) {
+        System.out.println(text);
+    }
+
+    @Override
     public void start() {
         hello();
-        while (work){
+        while (work) {
             printMenu();
             execute();
-            }
         }
+    }
 
-    public void addHuman(){
+    private void hello() {
+        System.out.println("Доброго времени суток!");
+    }
+
+    public void addHuman() {
         System.out.println("Укажите имя человека");
         String name = scanner.nextLine();
         System.out.println("Укажите возраст человека");
@@ -36,52 +45,52 @@ public class ConsoleUI implements View{
         presenter.addHuman(name, String.valueOf(birthDate));
     }
 
-    private void hello(){
-        System.out.println("Доброго времени суток!");
-    }
-    @Override
-    public void printAnswer(String text) {
-        System.out.println(text);
-    }
-    public void getHumanInfo(){
+    public void getHumanInfo() {
         presenter.getHumanInfo();
     }
-    public void sortByName(){
+
+    public void sortByName() {
         presenter.sortByName();
     }
-    public void sortByBirthDate(){
+
+    public void sortByBirthDate() {
         presenter.sortByBirthDate();
     }
+
     public void errorInput() {
         System.out.println("Вы ввели некорректное значение");
     }
+
     public void finish() {
         System.out.println("До новых встреч!");
         work = false;
     }
-    private void execute(){
+
+    private void execute() {
         String line = scanner.nextLine();
-        if (checkTextForInt(line)){
+        if (checkTextForInt(line)) {
             int numCommand = Integer.parseInt(line);
-            if (checkCommand(numCommand)){
+            if (checkCommand(numCommand)) {
                 menu.execute(numCommand);
             }
         }
     }
-    private boolean checkTextForInt(String text){
-        if (text.matches("[0-9]+")){
+
+    private boolean checkTextForInt(String text) {
+        if (text.matches("[0-9]+")) {
             return true;
         } else {
             errorInput();
             return false;
         }
     }
-    private void printMenu(){
+
+    private void printMenu() {
         System.out.println(menu.menu());
     }
 
-    private boolean checkCommand(int numCommand){
-        if (numCommand < menu.getSize()){
+    private boolean checkCommand(int numCommand) {
+        if (numCommand < menu.getSize()) {
             return true;
         } else {
             errorInput();
