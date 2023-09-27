bd3ddd218fdabbf46328fe6a1a89b60db347fcc3
Egor.S <Egorsoliakov@gmail.com>
2023-09-27 15:23:26 +0400

    Переделанно все для первого дз

--- src/ru/gb/family_tree/Main1.java
+++ src/ru/gb/family_tree/Main1.java
@@ -3,29 +3,52 @@ package ru.gb.family_tree;
 import ru.gb.family_tree.Family.Gender;
 import ru.gb.family_tree.Family.Human;
 import ru.gb.family_tree.Tree.FamilyTree;
-import ru.gb.family_tree.Tree.Parent;
+
+import java.time.LocalDate;
 
 public class Main1 {
     public static void main(String[] args) {
-        FamilyTree familyTree = new FamilyTree();
-        // Human notHuman = null;
-        Human human1 = new Human("Петрова Наталья Ивановна", Gender.Female,"12.12.1945", "11.11.2010");
+        FamilyTree tree = test();
+
+/*      Human human1 = new Human("Петрова Наталья Ивановна", Gender.Female,"12.12.1945", "11.11.2010");
         Human human2 = new Human("Петров Михаил Сергеевич", Gender.Male,"12.06.1945", "21.12.2011");
         Human human3 = new Human("Петров Сергей  Михайлович", Gender.Male, "12.12.1967", "No");
         Human human4 = new Human("Иванова Анна Михайловна", Gender.Female,"12.12.1967","No");
         Human human5 = new Human("Куликова Елена Анатольевна", Gender.Female,"12.10.1964","No");
         Human human6 = new Human("Иванов Aндрей Юрьевич", Gender.Male, "12.10.1964", "No");
         Human human7 = new Human("Петров Сергей Сергеевич", Gender.Male, "11.02.1989", "No");
+*/
+        System.out.println(tree);
+    }
+    static FamilyTree test(){
+        FamilyTree tree  = new FamilyTree();
+        Human human1 = new Human("Петрова Наталья Ивановна", Gender.Female,
+                LocalDate.of(1937, 12, 12), LocalDate.of(2010, 11, 11));
+        Human human2 = new Human("Петров Михаил Сергеевич", Gender.Male,
+                LocalDate.of(1933, 6, 12),LocalDate.of(2011,12,21));
+        Human human3 = new Human("Петров Сергей  Михайлович", Gender.Male,
+                LocalDate.of(1967, 12, 12), human1, human2);
+        Human human4 = new Human("Иванова Анна Михайловна", Gender.Female,
+                LocalDate.of(1967, 6, 12), human1, human2);
+        Human human5 = new Human("Куликова Елена Анатольевна", Gender.Female,
+                LocalDate.of(1964, 10, 12));
+        Human human6 = new Human("Иванов Aндрей Юрьевич", Gender.Male,
+                LocalDate.of(1964, 2, 10));
+        Human human7 = new Human("Петров Сергей Сергеевич", Gender.Male,
+                LocalDate.of(1989, 2, 10), human5, human3);
 
-        familyTree.addHuman(human1);
-        familyTree.addHuman(human2);
-        familyTree.addHuman(human3);
-        familyTree.addHuman(human4);
-        familyTree.addHuman(human5);
-        familyTree.addHuman(human6);
-        familyTree.addHuman(human7);
-        //List<Human> children1 = List.of(human3, human4);
-        //List<Human> children2 = List.of(human7);
-        System.out.println(familyTree);
+        tree.add(human1);
+        tree.add(human2);
+        tree.add(human3);
+        tree.add(human4);
+        tree.add(human5);
+        tree.add(human6);
+        tree.add(human7);
+
+        tree.setWedding(human1, human2);
+        tree.setWedding(human3, human5);
+        tree.setWedding(human4, human6);
+
+        return tree;
     }
 }
--- src/ru/gb/family_tree/Tree/FamilyTree.java
+++ src/ru/gb/family_tree/Tree/FamilyTree.java
@@ -6,10 +6,98 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FamilyTree {
+    private long humanId;
     private List<Human> humans;
 
     public FamilyTree() {
-        humans = new ArrayList<>();
+        this(new ArrayList<>());
+    }
+
+    public FamilyTree(List<Human> humans) {
+        this.humans = humans;
+    }
+
+    public boolean add(Human human) {
+        if (human == null){
+            return false;
+        }
+        if(!humans.contains(human)) {
+            humans.add(human);
+            human.setId(humanId++);
+            addToParents(human);
+            addToChildren(human);
+            return true;
+        }
+        return false;
+    }
+
+    public Human getById(long id) {
+        for (Human human : humans) {
+            if (human.getId() == id) {
+                return human;
+            }
+        }
+        return null;
+    }
+    private void addToParents(Human human) {
+        Human mother = human.getMother();
+        Human father = human.getFather();
+        if (mother!= null) {
+            mother.addChild(human);
+        }
+        if (father!= null) {
+            father.addChild(human);
+        }
+    }
+    private void addToChildren(Human human) {
+        for (Human child : human.getChildren()) {
+            child.addParent(human);
+        }
+    }
+    public List<Human> getSiblings(int id) {
+        Human human = getById(id);
+        if (human == null) {
+            return null;
+        }
+        List<Human> siblings = new ArrayList<>();
+        Human mother = human.getMother();
+        Human father = human.getFather();
+
+        for(Human child : mother.getChildren()){
+            if(!child.equals(human)){
+                siblings.add(child);
+            }
+        }
+        for(Human child : father.getChildren()){
+            if(!child.equals(human) && siblings.contains(child)){
+                siblings.add(child);
+            }
+        }
+    return siblings;
+    }
+
+    public boolean setWedding(long humanId1, long humanId2){
+        if(checkId(humanId1) && checkId(humanId2)){
+            Human human1 = getById(humanId1);
+            Human human2 = getById(humanId2);
+            return setWedding(human1, human2);
+        }
+        return false;
+    }
+
+    public boolean setWedding(Human human1, Human human2){
+        if(human1.getSpouse() == null && human2.getSpouse() == null){
+            human1.setSpouse(human2);
+            human2.setSpouse(human1);
+            return true;
+        }
+        return false;
+    }
+    private boolean checkId(long id){
+        if(id >= humanId || id < 0){
+            return false;
+        }
+        return true;
     }
 
     public void addHuman(Human human) {
