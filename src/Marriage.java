abe5aa1592aaa3f6e252cc423510286176d89946
Sveta <svetik.2007@list.ru>
2023-09-27 22:52:47 +0300

    09-27-23

---
+++ src/Marriage.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+//  Браки
+public class Marriage {
+    private double id;
+    private Date dateMarriage;
+    private Date dateDivorce;
+    private Human firstHuman;
+    private Human secondHuman;
+}
