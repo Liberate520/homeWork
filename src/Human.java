b624e9af2c9978831df8d39208976e47cb5f22a4
vladmiass <vladmiass01012001@gmail.com>
2023-10-14 21:28:32 +0500

    Осваиваю связку githab и IntelliJ

---
+++ src/Human.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+import java.util.List;
+
+public class Human {
+    String name;
+    LocalDate birthday;
+    Gender gender;
+    Human mother;
+    List<Human> children;
+}
