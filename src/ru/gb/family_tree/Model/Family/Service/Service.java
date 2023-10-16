8e96de8d4aee326da82a4d58728234ebbd33709e
Egor.S <Egorsoliakov@gmail.com>
2023-10-16 13:43:19 +0400

    Исправлены все замечания

--- src/ru/gb/family_tree/Model/Family/Service/Service.java
+++ src/ru/gb/family_tree/Model/Family/Service/Service.java
@@ -6,10 +6,8 @@ import ru.gb.family_tree.Model.Family.Service.writer.FileHandler;
 
 public class Service{
     private FamilyTree familyTree;
-    private FileHandler fileHandler;
     public Service() {
         familyTree = CreationTreeHuman.addTree();
-        fileHandler = new FileHandler();
     }
     public void saverTree(){
         SaverReader.save(familyTree);
