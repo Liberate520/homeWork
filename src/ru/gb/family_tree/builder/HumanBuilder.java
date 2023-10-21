7233e1d1d169bbc148e0f5d7a7d003f93113dbb6
Jane-547 <Jane-547@bk.com>
2023-10-21 22:13:16 +0300

    One of unnecessary imports deleted

--- src/ru/gb/family_tree/builder/HumanBuilder.java
+++ src/ru/gb/family_tree/builder/HumanBuilder.java
@@ -2,10 +2,8 @@ package ru.gb.family_tree.builder;
 
 import ru.gb.family_tree.human.Gender;
 import ru.gb.family_tree.human.Human;
-import ru.gb.family_tree.tree.FamilyTree;
 
 public class HumanBuilder {
-
 
     public Human build (String lastname, String name, Gender gender) {
         Human human = new Human(lastname, name, gender);
