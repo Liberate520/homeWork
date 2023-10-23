d8130493fb3257592a4bbb666ea5d0d3272c4e22
Artem Fedorov <fyod-artem@mail.ru>
2023-10-23 22:21:30 +0200

    Homework 1

---
+++ src/FamilyTree.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+    private int id;
+    private String family;
+    private List<Human> membersList;
+    int memberId = 1;
+
+
+    public FamilyTree(String family) {
+        this.family = family;
+        membersList = new ArrayList<>();
+    }
+
+    public void addMembers(Human human) {
+        human.setId(memberId++);
+        membersList.add(human);
+    }
+
+    public String getFamilyInfo(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Список родословной:\n");
+        for (Human human: membersList){
+            stringBuilder.append(human);
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+public class Main {
+    public static void main(String[] args) {
+        FamilyTree familyTree = new FamilyTree("Fedorov's");
+
+        Human father = new Human("Федоров", "Николай", Gender.Male);
+        father.setBirthday(LocalDate.of(1973, 1, 9));
+        System.out.println(father);
+        familyTree.addMembers(father);
+
+        Human mother = new Human("Федорова", "Елена", Gender.Female);
+        mother.setBirthday(LocalDate.of(1977, 8, 16));
+        System.out.println(mother);
+        familyTree.addMembers(mother);
+
+        Human me = new Human("Федоров", "Артем", Gender.Female);
+        me.setBirthday(LocalDate.of(1998, 9, 16));
+        System.out.println(me);
+        familyTree.addMembers(me);
+        father.setChild(me);
+        mother.setChild(me);
+        System.out.println(father.getChildren());
+        System.out.println(me.getParents());
+
+        Human sister = new Human("Федорова", "Полина", Gender.Female);
+        sister.setBirthday(LocalDate.of(2006, 2, 12));
+        System.out.println(sister);
+        familyTree.addMembers(sister);
+        father.setChild(sister);
+        mother.setChild(sister);
+        System.out.println(father.getChildren());
+        System.out.println(sister.getParents());
+    }
+}
