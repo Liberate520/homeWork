7922b1df22c5f67a79189821978eb1a6f9ad931a
Yuriy Emelin <yuem-1@yandex.ru>
2023-10-30 22:30:48 +0300

    ll

--- src/fam_tree/FamilyTree.java
+++ src/fam_tree/FamilyTree.java
@@ -1,4 +1,67 @@
-package fam_tree;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FamilyTree {
+
+    private long humansId;
+
+    public List<Human> getHumanList() {
+        return humanList;
+    }
+
+    public void setHumanList(List<Human> humanList) {
+        this.humanList = humanList;
+    }
+
+    private List<Human> humanList;
+
+
+    public FamilyTree() {
+        this(new ArrayList<>());
+    }
+
+
+
+    public FamilyTree(List<Human> humanList) {
+        this.humanList = humanList;
+    }
+
+    public long getHumansId() {
+        return humansId;
+    }
+
+    public void setHumansId(long humansId) {
+        this.humansId = humansId;
+    }
+
+    @Override
+    public String toString() {
+        return "FamilyTree{" +
+                "humansId=" + humansId +
+                ", humanList=" + humanList +
+                '}';
+    }
+
+    public boolean add(Human human) {
+        if (human == null) {
+            return false;
+        }
+        if (!humanList.contains(human)) {
+            humanList.add(human);
+            human.setId(humansId);
+
+            addToParents(human);
+            voidaddToChildren(human);
+
+            return true;
+
+        }
+        return false;
+    }
+
+    private void addToChildren(Human human) {
+    }
+
+    private void addToParents(Human human) {
+    }
 }
--- src/fam_tree/Main.java
+++ src/fam_tree/Main.java
@@ -1,4 +1,31 @@
-package fam_tree;
+
+
+
+
 
 public class Main {
+    public static void main(String[] args) {
+        FamilyTree tree = testTree();
+        System.out.println(tree);
+
+    }
+
+    static FamilyTree testTree() {
+        FamilyTree tree = new FamilyTree();
+
+        Human vasya = new Human("Vasya", Gender.Male);
+
+        tree.add(vasya);
+        Human masha = new Human("masha", Gender.Female);
+        tree.add(masha);
+        Human children = new Human("Vasya", Gender.Male);
+        tree.add(children);
+        Human semyon = new Human("Vasya", Gender.Male);
+        tree.add(semyon);
+
+
+        return tree;
+    }
+
+
 }
