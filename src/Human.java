e7b3412f52641db7fa50b7247e45ca00472ad254
yourenoticed <yourenoticed@gmail.com>
2023-11-21 17:04:01 +0700

    added equals method, protected setters

--- src/Human.java
+++ src/Human.java
@@ -1,19 +1,20 @@
-import java.sql.Date;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Human {
+    private long id;
     private String firstName;
     private String lastName;
-    private Date birthDate;
     private Sex sex;
+    private LocalDate birthDate;
+    private LocalDate deathDate;
     private Human mother, father;
     private List<Human> children;
-    private Date deathDate;
 
-    public Human(String firstName, String lastName, Date birthDate, Sex sex, Human mother, Human father, List<Human> children, Date deathDate) {
+    public Human(String firstName, String lastName, LocalDate birthDate, Sex sex, Human mother, Human father, List<Human> children, LocalDate deathDate) {
+        this.id = 0; 
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthDate = birthDate;
@@ -24,16 +25,20 @@ public class Human {
         this.deathDate = deathDate;
     }
     
-    public Human(String firstName, String lastName, Date birthDate, Sex sex, Human mother, Human father, List<Human> children) {
+    public Human(String firstName, String lastName, LocalDate birthDate, Sex sex, Human mother, Human father, List<Human> children) {
         this(firstName, lastName, birthDate, sex, mother, father, children, null);
     }
 
-    public Human(String firstName, String lastName, Date birthDate, Sex sex, Human mother, Human father) {
+    public Human(String firstName, String lastName, LocalDate birthDate, Sex sex, Human mother, Human father) {
         this(firstName, lastName, birthDate, sex, mother, father, new ArrayList<Human>(), null);
     }
 
-    public Human(String firstName, String lastName, Date birthDate, Sex sex) {
+    public Human(String firstName, String lastName, LocalDate birthDate, Sex sex) {
         this(firstName, lastName, birthDate, sex, null, null, new ArrayList<Human>(), null);
+    }
+
+    public long getId() {
+        return this.id;
     }
 
     public String getFirstName() {
@@ -44,11 +49,11 @@ public class Human {
         return this.lastName;
     }
 
-    public Date getBirthDate() {
+    public LocalDate getBirthDate() {
         return this.birthDate;
     }
 
-    public Date getDeathDate() {
+    public LocalDate getDeathDate() {
         return this.deathDate;
     }
 
@@ -56,7 +61,7 @@ public class Human {
         return this.father;
     }
 
-    public Sex getGender() {
+    public Sex getSex() {
         return this.sex;
     }
 
@@ -68,12 +73,20 @@ public class Human {
         return this.children;
     }
 
-    public void addChild(Human child) {
-        this.children.add(child);
+    public boolean addChild(Human child) {
+        if (!children.contains(child)) {
+            this.children.add(child);
+            return true;
+        }
+        return false;
     }
 
-    public void setDeathDate(Date deathDate) {
-        this.deathDate = deathDate;
+    public boolean setDeathDate(LocalDate deathDate) {
+        if (this.deathDate != null) {
+            this.deathDate = deathDate;
+            return true;
+        }
+        return false;
     }
 
     public void setFirstName(String firstName) {
@@ -84,12 +97,20 @@ public class Human {
         this.lastName = lastName;
     }
 
-    public void setMother(Human mother) {
-        this.mother = mother;
+    public boolean setMother(Human mother) {
+        if (this.mother == null) {
+            this.mother = mother;
+            return true;
+        }
+        return false;
     }
 
-    public void setFather(Human father) {
-        this.father = father;
+    public boolean setFather(Human father) {
+        if (this.father == null) {
+            this.father = father;
+            return true;
+        }
+        return false;
     }
 
     public int getChildrenAmount() {
@@ -99,33 +120,51 @@ public class Human {
     public int getAge() {
         LocalDate compareDate;
         if (this.deathDate == null) compareDate = LocalDate.now();
-        else compareDate = this.deathDate.toLocalDate();
+        else compareDate = this.deathDate;
 
-        LocalDate birthLocalDate = this.birthDate.toLocalDate();
-        return Period.between(birthLocalDate, compareDate).getYears();  
+        return Period.between(this.birthDate, compareDate).getYears();  
     }
 
-    public List<String> getAllChildrensFullNames() {
-        List<String> childrenFullNames = new ArrayList<>();
-        for (Human child : this.children) {
-            childrenFullNames.add(child.getFirstName() + " " + child.getLastName());
-        }
-        return childrenFullNames;
+    public String getFullName() {
+        StringBuilder fullName = new StringBuilder();
+        fullName.append(this.firstName).append(" ")
+            .append(this.lastName);
+        return fullName.toString();
     }
 
-    public List<String> getAllChildrensFirstNames() {
-        List<String> childrenFirstNames = new ArrayList<>();
+    private String getChildrenNames() {
+        StringBuilder childrenNames = new StringBuilder();
         for (Human child : this.children) {
-            childrenFirstNames.add(child.getFirstName());
+            childrenNames.append(child.getFullName()).append("\n");
         }
-        return childrenFirstNames;
+        return childrenNames.toString();
+    }
+    
+    private String getInfo() {
+        StringBuilder info = new StringBuilder();
+        info.append("id: ").append(this.id)
+        .append("\nfirst name: ").append(this.firstName)
+        .append("\nlast name: ").append(this.lastName)
+        .append("\nsex: ").append(this.sex)
+        .append("\nbirth date: ").append(this.birthDate.toString())
+        .append("\ndeath date: ").append(this.deathDate.toString())
+        .append("\nmother: ").append(this.mother.getFullName())
+        .append("\nfather: ").append(this.father.getFullName())
+        .append("\nchildren: ").append(this.getChildrenNames());
+        return info.toString();
     }
 
-    public List<String> getAllChildrensLastNames() {
-        List<String> childrenLastNames = new ArrayList<>();
-        for (Human child : this.children) {
-            childrenLastNames.add(child.getLastName());
+    @Override
+    public String toString() {
+        return this.getInfo();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Human) {
+            Human e = (Human) obj;
+            return this.id == e.getId();
         }
-        return childrenLastNames;
+        return false;
     }
 }
