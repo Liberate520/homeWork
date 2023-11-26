1319756fc9753867acc62b9525293d30ae9ccc6a
PtycaOo <DenKuzia5155@gmail.com>
2023-11-26 23:13:27 +0300

    Create Main.java

---
+++ src/Human.java
@@ -0,0 +1,73 @@
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Human {
+    String name;
+    int age;
+    Human Mother,Father;
+    List<Human> child;
+
+    LocalDate Ld;
+
+    public Human(String name, int age) {
+        child = new ArrayList<>();
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Human getMother() {
+        return Mother;
+    }
+
+    public void setMother(Human mother) {
+        Mother = mother;
+    }
+
+    public Human getFather() {
+        return Father;
+    }
+
+    public void setFather(Human father) {
+        Father = father;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", Mother=" + Mother +
+                ", Father=" + Father +
+                '}';
+    }
+
+    public void addChildren(Human human){
+        child.add(human);
+    }
+
+    public List<Human> getChild() {
+        return child;
+    }
+
+    public void setChild(List<Human> child) {
+        this.child = child;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Human human1 = new Human("Никита", 33);
+        Human human2 = new Human("Георгий", 32);
+        human1.addChildren(human2);
+        System.out.println(human1.getChild());
     }
 }
