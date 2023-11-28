72ee477ddeada9095479bb75bbe3f3b7d1188269
PtycaOo <Denkuzia5155@gmail.com>
2023-11-28 13:21:40 +0300

    Delete Parents

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,8 @@ public class Main {
         FamilyTree familyTree = new FamilyTree();
         Human nikita = new Human("Никита",32);
         Human gosha = new Human("Георгий",31);
-        Parent yulia = new Parent("Юлия",50);
-        Parent kostya = new Parent("Костя",49);
+        Human yulia = new Human("Юлия",50);
+        Human kostya = new Human("Костя",49);
 
-
-        yulia.addChildren(nikita);
-        yulia.addChildren(gosha);
-        familyTree.addHumanInTree(yulia);
-        familyTree.addHumanInTree(nikita);
-        familyTree.addHumanInTree(gosha);
-        familyTree.addHumanInTree(kostya);
-        System.out.println(familyTree.getFamilyTree());
-        System.out.println(yulia.getChild());
     }
 }
