3d52edf125164bea49a6ed77b30512ff05538fe9
PtycaOo <DenKuzia5155@gmail.com>
2023-11-29 20:58:45 +0300

    Fix Class Human

--- src/Human.java
+++ src/Human.java
@@ -1,55 +1,104 @@
 import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Human {
-    String name;
-    int age;
-    Gender gender;
-    Human Mother,Father;
-    List<Human> child;
+    private String name;
+    private Gender gender;
+    private Human mother, father;
+    private List<Human> child;
+    private LocalDate Ld;
 
-    LocalDate Ld;
-    Gender gender;
-
-    public Human(String name, int age) {
+    public Human(String name,Gender gender, LocalDate Ld) {
         child = new ArrayList<>();
+        this.gender = gender;
         this.name = name;
-        this.age = age;
+        this.Ld = Ld;
     }
 
+    public Human(String name, Gender gender, Human mother, Human father, LocalDate ld) {
+        child = new ArrayList<>();
+        this.name = name;
+        this.gender = gender;
+        this.mother = mother;
+        this.father = father;
+        Ld = ld;
+    }
 
     public String getName() {
         return name;
     }
 
-
-    public int getAge() {
-        return age;
+    public void setName(String name) {
+        this.name = name;
     }
 
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Human getMother() {
+        return mother;
+    }
 
     public void setMother(Human mother) {
-        Mother = mother;
+        this.mother = mother;
     }
 
+    public Human getFather() {
+        return father;
+    }
 
     public void setFather(Human father) {
-        Father = father;
+        this.father = father;
     }
-
-    @Override
-    public String toString() {
-        return name + " " + age;
-    }
-
 
     public List<Human> getChild() {
+
         return child;
     }
 
     public void setChild(List<Human> child) {
         this.child = child;
     }
+
+    public LocalDate getLd() {
+        return Ld;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Имя: ");
+        sb.append(name).append(" ");
+        sb.append("Дата рождения: ");
+        sb.append(Ld).append(" ");
+        sb.append("\n");
+        if(!(child.size() == 0)){
+            for (Human human : child) {
+                if(human.getGender() == Gender.Mail) {
+                    sb.append("Сын: ");
+                } else {
+                    sb.append("Дочь: ");
+                }
+                sb.append(human.getName() +" Дата рождения:" + human.getLd() + "\n");
+            }
+
+        }
+        return sb.toString();
+    }
+
+    public boolean addChildren(Human human){
+        if(!child.contains(human)) {
+            child.add(human);
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         FamilyTree familyTree = new FamilyTree();
-        Human nikita = new Human("Никита",32);
-        Human gosha = new Human("Георгий",31);
-        Human yulia = new Human("Юлия",50);
-        Human kostya = new Human("Костя",49);
+        Human yulia = new Human("Юлия",Gender.Female,LocalDate.of(1981,4,12));
+        Human kostya = new Human("Костя",Gender.Mail,LocalDate.of(1982,2,2));
+        Human nikita = new Human("Никита",Gender.Female,yulia,kostya,LocalDate.of(1991,7,3));
+        Human gosha = new Human("Георгий",Gender.Mail,yulia,kostya,LocalDate.of(1992,12,4));
+        yulia.addChildren(nikita);
+        yulia.addChildren(gosha);
+        yulia.addChildren(nikita);
+        yulia.addChildren(gosha);
+        kostya.addChildren(nikita);
+        kostya.addChildren(gosha);
+
+
+
+
+        System.out.println(yulia.toString());
 
     }
 }
