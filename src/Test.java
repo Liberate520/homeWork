dd5256a503424b51b0230fc3e38db20d0ed7d6be
Ksenia <pankratovaks@gmail.com>
2023-12-05 12:01:36 +0300

    изменение файла Test

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,14 @@
 public class Test {
+    public static void main(String[] args) {
+        FamilyTree familyTree = new FamilyTree();
+
+        Human people1 = new Human("Маша",Gender.Female);
+        Human people2 = new Human("Паша", Gender.Male);
+        Human people3 = new Human("Катя", Gender.Female,people1,people2);
+
+        FamilyTree.addHuman(people1);
+        FamilyTree.addHuman(people2);
+        System.out.println();
+    }
+
 }
