e196a866e72049092317075b985a63cb8bb11bd8
Vasily <48855460+basfeo@users.noreply.github.com>
2023-12-19 15:47:50 +0300

    Added MVP UI

--- src/ru/gb/family_tree_homework/file_handler/Writable.java
+++
@@ -1,8 +0,0 @@
-package ru.gb.family_tree_homework.file_handler;
-
-import java.io.Serializable;
-
-public interface Writable {
-    boolean save(Serializable serializable, String path);
-    Object read(String path);
-}
